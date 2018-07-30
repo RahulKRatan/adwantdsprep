b79cf03f85b46296b56238d03e67ed445e793259
goutamadwant <adwant.goutam@gmail.com>
2018-07-30 00:10:18 -0700

    "First string problem from ctci"

---
+++ src/CTCI6/CheckPermutation.java
@@ -0,0 +1,14 @@
+package CTCI6;
+
+/*
+Given two strings,write a method to decide if one is a permutation of the
+other.
+ */
+public class CheckPermutation {
+
+
+    public static void main(String args[]) {
+
+    }
+
+}
