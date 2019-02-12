27aa771bdb3b9da74e14fa8456c0f709691a4f15
goutamadwant <adwant.goutam@gmail.com>
2019-02-11 23:05:32 -0800

    Knapsack - recursive

--- src/Bootcamp/Knapsack.java
+++ src/Bootcamp/Knapsack.java
@@ -1,4 +1,24 @@
 package Bootcamp;
 
 public class Knapsack {
+
+    static int[] values = new int[] {1, 4, 5, 7};
+    static int[] weights = new int[] {1, 3, 4, 5};
+    static int W = 7;
+
+    private static int knapsack(int i, int W) {
+        if (i < 0) {
+            return 0;
+        }
+        if (weights[i] > W) {
+            return knapsack(i-1, W);
+        } else {
+            return Math.max(knapsack(i-1, W), knapsack(i-1, W - weights[i]) + values[i]);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(knapsack(values.length - 1, W));
+    }
 }
