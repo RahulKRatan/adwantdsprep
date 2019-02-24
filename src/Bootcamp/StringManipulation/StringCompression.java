f40020ebfd20cb9fade4e4d77426836ca43dbf69
goutamadwant <adwant.goutam@gmail.com>
2019-02-23 18:12:06 -0800

    Update StringCompression.java

--- src/Bootcamp/StringManipulation/StringCompression.java
+++ src/Bootcamp/StringManipulation/StringCompression.java
@@ -1,5 +1,27 @@
 package Bootcamp.StringManipulation;
 
+
 public class StringCompression {
-    
+
+    public static int compression(char[] chars){
+        int indexAns = 0, index = 0;
+        while(index < chars.length){
+            char currentChar = chars[index];
+            int count = 0;
+            while(index < chars.length && chars[index] == currentChar){
+                index++;
+                count++;
+            }
+            chars[indexAns++] = currentChar;
+            if(count != 1)
+                for(char c : Integer.toString(count).toCharArray())
+                    chars[indexAns++] = c;
+        }
+        return indexAns;
+    }
+
+    public static void main(String[] args) {
+        char[] chars = new char[]{'a','a','b','b','c','d','d'};
+        System.out.println(compression(chars));
+    }
 }
