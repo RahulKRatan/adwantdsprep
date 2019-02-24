50b61c8ca9d6120de6e05e854b6b8851c75f4a86
goutamadwant <adwant.goutam@gmail.com>
2019-02-23 22:20:58 -0800

    Update ReverseString.java

--- src/Bootcamp/StringManipulation/ReverseString.java
+++ src/Bootcamp/StringManipulation/ReverseString.java
@@ -3,7 +3,16 @@ package Bootcamp.StringManipulation;
 public class ReverseString {
 
     public static void reverseString(char[] s) {
-            
+        int i = 0;
+            while(i< s.length/2){
+                char temp = s[i];
+                s[i] = s[(s.length-1) - i];
+                s[(s.length-1) - i] = temp;
+                i++;
+            }
+            for(int j=0;j<s.length;j++){
+                System.out.println(s[j]);
+            }
     }
 
     public static void main(String[] args) {
