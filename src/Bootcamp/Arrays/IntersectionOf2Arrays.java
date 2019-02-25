eb501f1a64c243546bac92f18e6caa2f3f4a9873
goutamadwant <adwant.goutam@gmail.com>
2019-02-24 22:55:41 -0800

    Create IntersectionOf2Arrays.java

---
+++ src/Bootcamp/Arrays/IntersectionOf2Arrays.java
@@ -0,0 +1,35 @@
+package Bootcamp.Arrays;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+
+public class IntersectionOf2Arrays {
+
+    public static int[] intersection(int[] nums1, int[] nums2) {
+        ArrayList<Integer> res = new ArrayList<>();
+        if(nums1.length == 0 || nums2.length == 0)
+            return new int[]{};
+        HashSet<Integer> intersection = new HashSet<>();
+        for (int i : nums1) {
+            intersection.add(i); // 4,7,9,6
+        }
+        for (int j : nums2){
+            if(intersection.contains(j)){
+                res.add(j);
+                intersection.remove(j);
+            }
+        }
+        int[] arr = new int[res.size()];
+        for (int i= 0; i < res.size(); i++) arr[i] = res.get(i);
+        return arr;
+    }
+
+
+    public static void main(String[] args) {
+        int[] first = new int[]{1,2,2,1};
+        int[] second = new int[]{2,2};
+        System.out.print(intersection(first,second));
+    }
+
+}
