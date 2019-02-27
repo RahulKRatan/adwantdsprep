015f101ec8c351dfcd6a16980598d03f125c7987
goutamadwant <adwant.goutam@gmail.com>
2019-02-26 22:23:54 -0800

    Create FindDuplicateNumber.java

---
+++ src/Bootcamp/Arrays/FindDuplicateNumber.java
@@ -0,0 +1,17 @@
+package Bootcamp.Arrays;
+
+public class FindDuplicateNumber {
+
+    public static int findDuplicate(int[] nums){
+
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,3,4,2,2};
+        System.out.print(findDuplicate(nums));
+    }
+
+
+}
