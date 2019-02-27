60e2ec2c254ed03ce63d0bce27654763efa91cb5
goutamadwant <adwant.goutam@gmail.com>
2019-02-26 23:40:21 -0800

    Update FindDuplicateNumber.java

--- src/Bootcamp/Arrays/FindDuplicateNumber.java
+++ src/Bootcamp/Arrays/FindDuplicateNumber.java
@@ -3,12 +3,25 @@ package Bootcamp.Arrays;
 public class FindDuplicateNumber {
 
     public static int findDuplicate(int[] nums){
-        boolean[] flag = new boolean[nums.length];
-        for(int i=0;i<nums.length;i++){
-            
-        }
+        if (nums.length > 1)
+        {
+            int slow = nums[0];
+            int fast = nums[nums[0]];
+            while (slow != fast)
+            {
+                slow = nums[slow];
+                fast = nums[nums[fast]];
+            }
 
-        return 0;
+            fast = 0;
+            while (fast != slow)
+            {
+                fast = nums[fast];
+                slow = nums[slow];
+            }
+            return slow;
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
