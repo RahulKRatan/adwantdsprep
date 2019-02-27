0064adf128828531a1b5dbe2448d917904f23ac2
goutamadwant <adwant.goutam@gmail.com>
2019-02-27 00:15:43 -0800

    Update MoveZeroes.java

--- src/Bootcamp/Arrays/MoveZeroes.java
+++ src/Bootcamp/Arrays/MoveZeroes.java
@@ -3,7 +3,16 @@ package Bootcamp.Arrays;
 public class MoveZeroes {
 
     public static void moveZeroes(int[] nums) {
-        
+        int pos = 0;
+        for(int num: nums){
+            if(num !=0){
+                nums[pos++] = num;
+            }
+        }
+        while (pos < nums.length){
+            nums[pos++] = 0;
+        }
+
     }
 
     public static void main(String[] args) {
