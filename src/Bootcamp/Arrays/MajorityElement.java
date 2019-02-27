77c37e269044c2f5c42cc65af61f2a7e2e479c2f
goutamadwant <adwant.goutam@gmail.com>
2019-02-27 00:30:43 -0800

    Update MajorityElement.java

--- src/Bootcamp/Arrays/MajorityElement.java
+++ src/Bootcamp/Arrays/MajorityElement.java
@@ -9,9 +9,25 @@ public class MajorityElement {
         return nums[nums.length/2];
     }
 
+   // ** Moore voting algorithm
+   public static int majorityElement2(int[] num) {
+
+       int major=num[0], count = 1;
+       for(int i=1; i<num.length;i++){
+           if(count==0){
+               count++;
+               major=num[i];
+           }else if(major==num[i]){
+               count++;
+           }else count--;
+
+       }
+       return major;
+   }
 
     public static void main(String[] args) {
         int[] nums = new int[]{2,2,1,1,1,2,2};
-        System.out.print(majorityElement(nums));
+        System.out.println(majorityElement(nums));
+        System.out.println(majorityElement2(nums));
     }
 }
