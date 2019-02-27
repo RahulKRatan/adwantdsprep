7de170d4baf1560ac88feffeb3ab2c79c2aa2f5c
goutamadwant <adwant.goutam@gmail.com>
2019-02-27 00:20:23 -0800

    Create MajorityElement.java

---
+++ src/Bootcamp/Arrays/MajorityElement.java
@@ -0,0 +1,16 @@
+package Bootcamp.Arrays;
+
+public class MajorityElement {
+
+    public static int majorityElement(int[] nums) {
+
+
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{3,2,3};
+        majorityElement(nums);
+    }
+}
