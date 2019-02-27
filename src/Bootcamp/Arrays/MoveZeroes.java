653e596ccc6805df863646e4f0c25e107daead8e
goutamadwant <adwant.goutam@gmail.com>
2019-02-27 00:04:12 -0800

    Create MoveZeroes.java

---
+++ src/Bootcamp/Arrays/MoveZeroes.java
@@ -0,0 +1,14 @@
+package Bootcamp.Arrays;
+
+public class MoveZeroes {
+
+    public static void moveZeroes(int[] nums) {
+        
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{0,1,0,3,12};
+        moveZeroes(nums);
+    }
+
+}
