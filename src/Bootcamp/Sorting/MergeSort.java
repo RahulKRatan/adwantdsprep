5189d098aeab0d63a032e7c60fff69300b90c56c
goutamadwant <adwant.goutam@gmail.com>
2019-03-02 11:24:01 -0800

    Update MergeSort.java

--- src/Bootcamp/Sorting/MergeSort.java
+++ src/Bootcamp/Sorting/MergeSort.java
@@ -2,18 +2,22 @@ package Bootcamp.Sorting;
 
 public class MergeSort {
 
-    public static void merge(int[] arr,int start, int mid, int end){
-
-        if(start == end)
+    public static void merge(int[] arr,int start, int end){
+        if(start >= end)
             return;
-        merge(arr,start,mid/2,mid);
-        merge(arr,mid+1,mid+end/2, end);
+        int mid = start + (end-start)/2;
+        merge(arr,start,mid);
+        merge(arr,mid+1,end);
+        mergeSort(arr,start,mid,end);
+    }
+
+    public static void mergeSort(int[] arr, int start, int mid, int end){
 
     }
 
     public static void main(String[] args) {
         int[] arr = new int[]{15,20,2,4,25,3,14,19};
-        merge(arr,0,arr.length/2,arr.length-1);
+        merge(arr,0,arr.length-1);
     }
 
 }
