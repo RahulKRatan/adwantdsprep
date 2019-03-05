1f0a798a4e65a836e1a287049d481da93b0e4b5b
goutamadwant <adwant.goutam@gmail.com>
2019-03-04 17:11:23 -0800

    Update ThreeSum.java

--- src/Bootcamp/Arrays/ThreeSum.java
+++ src/Bootcamp/Arrays/ThreeSum.java
@@ -3,5 +3,17 @@ package Bootcamp.Arrays;
 public class ThreeSum {
 
 
-    
+    static String[] threeSum(int[] arr){
+        String[] strings = new String[3];
+        
+
+
+
+        return strings;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{10, 3, -4, 1, -6, 9};
+        System.out.print(threeSum(arr));
+    }
 }
