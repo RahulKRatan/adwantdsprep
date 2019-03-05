6b5b7a0daf19c442b9b9b542b871db587e8760b3
goutamadwant <adwant.goutam@gmail.com>
2019-03-04 18:04:26 -0800

    Update ThreeSum.java

--- src/Bootcamp/Arrays/ThreeSum.java
+++ src/Bootcamp/Arrays/ThreeSum.java
@@ -1,19 +1,41 @@
 package Bootcamp.Arrays;
 
+import java.util.Arrays;
+
 public class ThreeSum {
 
 
-    static String[] threeSum(int[] arr){
+    static void threeSum(int[] arr){
         String[] strings = new String[3];
-        
 
-
-
-        return strings;
+        if(arr == null || arr.length == 0){
+            return;
+        }
+        Arrays.sort(arr);
+        int size = arr.length;
+        for(int i=0;i<size-2;i++){
+            int l = i+1,r =size-1;
+        while(l<r){
+            if (arr[i] + arr[l]+arr[r] == 0) {
+                System.out.print(arr[i]+","+arr[l]+","+arr[r]);
+                l++;r--;
+                while (l<r && arr[l] == arr[l-1])
+                    l++;
+                while (l<r && arr[r] == arr[r+1])
+                    r--;
+            }
+            if(arr[i] + arr[l]+arr[r] < 0){
+                l++;
+            }
+            else {
+                r--;
+            }
+        }
+        }
     }
 
     public static void main(String[] args) {
-        int[] arr = new int[]{10, 3, -4, 1, -6, 9};
-        System.out.print(threeSum(arr));
+        int[] arr = new int[]{-2,2,0,-2,2};
+        threeSum(arr);
     }
 }
