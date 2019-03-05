863c0d55c41d5a71d48dfd42d9101cb207df0989
goutamadwant <adwant.goutam@gmail.com>
2019-03-04 21:18:53 -0800

    TopK elements from array

--- src/Bootcamp/Arrays/TopK.java
+++ src/Bootcamp/Arrays/TopK.java
@@ -1,4 +1,30 @@
 package Bootcamp.Arrays;
 
+import java.util.Arrays;
+import java.util.TreeSet;
+
 public class TopK {
+
+    static int[] topK(int[] arr, int k) {
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        for(int i=0;i<arr.length;i++){
+            treeSet.add(arr[i]);
+            if(treeSet.size() > k){
+                treeSet.pollFirst();
+            }
+        }
+        int ans[] = new int[treeSet.size()];
+        int pointer = 0;
+        while(treeSet.iterator().hasNext()){
+            ans[pointer++] = treeSet.pollFirst();
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 5, 1, 5, 1};
+        int k = 3;
+        int[] result = topK(arr,k);
+        Arrays.stream(result).forEach(s-> System.out.println(s));
+    }
 }
