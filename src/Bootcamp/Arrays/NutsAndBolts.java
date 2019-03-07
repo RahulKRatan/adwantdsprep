0b4eea78ea9e541fbe272c53691b117fdf5fa61c
goutamadwant <adwant.goutam@gmail.com>
2019-03-06 21:10:10 -0800

    Update NutsAndBolts.java

--- src/Bootcamp/Arrays/NutsAndBolts.java
+++ src/Bootcamp/Arrays/NutsAndBolts.java
@@ -1,5 +1,6 @@
 package Bootcamp.Arrays;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class NutsAndBolts {
@@ -18,10 +19,20 @@ public class NutsAndBolts {
         return strings;
     }
 
+    static String[] solve2(int[] nuts, int[] bolts) {
+        String[] strings = new String[nuts.length];
+        Arrays.sort(nuts);
+        Arrays.sort(bolts);
+        for(int i=0;i<bolts.length;i++){
+                strings[i] = nuts[i] + " " + bolts[i];
+        }
+        return strings;
+    }
+
     public static void main(String[] args) {
         int[] nuts = new int[]{4, 32, 5, 7};
         int[] bolts = new int[]{32, 7, 5, 4};
-        String[] result = solve(nuts,bolts);
+        String[] result = solve2(nuts,bolts);
         for (int i=0;i<result.length;i++){
             System.out.println(result[i]);
         }
