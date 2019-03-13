8abdfcf9f53c8b134ac266004f8d04e897090a21
goutamadwant <adwant.goutam@gmail.com>
2019-03-12 22:54:00 -0700

    Update MergeSort.java

--- src/Bootcamp/Sorting/MergeSort.java
+++ src/Bootcamp/Sorting/MergeSort.java
@@ -2,21 +2,77 @@ package Bootcamp.Sorting;
 
 public class MergeSort {
 
-    public static void mergeSort(int[] arr,int start, int end){
-        if(start >= end)
+    public static void mergeSort(int[] arr,int l, int r){
+        if(l >= r)
             return;
-        int mid = start + (end-start)/2; // ** this is to avoid overflow problem.. if start and end are two large integers.
-        mergeSort(arr,start,mid);
-        mergeSort(arr,mid+1,end);
-        merge(arr,start,mid,end);
+        int m = l + (r-l)/2; // ** this is to avoid overflow problem.. if start and end are two large integers.
+        mergeSort(arr,l,m);
+        mergeSort(arr,m+1,r);
+        merge(arr,l,m,r);
     }
 
-    public static void merge(int[] arr, int start, int mid, int end){
+    public static void merge(int[] arr, int l, int m, int r){
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        /* Create temp arrays */
+        int L[] = new int [n1];
+        int R[] = new int [n2];
+
+        /*Copy data to temp arrays*/
+        for (int i=0; i<n1; ++i)
+            L[i] = arr[l + i];
+        for (int j=0; j<n2; ++j)
+            R[j] = arr[m + 1+ j];
+
+
+        /* Merge the temp arrays */
+
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarry array
+        int k = l;
+        while (i < n1 && j < n2)
+        {
+            if (L[i] <= R[j])
+            {
+                arr[k] = L[i];
+                i++;
+            }
+            else
+            {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        /* Copy remaining elements of L[] if any */
+        while (i < n1)
+        {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        /* Copy remaining elements of R[] if any */
+        while (j < n2)
+        {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+
     }
 
     public static void main(String[] args) {
         int[] arr = new int[]{15,20,2,4,25,3,14,19};
         mergeSort(arr,0,arr.length-1);
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
     }
 
 }
