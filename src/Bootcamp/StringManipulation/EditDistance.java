69999dad45a2ca9f1dc7f9a0c66ea1c9923fae4e
goutamadwant <adwant.goutam@gmail.com>
2019-03-14 19:08:07 -0700

    Update EditDistance.java

--- src/Bootcamp/StringManipulation/EditDistance.java
+++ src/Bootcamp/StringManipulation/EditDistance.java
@@ -1,5 +1,28 @@
 package Bootcamp.StringManipulation;
 
 public class EditDistance {
-    
+
+    public static int minDistance(String word1, String word2) {
+        return editDistance(word1,word2,0,0);
+    }
+
+    public static int editDistance(String s1, String s2, int i, int j){
+        if(i==s1.length() || j==s2.length()){
+            return Integer.max(s1.length()-i,s2.length()-j);
+        }
+        if(s1.charAt(i) == s2.charAt(j)){
+            return editDistance(s1,s2,i+1,j+1);
+        }
+
+        int temp = editDistance(s1,s2,i+1,j) < editDistance(s1,s2,i,j+1) ? editDistance(s1,s2,i+1,j):editDistance(s1,s2,i,j+1);
+        int result = editDistance(s1,s2,i+1,j+1) < temp ? editDistance(s1,s2,i+1,j+1):temp;
+
+        return 1 + result;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "intention";
+        String s2 = "execution";
+        System.out.println(minDistance(s1,s2));
+    }
 }
