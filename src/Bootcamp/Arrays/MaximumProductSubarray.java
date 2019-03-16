4e7d630c2a246d11a2bd16ce11501bdb543aa180
goutamadwant <adwant.goutam@gmail.com>
2019-03-16 12:35:51 -0700

    Update MaximumProductSubarray.java

--- src/Bootcamp/Arrays/MaximumProductSubarray.java
+++ src/Bootcamp/Arrays/MaximumProductSubarray.java
@@ -1,4 +1,21 @@
 package Bootcamp.Arrays;
 
 public class MaximumProductSubarray {
+    public static int maxProduct(int[] nums) {
+        if (nums.length == 0) return 0;
+        int max = nums[0], min = nums[0], totalMax = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int tempMax = nums[i] * max;
+            int tempMin = nums[i] * min;
+            max = Math.max(Math.max(tempMax,tempMin), nums[i]);
+            min = Math.min(Math.min(tempMax,tempMin), nums[i]);
+            totalMax = Math.max(totalMax, max);
+        }
+        return totalMax;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{-2,3,-4};
+        System.out.println(maxProduct(nums));
+    }
 }
