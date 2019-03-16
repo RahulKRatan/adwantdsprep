104c8408fb4d04429b58b3d8ea7ea1e5b0ce623a
goutamadwant <adwant.goutam@gmail.com>
2019-03-16 13:18:56 -0700

    Create WordSearch.java

---
+++ src/Bootcamp/Arrays/WordSearch.java
@@ -0,0 +1,15 @@
+package Bootcamp.Arrays;
+
+public class WordSearch {
+
+    public static boolean exist(char[][] board, String word) {
+
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        char[][] chars = new char[][]{{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+        String word = "ABCCED";
+    }
+}
