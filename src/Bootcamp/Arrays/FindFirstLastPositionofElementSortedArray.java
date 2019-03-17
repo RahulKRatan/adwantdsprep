31a1c2177f21d4315c4c5ecb3269cd623813543a
goutamadwant <adwant.goutam@gmail.com>
2019-03-16 19:58:15 -0700

    Update FindFirstLastPositionofElementSortedArray.java

--- src/Bootcamp/Arrays/FindFirstLastPositionofElementSortedArray.java
+++ src/Bootcamp/Arrays/FindFirstLastPositionofElementSortedArray.java
@@ -1,4 +1,10 @@
 package Bootcamp.Arrays;
 
 public class FindFirstLastPositionofElementSortedArray {
+
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{4,5,6,7,0,1,2};
+
+    }
 }
