3fac435919bc082f60e7f81c63953eed76706e36
goutamadwant <adwant.goutam@gmail.com>
2019-03-16 19:59:57 -0700

    Update FindFirstLastPositionofElementSortedArray.java

--- src/Bootcamp/Arrays/FindFirstLastPositionofElementSortedArray.java
+++ src/Bootcamp/Arrays/FindFirstLastPositionofElementSortedArray.java
@@ -2,9 +2,16 @@ package Bootcamp.Arrays;
 
 public class FindFirstLastPositionofElementSortedArray {
 
+    public static int[] searchRange(int[] nums, int target) {
+
+        return nums;
+    }
 
     public static void main(String[] args) {
-        int[] nums = new int[]{4,5,6,7,0,1,2};
-
+        int[] nums = new int[]{5,7,7,8,8,10};
+        int[] result = searchRange(nums,8);
+        for (int res: result) {
+            System.out.println(res);
+        }
     }
 }
