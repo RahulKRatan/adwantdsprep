e6f56b3a186a4595493b77fa710f0aa2e8df205d
goutamadwant <adwant.goutam@gmail.com>
2019-03-18 21:53:17 -0700

    Update EditDistance.java

--- src/Bootcamp/StringManipulation/EditDistance.java
+++ src/Bootcamp/StringManipulation/EditDistance.java
@@ -3,21 +3,38 @@ package Bootcamp.StringManipulation;
 public class EditDistance {
 
     public static int minDistance(String word1, String word2) {
-        return editDistance(word1,word2,0,0);
+        return editDistance(word1,word2,word1.length(),word2.length());
+    }
+
+    static int min(int x,int y,int z)
+    {
+        if (x<=y && x<=z) return x;
+        if (y<=x && y<=z) return y;
+        else return z;
     }
 
     public static int editDistance(String s1, String s2, int i, int j){
-        if(i==s1.length() || j==s2.length()){
-            return Integer.max(s1.length()-i,s2.length()-j);
+        // If first string is empty, the only option is to
+        // insert all characters of second string into first
+        if(i==0){
+            return j;
         }
-        if(s1.charAt(i) == s2.charAt(j)){
-            return editDistance(s1,s2,i+1,j+1);
+        // If second string is empty, the only option is to
+        // remove all characters of first string
+        if(j==0){
+            return i;
         }
-
-        int temp = editDistance(s1,s2,i+1,j) < editDistance(s1,s2,i,j+1) ? editDistance(s1,s2,i+1,j):editDistance(s1,s2,i,j+1);
-        int result = editDistance(s1,s2,i+1,j+1) < temp ? editDistance(s1,s2,i+1,j+1):temp;
-
-        return 1 + result;
+        // If last characters of two strings are same, nothing
+        // much to do. Ignore last characters and get count for
+        // remaining strings.
+        if(s1.charAt(i-1) == s2.charAt(j-1)){
+            return editDistance(s1,s2,i-1,j-1);
+        }
+        //If last characters are not same, consider all three
+        // operations on last character of first string, recursively
+        // compute minimum cost for all three operations and take
+        // minimum of three values.
+        return 1 + min(editDistance(s1,s2,i-1,j),editDistance(s1,s2,i,j-1),editDistance(s1,s2,i-1,j-1));
     }
 
     public static void main(String[] args) {
