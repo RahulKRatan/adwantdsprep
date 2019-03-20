7d57fdb249e3fd00f3fd51ab09efad1804ca0b8b
goutamadwant <adwant.goutam@gmail.com>
2019-03-19 20:48:06 -0700

    Update IsBST.java

--- src/Bootcamp/Trees/IsBST.java
+++ src/Bootcamp/Trees/IsBST.java
@@ -1,4 +1,24 @@
 package Bootcamp.Trees;
 
 public class IsBST {
+
+
+    public static void main(String[] args) {
+        
+    }
+}
+
+
+
+
+
+class Node4{
+    int data;
+    Node4 left;
+    Node4 right;
+    public Node4(int item)
+    {
+        data = item;
+        left = right = null;
+    }
 }
