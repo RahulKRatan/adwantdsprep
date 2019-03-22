e864595e28bee96461d787cf271fa135b3d3dd72
goutamadwant <adwant.goutam@gmail.com>
2019-03-21 18:27:29 -0700

    Update BalancedBSTFromSortedArray.java

--- src/Bootcamp/Trees/BalancedBSTFromSortedArray.java
+++ src/Bootcamp/Trees/BalancedBSTFromSortedArray.java
@@ -1,4 +1,25 @@
 package Bootcamp.Trees;
 
 public class BalancedBSTFromSortedArray {
+
+
+    public static void main(String[] args) {
+        BalancedBSTFromSortedArray tree = new BalancedBSTFromSortedArray();
+        
+    }
 }
+
+
+class TreeNode1
+{
+    int val;
+    TreeNode1 left_ptr;
+    TreeNode1 right_ptr;
+
+    TreeNode1(int _val)
+    {
+        val = _val;
+        left_ptr = null;
+        right_ptr = null;
+    }
+};
