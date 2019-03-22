ae5ce9fff1735ef99b0b747ff323fd3dbfae6e46
goutamadwant <adwant.goutam@gmail.com>
2019-03-21 18:12:03 -0700

    Update KSmallestElement.java

--- src/Bootcamp/Trees/KSmallestElement.java
+++ src/Bootcamp/Trees/KSmallestElement.java
@@ -1,4 +1,26 @@
 package Bootcamp.Trees;
 
 public class KSmallestElement {
+
+
+    public static void main(String[] args) {
+        KSmallestElement tree = new KSmallestElement();
+
+    }
 }
+
+
+
+class TreeNode
+{
+    int val;
+    TreeNode left_ptr;
+    TreeNode right_ptr;
+
+    TreeNode(int _val)
+    {
+        val = _val;
+        left_ptr = null;
+        right_ptr = null;
+    }
+};
