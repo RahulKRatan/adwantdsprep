1cd2989a173f91e8dabbb1a6b2bcfa695f55f7bd
goutamadwant <adwant.goutam@gmail.com>
2019-03-22 16:08:22 -0700

    Update PrintAllPaths.java

--- src/Bootcamp/Trees/PrintAllPaths.java
+++ src/Bootcamp/Trees/PrintAllPaths.java
@@ -1,19 +1,19 @@
 package Bootcamp.Trees;
 
 public class PrintAllPaths {
-    Node6 root;
-    void printPaths(Node6 node)
+    TreeNode root;
+    void printPaths(TreeNode node)
     {
         int path[] = new int[1000];
         printPathsRecur(node, path, 0);
     }
-    void printPathsRecur(Node6 node, int path[], int pathLen)
+    void printPathsRecur(TreeNode node, int path[], int pathLen)
     {
         if (node == null)
             return;
 
         /* append this node to the path array */
-        path[pathLen] = node.data;
+        path[pathLen] = node.val;
         pathLen++;
 
         /* it's a leaf, so print the path that led to here */
@@ -37,25 +37,11 @@ public class PrintAllPaths {
     }
     public static void main(String[] args) {
         PrintAllPaths tree = new PrintAllPaths();
-        tree.root = new Node6(2);
-        tree.root.left = new Node6(1);
-        tree.root.right = new Node6(3);
+        tree.root = new TreeNode(2);
+        tree.root.left = new TreeNode(1);
+        tree.root.right = new TreeNode(3);
         tree.printPaths(tree.root);
     }
 
 
-}
-
-
-
-
-class Node6{
-    int data;
-    Node6 left;
-    Node6 right;
-    public Node6(int item)
-    {
-        data = item;
-        left = right = null;
-    }
 }
