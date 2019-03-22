29c62ceb7ae80f51b54ca5088110001886a9b26e
goutamadwant <adwant.goutam@gmail.com>
2019-03-22 16:06:57 -0700

    Update IsBST.java

--- src/Bootcamp/Trees/IsBST.java
+++ src/Bootcamp/Trees/IsBST.java
@@ -1,42 +1,27 @@
 package Bootcamp.Trees;
 
 public class IsBST {
-    Node4 root;
+    TreeNode root;
 
-    boolean isBST(Node4 root){
+    boolean isBST(TreeNode root){
         return isBST(root,Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
     }
 
-    boolean isBST(Node4 root,double min, double max)
+    boolean isBST(TreeNode root,double min, double max)
     {
         if(root == null)
             return true;
-        if(root.data < min || root.data > max){
+        if(root.val < min || root.val > max){
             return false;
         }
-        return isBST(root.left,min,root.data) && isBST(root.right,root.data,max);
+        return isBST(root.left,min,root.val) && isBST(root.right,root.val,max);
     }
 
     public static void main(String[] args) {
         IsBST tree = new IsBST();
-        tree.root = new Node4(2);
-        tree.root.left = new Node4(1);
-        tree.root.right = new Node4(3);
+        tree.root = new TreeNode(2);
+        tree.root.left = new TreeNode(1);
+        tree.root.right = new TreeNode(3);
         System.out.println(tree.isBST(tree.root));
-    }
-}
-
-
-
-
-
-class Node4{
-    int data;
-    Node4 left;
-    Node4 right;
-    public Node4(int item)
-    {
-        data = item;
-        left = right = null;
     }
 }
