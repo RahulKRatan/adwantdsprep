9117626e4aa0d49ca5897de56374ae157bf70aa9
goutamadwant <adwant.goutam@gmail.com>
2019-03-22 15:41:53 -0700

    Update MaxDepthBinaryTree.java

--- src/Bootcamp/Trees/MaxDepthBinaryTree.java
+++ src/Bootcamp/Trees/MaxDepthBinaryTree.java
@@ -2,12 +2,19 @@ package Bootcamp.Trees;
 
 public class MaxDepthBinaryTree {
     TreeNode node;
+    public static int maxDepth(TreeNode root) {
+        
+
+        return 0;
+    }
 
     public static void main(String[] args) {
         MaxDepthBinaryTree tree = new MaxDepthBinaryTree();
-        tree.node = new TreeNode(1);
-        tree.node.left = new TreeNode(3);
-        tree.node.right = new TreeNode(2);
-        tree.node.left.left = new TreeNode(5);
+        tree.node = new TreeNode(3);
+        tree.node.left = new TreeNode(9);
+        tree.node.right = new TreeNode(20);
+        tree.node.right.left = new TreeNode(15);
+        tree.node.right.right = new TreeNode(7);
+        System.out.println(maxDepth(tree.node));
     }
 }
