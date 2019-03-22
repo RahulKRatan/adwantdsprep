f9d82f79c6cd25a41b6b4ede57484535be3ce030
goutamadwant <adwant.goutam@gmail.com>
2019-03-22 15:16:05 -0700

    Update MergeTwoBinaryTrees.java

--- src/Bootcamp/Trees/MergeTwoBinaryTrees.java
+++ src/Bootcamp/Trees/MergeTwoBinaryTrees.java
@@ -1,4 +1,18 @@
 package Bootcamp.Trees;
 
 public class MergeTwoBinaryTrees {
+    TreeNode node1;
+    TreeNode node2;
+
+
+    public static void main(String[] args) {
+        MergeTwoBinaryTrees trees = new MergeTwoBinaryTrees();
+        trees.node1 = new TreeNode(2);
+        trees.node1.left = new TreeNode(1);
+        trees.node1.right = new TreeNode(3);
+        trees.node2 = new TreeNode(5);
+        trees.node2 = new TreeNode(4);
+        trees.node2 = new TreeNode(6);
+    }
+
 }
