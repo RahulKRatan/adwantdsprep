82f48ab6c1f761bcbb4c02e0b259e1606baa4568
goutamadwant <adwant.goutam@gmail.com>
2019-03-22 15:12:24 -0700

    Update BalancedBSTFromSortedArray.java

--- src/Bootcamp/Trees/BalancedBSTFromSortedArray.java
+++ src/Bootcamp/Trees/BalancedBSTFromSortedArray.java
@@ -1,40 +1,25 @@
 package Bootcamp.Trees;
 
 public class BalancedBSTFromSortedArray {
-    static TreeNode1 build_balanced_bst(int[] a)
+    static TreeNode build_balanced_bst(int[] a)
     {
         return balancedBST(a,0,a.length-1);
     }
 
-    static TreeNode1 balancedBST(int[] a, int start, int end){
+    static TreeNode balancedBST(int[] a, int start, int end){
         if (start > end) {
             return null;
         }
         int mid = (start + end) / 2;
-        TreeNode1 node = new TreeNode1(a[mid]);
-        node.left_ptr = balancedBST(a,start,mid-1);
-        node.right_ptr = balancedBST(a,mid+1,end);
+        TreeNode node = new TreeNode(a[mid]);
+        node.left = balancedBST(a,start,mid-1);
+        node.right = balancedBST(a,mid+1,end);
         return node;
     }
 
     public static void main(String[] args) {
        int[] a = new int[]{8,10 ,12, 15, 16, 20, 25};
-       TreeNode1 result = build_balanced_bst(a);
+        TreeNode result = build_balanced_bst(a);
        System.out.println(result.val);
     }
 }
-
-
-class TreeNode1
-{
-    int val;
-    TreeNode1 left_ptr;
-    TreeNode1 right_ptr;
-
-    TreeNode1(int _val)
-    {
-        val = _val;
-        left_ptr = null;
-        right_ptr = null;
-    }
-};
