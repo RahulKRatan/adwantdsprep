8b41ce5b38f73f6b1caae5e04c98f34975e33768
goutamadwant <adwant.goutam@gmail.com>
2019-03-23 09:54:44 -0700

    Update PathSum3.java

--- src/Bootcamp/Trees/PathSum3.java
+++ src/Bootcamp/Trees/PathSum3.java
@@ -1,4 +1,19 @@
 package Bootcamp.Trees;
 
 public class PathSum3 {
+    TreeNode node;
+    
+
+
+    public static void main(String[] args) {
+        PathSum3 tree = new PathSum3();
+        tree.node = new TreeNode(10);
+        tree.node.left = new TreeNode(5);
+        tree.node.right = new TreeNode(-3);
+        tree.node.left.left = new TreeNode(3);
+        tree.node.left.right = new TreeNode(2);
+        tree.node.left.left.left = new TreeNode(3);
+        tree.node.left.left.right= new TreeNode(-2);
+        tree.node.right.right = new TreeNode(11);
+    }
 }
