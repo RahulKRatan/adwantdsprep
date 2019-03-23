12de7d211fc9e9ef7df8e1ee65368a6c465a7611
goutamadwant <adwant.goutam@gmail.com>
2019-03-23 12:15:16 -0700

    Update PathSum3.java

--- src/Bootcamp/Trees/PathSum3.java
+++ src/Bootcamp/Trees/PathSum3.java
@@ -1,11 +1,30 @@
 package Bootcamp.Trees;
 
+
+/*
+The first function pathSum() recursively calls itself for every child Node of root, returning 0 when finding a null Node.
+pathSum also calls pathSumFrom on its current Node. pathSumFrom recursively traverses the tree down,
+subtracting the nodes value from the sum. Doing this, keeps a running total of the sum of the visited nodes.
+If the node.val == sum that means the path from the original root to the current node, is a sum path.
+Since we aren't required to keep any information on the path itself, we simply return 1 representing a valid path.
+
+From a high level, pathSum() recursively visits each Node and pathSumFrom() recursively visits each child Node and
+totals their values and checks that against the sum. Every node will be checked including all possible paths downward. */
+
+
 public class PathSum3 {
     TreeNode node;
     public static int pathSum(TreeNode root, int sum) {
-        
-
-        return 0;
+        if(root == null){
+            return 0;
+        }
+        return pathFromSum(root,sum) + pathSum(root.left,sum) + pathSum(root.right,sum);
+    }
+    public static int pathFromSum(TreeNode node,int sum){
+        if(node == null){
+            return 0;
+        }
+        return (node.val == sum ? 1 : 0) + pathFromSum(node.left,sum-node.val) + pathFromSum(node.right,sum-node.val);
     }
 
 
