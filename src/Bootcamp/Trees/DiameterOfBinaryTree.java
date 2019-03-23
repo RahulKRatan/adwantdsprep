db474b3c0ee1f1199933302e76a73d9d213c3c8a
goutamadwant <adwant.goutam@gmail.com>
2019-03-23 14:01:30 -0700

    Update DiameterOfBinaryTree.java

--- src/Bootcamp/Trees/DiameterOfBinaryTree.java
+++ src/Bootcamp/Trees/DiameterOfBinaryTree.java
@@ -1,9 +1,15 @@
 package Bootcamp.Trees;
 
 public class DiameterOfBinaryTree {
-
+    TreeNode node;
+    
 
     public static void main(String[] args) {
-        
+        DiameterOfBinaryTree tree = new DiameterOfBinaryTree();
+        tree.node = new TreeNode(1);
+        tree.node.left = new TreeNode(2);
+        tree.node.right = new TreeNode(3);
+        tree.node.left.left = new TreeNode(4);
+        tree.node.left.right = new TreeNode(5);
     }
 }
