942eeba6b75ae87ca1f52ac97175c0db4361050b
goutamadwant <adwant.goutam@gmail.com>
2019-03-23 17:41:25 -0700

    Update DiameterOfBinaryTree.java

--- src/Bootcamp/Trees/DiameterOfBinaryTree.java
+++ src/Bootcamp/Trees/DiameterOfBinaryTree.java
@@ -1,9 +1,24 @@
 package Bootcamp.Trees;
 
+//For every node, length of longest path which pass it = MaxDepth of its left subtree + MaxDepth of its right subtree.
 public class DiameterOfBinaryTree {
     TreeNode node;
-    
+    static int max = 0;
+    public static int diameterOfBinaryTree(TreeNode root) {
+        maxDepth(root);
+        return max;
+    }
 
+    private static int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        max = Math.max(max, left + right);
+
+        return Math.max(left, right) + 1;
+    }
     public static void main(String[] args) {
         DiameterOfBinaryTree tree = new DiameterOfBinaryTree();
         tree.node = new TreeNode(1);
@@ -11,5 +26,6 @@ public class DiameterOfBinaryTree {
         tree.node.right = new TreeNode(3);
         tree.node.left.left = new TreeNode(4);
         tree.node.left.right = new TreeNode(5);
+        System.out.println(diameterOfBinaryTree(tree.node));
     }
 }
