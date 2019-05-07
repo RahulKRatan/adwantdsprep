1a349235bcebe0e81221806b65159ff012503ad9
goutamadwant <adwant.goutam@gmail.com>
2019-05-07 08:52:54 -0700

    Update ProductArrayExceptSelf.java

--- src/Bootcamp/Arrays/ProductArrayExceptSelf.java
+++ src/Bootcamp/Arrays/ProductArrayExceptSelf.java
@@ -2,11 +2,12 @@ package Bootcamp.Arrays;
 
 public class ProductArrayExceptSelf {
 
-    public int[] productExceptSelf(int[] nums) {
+    public static int[] productExceptSelf(int[] nums) {
 
     }
 
     public static void main(String[] args) {
-        
+        int[] nums = new int[]{1,2,3,4};
+        productExceptSelf(nums);
     }
 }
