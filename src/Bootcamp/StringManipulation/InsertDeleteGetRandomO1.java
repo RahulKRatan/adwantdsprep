dbdb913225178ba64903b6edca211e518f4766b8
goutamadwant <adwant.goutam@gmail.com>
2019-05-07 20:47:25 -0700

    Update InsertDeleteGetRandomO1.java

--- src/Bootcamp/StringManipulation/InsertDeleteGetRandomO1.java
+++ src/Bootcamp/StringManipulation/InsertDeleteGetRandomO1.java
@@ -1,4 +1,51 @@
 package Bootcamp.StringManipulation;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+/*
+Thank you! You gave me the missing piece of the puzzle - I couldn't figure out how to remove an element from the array
+without shifting the elements and updating their indexes in the map, causing the removal to cost O(N).
+The idea of swapping the item to be deleted with the last item is simply brilliant !
+ */
 public class InsertDeleteGetRandomO1 {
+
+    ArrayList<Integer> nums;
+    HashMap<Integer, Integer> locs;
+    java.util.Random rand = new java.util.Random();
+    /** Initialize your data structure here. */
+    public InsertDeleteGetRandomO1() {
+        nums = new ArrayList<Integer>();
+        locs = new HashMap<Integer, Integer>();
+    }
+
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    public boolean insert(int val) {
+        boolean contain = locs.containsKey(val);
+        if ( contain ) return false;
+        locs.put( val, nums.size());
+        nums.add(val);
+        return true;
+    }
+
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    public boolean remove(int val) {
+        boolean contain = locs.containsKey(val);
+        if ( ! contain ) return false;
+        int loc = locs.get(val);
+        if (loc < nums.size() - 1 ) { // not the last one than swap the last one with this val
+            int lastone = nums.get(nums.size() - 1 );
+            nums.set( loc , lastone );
+            locs.put(lastone, loc);
+        }
+        locs.remove(val);
+        nums.remove(nums.size() - 1);
+        return true;
+    }
+
+    /** Get a random element from the set. */
+    public int getRandom() {
+        return nums.get( rand.nextInt(nums.size()) );
+    }
+
+
 }
