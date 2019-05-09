195d41090ff3dc290c2c775ee2c7e398fe85a58d
goutamadwant <adwant.goutam@gmail.com>
2019-05-09 00:37:34 -0700

    Update CountPrimes.java

--- src/Bootcamp/StringManipulation/CountPrimes.java
+++ src/Bootcamp/StringManipulation/CountPrimes.java
@@ -1,4 +1,14 @@
 package Bootcamp.StringManipulation;
 
 public class CountPrimes {
+
+    public static int countPrimes(int n) {
+
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countPrimes(20));
+    }
 }
