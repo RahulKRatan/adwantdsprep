1886d973d16a55e298c19f92c0fde76be0be7757
goutamadwant <adwant.goutam@gmail.com>
2019-05-10 16:39:56 -0700

    Update AddTwoNumbers.java

--- src/Bootcamp/LinkedList/AddTwoNumbers.java
+++ src/Bootcamp/LinkedList/AddTwoNumbers.java
@@ -1,4 +1,20 @@
 package Bootcamp.LinkedList;
 
 public class AddTwoNumbers {
+
+
+
+
+
+    public static void main(String[] args) {
+        ListNode first = new ListNode(2);
+        first.next = new ListNode(4);
+        first.next.next = new ListNode(3);
+
+        ListNode second = new ListNode(5);
+        second.next = new ListNode(6);
+        second.next.next = new ListNode(4);
+    }
+
+
 }
