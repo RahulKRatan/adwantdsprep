d4df3011057c31da0d764fe73615e42fdd5eee15
goutamadwant <adwant.goutam@gmail.com>
2019-05-09 21:26:48 -0700

    Update CountPrimes.java

--- src/Bootcamp/StringManipulation/CountPrimes.java
+++ src/Bootcamp/StringManipulation/CountPrimes.java
@@ -1,11 +1,22 @@
 package Bootcamp.StringManipulation;
 
+
+// Sieve of Eratosthenes algorithm - T : O(nloglogn)
 public class CountPrimes {
 
     public static int countPrimes(int n) {
+        boolean[] notPrime = new boolean[n];
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (notPrime[i] == false) {
+                count++;
+                for (int j = 2; i*j < n; j++) {
+                    notPrime[i*j] = true;
+                }
+            }
+        }
 
-
-        return 0;
+        return count;
     }
 
     public static void main(String[] args) {
