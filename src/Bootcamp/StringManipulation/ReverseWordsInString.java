9e041b4b4a69d068c397193c8a86487699f97d2f
goutamadwant <adwant.goutam@gmail.com>
2019-05-24 01:05:17 -0700

    Update ReverseWordsInString.java

--- src/Bootcamp/StringManipulation/ReverseWordsInString.java
+++ src/Bootcamp/StringManipulation/ReverseWordsInString.java
@@ -3,16 +3,21 @@ package Bootcamp.StringManipulation;
 public class ReverseWordsInString {
 
     public static String reverseWords(String s) {
-
-
-
+        System.out.println(reverse(s));
         return "";
     }
 
     public static String reverse(String string){
-
-
-        return "";
+        char[] str = string.trim().toCharArray();
+        int i=0;
+        int j = str.length-1;
+        while(i<j){
+            char temp = str[i];
+            str[i] = str [j];
+            str[j] = temp;
+            i++;j--;
+        }
+        return str.toString();
     }
 
     public static void main(String[] args) {
