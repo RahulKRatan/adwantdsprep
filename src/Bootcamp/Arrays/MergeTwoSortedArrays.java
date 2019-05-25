09f885c23a46b1b82bda8f22bafeb09930b0b77f
goutamadwant <adwant.goutam@gmail.com>
2019-05-24 23:33:59 -0700

    Update MergeTwoSortedArrays.java

--- src/Bootcamp/Arrays/MergeTwoSortedArrays.java
+++ src/Bootcamp/Arrays/MergeTwoSortedArrays.java
@@ -1,9 +1,10 @@
 package Bootcamp.Arrays;
 
 public class MergeTwoSortedArrays {
-
+        
 
     public static void main(String[] args) {
-        
+        int[] num2 = new int[]{1,3,5};
+        int[] num1 = new int[]{2,4,6,0,0,0};
     }
 }
