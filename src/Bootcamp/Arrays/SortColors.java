fa6543562e7ecff0e21243172a423dd16d2a0e51
goutamadwant <adwant.goutam@gmail.com>
2019-05-25 18:50:37 -0700

    Update SortColors.java

--- src/Bootcamp/Arrays/SortColors.java
+++ src/Bootcamp/Arrays/SortColors.java
@@ -6,7 +6,18 @@ public class SortColors {
         int first = 0;
         int third = nums.length-1;
         for(int i=0;i<third;i++){
-            
+            if(nums[i] == 0){
+                int temp = nums[i];
+                nums[i] = nums[first];
+                nums[first] = temp;
+                first++;
+            }else if(nums[i] == 2){
+                int temp = nums[i];
+                nums[i] = nums[third];
+                nums[third] = temp;
+                third--;
+                i--;
+            }
         }
         return nums;
     }
