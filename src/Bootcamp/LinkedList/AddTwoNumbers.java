a9c437f0b7feda34253e86771c0b31edb87f3155
goutamadwant <adwant.goutam@gmail.com>
2019-06-03 00:19:02 -0700

    Update AddTwoNumbers.java

--- src/Bootcamp/LinkedList/AddTwoNumbers.java
+++ src/Bootcamp/LinkedList/AddTwoNumbers.java
@@ -3,10 +3,23 @@ package Bootcamp.LinkedList;
 public class AddTwoNumbers {
 
     public static Node addTwoNumbers(Node l1, Node l2) {
-        
-
-
-        return null;
+        Node dummyHead = new Node(0);
+        Node p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new Node(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new Node(carry);
+        }
+        return dummyHead.next;
     }
 
 
@@ -21,6 +34,7 @@ public class AddTwoNumbers {
         second.next.next = new Node(4);
 
         Node result = addTwoNumbers(first,second);
+        System.out.println(result);
     }
 
 
