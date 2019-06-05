b9f3d34b9ed53019c26ab97820c9182788649935
goutamadwant <adwant.goutam@gmail.com>
2019-06-04 21:06:26 -0700

    Create LRUCache.java

---
+++ src/Bootcamp/Design/LRUCache.java
@@ -0,0 +1,35 @@
+package Bootcamp.Design;
+
+/**
+ * Design and implement a data structure for Least Recently Used (LRU) cache.
+ * It should support the following operations: get and put.
+ *
+ * get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+ * put(key, value) - Set or insert the value if the key is not already present.
+ * When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+ */
+
+/**
+ *  Solution : 
+ *  Hashmap + DoubleLinkedList
+ */
+public class LRUCache {
+
+    public static void LRUCache(int capacity) {
+
+
+    }
+
+    public static int get(int key) {
+
+        return 0;
+    }
+
+    public static void put(int key, int value) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
