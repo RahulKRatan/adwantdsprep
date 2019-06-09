61d6868329ce85a3be754a43947d68fbda2f8529
goutamadwant <adwant.goutam@gmail.com>
2019-06-08 21:44:30 -0700

    Update AutocompleteSystem.java

--- src/Bootcamp/Design/AutocompleteSystem.java
+++ src/Bootcamp/Design/AutocompleteSystem.java
@@ -1,8 +1,21 @@
 package Bootcamp.Design;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class AutocompleteSystem {
+
+    class TrieNode {
+        Map<Character, TrieNode> children;
+        Map<String, Integer> counts;
+        boolean isWord;
+        public TrieNode() {
+            children = new HashMap<>();
+            counts = new HashMap<>();
+            isWord = false;
+        }
+    }
 
     public AutocompleteSystem(String[] sentences, int[] times) {
 
