8d7761c5a7724375667afc77b2adf9a37a025e02
goutamadwant <adwant.goutam@gmail.com>
2019-06-09 01:34:11 -0700

    Update MaxStack.java

--- src/Bootcamp/Design/MaxStack.java
+++ src/Bootcamp/Design/MaxStack.java
@@ -1,4 +1,30 @@
 package Bootcamp.Design;
 
 public class MaxStack {
+
+    /** initialize your data structure here. */
+    public MaxStack() {
+
+    }
+
+    public void push(int x) {
+
+    }
+
+    public int pop() {
+
+    }
+
+    public int top() {
+
+    }
+
+    public int peekMax() {
+
+    }
+
+    public int popMax() {
+
+    }
+
 }
