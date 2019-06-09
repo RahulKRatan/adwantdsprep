d705a5d005444fceb6b2376fb49a1550f32d8a94
goutamadwant <adwant.goutam@gmail.com>
2019-06-08 21:43:42 -0700

    Update AutocompleteSystem.java

--- src/Bootcamp/Design/AutocompleteSystem.java
+++ src/Bootcamp/Design/AutocompleteSystem.java
@@ -1,4 +1,16 @@
 package Bootcamp.Design;
 
+import java.util.List;
+
 public class AutocompleteSystem {
+
+    public AutocompleteSystem(String[] sentences, int[] times) {
+
+    }
+
+    public static List<String> input(char c) {
+
+    }
+
+
 }
