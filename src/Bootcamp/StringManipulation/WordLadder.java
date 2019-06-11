7d5387affe99574288083e07615cc5ae44beb736
goutamadwant <adwant.goutam@gmail.com>
2019-06-11 00:35:08 -0700

    Update WordLadder.java

--- src/Bootcamp/StringManipulation/WordLadder.java
+++ src/Bootcamp/StringManipulation/WordLadder.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
+ * https://leetcode.com/problems/word-ladder/discuss/40704/Java-Solution-using-BFS-with-explanation
  * Basically I keep two sets of words, one set reached that represents the borders that have been reached with "distance" steps; another set wordDict
  * that has not been reached. In the while loop, for each word in the reached set, I give all variations and check if it matches anything from wordDict,
  * if it has a match, I add that word into toAdd set, which will be my "reached" set in the next loop, and remove the word from wordDict because I already
