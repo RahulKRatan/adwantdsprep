a3416cdba8a3ba8a7094d5fa4336c1cae4096a6d
goutamadwant <adwant.goutam@gmail.com>
2019-06-12 01:12:46 -0700

    Update GroupAnagrams.java

--- src/Bootcamp/StringManipulation/Anagram/GroupAnagrams.java
+++ src/Bootcamp/StringManipulation/Anagram/GroupAnagrams.java
@@ -5,9 +5,19 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * Two strings are anagrams if and only if their sorted strings are equal.
+ * Maintain a map ans : {String -> List} where each key K is a sorted string, and each value is the list of strings from the initial input that when sorted, are equal to K.
+ *
+ * In Java, we will store the key as a string, eg. code
+ * Time Complexity: O(NKlog K), where N is the length of strs, and K is the maximum length of a string in strs.
+ * The outer loop has complexity O(N) as we iterate through each string. Then, we sort each string in O(KlogK) time.
+ *
+ * Space Complexity: O(NK), the total information content stored in ans.
+ */
+
 public class GroupAnagrams {
-
-
+    
     public static List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String,List<String>> temp = new HashMap<>();
         List<List<String>> result = new ArrayList<>();
@@ -25,7 +35,6 @@ public class GroupAnagrams {
                 cde.add(str);
                 temp.put(sorted,cde);
             }
-
         }
         result.addAll(temp.values());
         return result;
