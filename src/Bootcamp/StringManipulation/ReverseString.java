de48f00226a18f36f7c9bd6e7af15c3a0e8f57f1
goutamadwant <adwant.goutam@gmail.com>
2019-06-13 20:25:39 -0700

    Update ReverseString.java

--- src/Bootcamp/StringManipulation/ReverseString.java
+++ src/Bootcamp/StringManipulation/ReverseString.java
@@ -12,11 +12,33 @@ public class ReverseString {
             }
             for(int j=0;j<s.length;j++){
                 System.out.print(s[j]);
+                System.out.println();
             }
+    }
+
+    public static void swap(char[] s, int i, int j){
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+    }
+
+    public static void recursiveReverse(char[] s){
+        recursiveHelper(s,0,s.length);
+        String result = new String(s);
+        System.out.println(result);
+    }
+
+    public static void recursiveHelper(char[] s, int start, int end){
+        if(start < end/2){
+            return;
+        }
+        recursiveHelper(s,start+1,end-1);
+        swap(s,start,end);
     }
 
     public static void main(String[] args) {
         char[] chars = new char[]{'h','e','l','l','o'};
         reverseString(chars);
+        recursiveReverse(chars);
     }
 }
