a6288de9a20b69ce9d54f809c79a86d40770a30c
goutamadwant <adwant.goutam@gmail.com>
2019-06-17 10:03:44 -0700

    Update LRUCache.java

--- src/Bootcamp/Design/LRUCache.java
+++ src/Bootcamp/Design/LRUCache.java
@@ -9,119 +9,133 @@ package Bootcamp.Design;
  * When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
  */
 
-import java.util.Hashtable;
-
 /**
  *  Solution :
  *  Hashmap + DoubleLinkedList
  */
-public class LRUCache {
+import java.util.HashMap;
 
-    private Hashtable<Integer, DLinkedNode> cache = new Hashtable<>();
-    private int count;
-    private int capacity;
-    private DLinkedNode head, tail;
+class Node {
+    int key;
+    int value;
+    Node pre;
+    Node next;
 
-    class DLinkedNode {
-        int key;
-        int value;
-        DLinkedNode pre;
-        DLinkedNode post;
-    }
-
-    /**
-     * Always add the new node right after head;
-     */
-    private void addNode(DLinkedNode node) {
-
-        node.pre = head;
-        node.post = head.post;
-
-        head.post.pre = node;
-        head.post = node;
-    }
-
-    /**
-     * Remove an existing node from the linked list.
-     */
-    private void removeNode(DLinkedNode node){
-        DLinkedNode pre = node.pre;
-        DLinkedNode post = node.post;
-
-        pre.post = post;
-        post.pre = pre;
-    }
-
-    /**
-     * Move certain node in between to the head.
-     */
-    private void moveToHead(DLinkedNode node){
-        this.removeNode(node);
-        this.addNode(node);
-    }
-
-    // pop the current tail.
-    private DLinkedNode popTail(){
-        DLinkedNode res = tail.pre;
-        this.removeNode(res);
-        return res;
-    }
-
-    public  void LRUCache(int capacity) {
-        this.count = 0;
-        this.capacity = capacity;
-
-        head = new DLinkedNode();
-        head.pre = null;
-
-        tail = new DLinkedNode();
-        tail.post = null;
-
-        head.post = tail;
-        tail.pre = head;
-
-    }
-
-    public  int get(int key) {
-        DLinkedNode node = cache.get(key);
-        if(node == null){
-            return -1; // should raise exception here.
-        }
-
-        // move the accessed node to the head;
-        this.moveToHead(node);
-
-        return node.value;
-    }
-
-    public void put(int key, int value) {
-        DLinkedNode node = cache.get(key);
-
-        if(node == null){
-
-            DLinkedNode newNode = new DLinkedNode();
-            newNode.key = key;
-            newNode.value = value;
-
-            this.cache.put(key, newNode);
-            this.addNode(newNode);
-
-            ++count;
-
-            if(count > capacity){
-                // pop the tail
-                DLinkedNode tail = this.popTail();
-                this.cache.remove(tail.key);
-                --count;
-            }
-        }else{
-            // update the value.
-            node.value = value;
-            this.moveToHead(node);
-        }
-    }
-
-    public static void main(String[] args) {
-
+    public Node(int key, int value)
+    {
+        this.key = key;
+        this.value = value;
     }
 }
+
+class LRUCacheTest {
+    private HashMap<Integer, Node> map;
+    private int capicity, count;
+    private Node head, tail;
+
+    public LRUCacheTest(int capacity)
+    {
+        this.capicity = capacity;
+        map = new HashMap<>();
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head.next = tail;
+        tail.pre = head;
+        head.pre = null;
+        tail.next = null;
+        count = 0;
+    }
+
+    public void deleteNode(Node node)
+    {
+        node.pre.next = node.next;
+        node.next.pre = node.pre;
+    }
+
+    public void addToHead(Node node)
+    {
+        node.next = head.next;
+        node.next.pre = node;
+        node.pre = head;
+        head.next = node;
+    }
+
+    // This method works in O(1)
+    public int get(int key)
+    {
+        if (map.get(key) != null) {
+            Node node = map.get(key);
+            int result = node.value;
+            deleteNode(node);
+            addToHead(node);
+            System.out.println("Got the value : " +
+                    result + " for the key: " + key);
+            return result;
+        }
+        System.out.println("Did not get any value" +
+                " for the key: " + key);
+        return -1;
+    }
+
+    // This method works in O(1)
+    public void set(int key, int value)
+    {
+        System.out.println("Going to set the (key, "+
+                "value) : (" + key + ", " + value + ")");
+        if (map.get(key) != null) {
+            Node node = map.get(key);
+            node.value = value;
+            deleteNode(node);
+            addToHead(node);
+        }
+        else {
+            Node node = new Node(key, value);
+            map.put(key, node);
+            if (count < capicity) {
+                count++;
+                addToHead(node);
+            }
+            else {
+                map.remove(tail.pre.key);
+                deleteNode(tail.pre);
+                addToHead(node);
+            }
+        }
+    }
+}
+
+public class LRUCache {
+    public static void main(String[] args)
+    {
+        System.out.println("Going to test the LRU "+
+                " Cache Implementation");
+        LRUCacheTest cache = new LRUCacheTest(2);
+
+        // it will store a key (1) with value
+        // 10 in the cache.
+        cache.set(1, 10);
+
+        // it will store a key (1) with value 10 in the cache.
+        cache.set(2, 20);
+        System.out.println("Value for the key: 1 is " +
+                cache.get(1)); // returns 10
+
+        // evicts key 2 and store a key (3) with
+        // value 30 in the cache.
+        cache.set(3, 30);
+
+        System.out.println("Value for the key: 2 is " +
+                cache.get(2)); // returns -1 (not found)
+
+        // evicts key 1 and store a key (4) with
+        // value 40 in the cache.
+        cache.set(4, 40);
+        System.out.println("Value for the key: 1 is " +
+                cache.get(1)); // returns -1 (not found)
+        System.out.println("Value for the key: 3 is " +
+                cache.get(3)); // returns 30
+        System.out.println("Value for the key: 4 is " +
+                cache.get(4)); // return 40
+    }
+}
+
