ba7942840c26b64aca3a943c2b985404201738b5
goutamadwant <adwant.goutam@gmail.com>
2019-07-09 00:54:37 -0700

    Update ZigzagLevelOrder.java

--- src/Bootcamp/Trees/ZigzagLevelOrder.java
+++ src/Bootcamp/Trees/ZigzagLevelOrder.java
@@ -2,9 +2,16 @@ package Bootcamp.Trees;
 
 public class ZigzagLevelOrder {
 
+    TreeNode node;
 
 
+    public static void main(String[] args) {
+        ZigzagLevelOrder tree = new ZigzagLevelOrder();
+        tree.node = new TreeNode(1);
+        tree.node.left = new TreeNode(2);
+        tree.node.right = new TreeNode(5);
+        tree.node.left.left = new TreeNode(3);
+        tree.node.left.right = new TreeNode(4);
+    }
 
-
-    
 }
