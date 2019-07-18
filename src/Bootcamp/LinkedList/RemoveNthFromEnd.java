4c169e3e94fe7f383cdf93c527beda71aa649b2d
goutamadwant <adwant.goutam@gmail.com>
2019-07-17 23:58:06 -0700

    Update RemoveNthFromEnd.java

--- src/Bootcamp/LinkedList/RemoveNthFromEnd.java
+++ src/Bootcamp/LinkedList/RemoveNthFromEnd.java
@@ -4,6 +4,9 @@ public class RemoveNthFromEnd {
 
 
     public static void main(String[] args) {
-        
+        Node first = new Node(1);
+        first.next = new Node(2);
+        first.next.next = new Node(3);
+        first.next.next.next = new Node(4);
     }
 }
