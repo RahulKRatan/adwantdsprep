8a32ee7c80868e1cb7f9474647b653eaabd4538d
goutamadwant <adwant.goutam@gmail.com>
2019-07-17 23:12:29 -0700

    Create ReorderList.java

---
+++ src/Bootcamp/LinkedList/ReorderList.java
@@ -0,0 +1,16 @@
+package Bootcamp.LinkedList;
+
+public class ReorderList {
+
+    public static void reorderList(Node head) {
+
+    }
+
+    public static void main(String[] args) {
+        Node first = new Node(1);
+        first.next = new Node(2);
+        first.next.next = new Node(3);
+        first.next.next.next = new Node(4);
+    }
+
+}
