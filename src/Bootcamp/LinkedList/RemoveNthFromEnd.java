a0c2f3dbe58f3568399b0c8c99d635295b438525
goutamadwant <adwant.goutam@gmail.com>
2019-07-18 01:23:53 -0700

    Update RemoveNthFromEnd.java

--- src/Bootcamp/LinkedList/RemoveNthFromEnd.java
+++ src/Bootcamp/LinkedList/RemoveNthFromEnd.java
@@ -1,9 +1,32 @@
 package Bootcamp.LinkedList;
 
+
+/**
+ * Fast and slow pointers
+ */
 public class RemoveNthFromEnd {
 
     public static Node removeNthFromEnd(Node head, int n) {
-        
+        if(head == null){
+            return null;
+        }
+        Node slow = head;
+        Node fast = head;
+        for(int i=0; i<n; i++){
+            fast = fast.next;
+        }
+        //if remove the first node
+        if(fast == null){
+            head = head.next;
+            return head;
+        }
+        while(fast.next != null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+
+        return head;
     }
 
     public static void main(String[] args) {
