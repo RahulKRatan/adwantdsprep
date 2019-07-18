f4a08a6c50af91542accb1a27890d4c0ef47f32b
goutamadwant <adwant.goutam@gmail.com>
2019-07-17 21:09:01 -0700

    Create AddTwoNumbersII.java

---
+++ src/Bootcamp/LinkedList/AddTwoNumbersII.java
@@ -0,0 +1,18 @@
+package Bootcamp.LinkedList;
+
+public class AddTwoNumbersII {
+
+    
+
+
+    public static void main(String[] args) {
+        Node first = new Node(2);
+        first.next = new Node(4);
+        first.next.next = new Node(3);
+
+        Node second = new Node(5);
+        second.next = new Node(6);
+        second.next.next = new Node(4);
+    }
+
+}
