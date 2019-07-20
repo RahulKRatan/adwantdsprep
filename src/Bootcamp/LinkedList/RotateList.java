79a2e92482962733ece8b5504379bc7b333ff8b1
goutamadwant <adwant.goutam@gmail.com>
2019-07-20 00:42:20 -0700

    Update RotateList.java

--- src/Bootcamp/LinkedList/RotateList.java
+++ src/Bootcamp/LinkedList/RotateList.java
@@ -1,4 +1,21 @@
 package Bootcamp.LinkedList;
 
 public class RotateList {
+
+    public static Node rotateRight(Node head, int k) {
+
+
+        return head;
+    }
+
+
+
+    public static void main(String[] args) {
+        Node first = new Node(1);
+        first.next = new Node(2);
+        first.next.next = new Node(3);
+        first.next.next.next = new Node(4);
+        first.next.next.next.next = new Node(5);
+        System.out.println(rotateRight(first,3));
+    }
 }
