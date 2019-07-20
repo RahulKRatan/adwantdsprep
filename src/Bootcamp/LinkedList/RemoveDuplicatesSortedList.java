8bf81d4671b34cb8e609ba789ca7f8655accb43b
goutamadwant <adwant.goutam@gmail.com>
2019-07-20 01:10:16 -0700

    Update RemoveDuplicatesSortedList.java

--- src/Bootcamp/LinkedList/RemoveDuplicatesSortedList.java
+++ src/Bootcamp/LinkedList/RemoveDuplicatesSortedList.java
@@ -1,4 +1,27 @@
 package Bootcamp.LinkedList;
 
 public class RemoveDuplicatesSortedList {
+
+    public static Node deleteDuplicates(Node head) {
+        Node temp = new Node(-1);
+        temp.next = head;
+        while (temp != null && temp.next !=null){
+            if(temp.val == temp.next.val){
+                temp.next = temp.next.next;
+            }
+            else {
+                temp = temp.next;
+            }
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        Node first = new Node(1);
+        first.next = new Node(1);
+        first.next.next = new Node(1);
+        Node result = deleteDuplicates(first);
+        System.out.println(result);
+    }
+
 }
