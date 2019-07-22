d165cd90c8d112a7a5f0355d60aac101c03d7784
goutamadwant <adwant.goutam@gmail.com>
2019-07-21 22:38:28 -0700

    Update SearchInRotatedSortedArray.java

--- src/Bootcamp/Arrays/SearchInRotatedSortedArray.java
+++ src/Bootcamp/Arrays/SearchInRotatedSortedArray.java
@@ -15,9 +15,7 @@ public class SearchInRotatedSortedArray {
                     end = mid - 1;
                 else
                     start = mid + 1;
-            }
-
-            if (nums[mid] <= nums[end]){
+            }else{
                 if (target > nums[mid] && target <= nums[end])
                     start = mid + 1;
                 else
@@ -26,6 +24,31 @@ public class SearchInRotatedSortedArray {
         }
         return -1;
     }
+
+    /*
+    public int search(int[] nums, int target) {
+    int lo = 0, hi = nums.length - 1;
+    while (lo <= hi) {
+        int mid = lo + (hi - lo) / 2;
+
+        int num = nums[mid];
+        // If nums[mid] and target are "on the same side" of nums[0], we just take nums[mid].
+        if ((nums[mid] < nums[0]) == (target < nums[0])) {
+            num = nums[mid];
+        } else {
+            num = target < nums[0] ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        }
+
+        if (num < target)
+            lo = mid + 1;
+        else if (num > target)
+            hi = mid - 1;
+        else
+            return mid;
+    }
+    return -1;
+}
+     */
 
     public static void main(String[] args) {
         int[] nums = new int[]{4,5,6,7,0,1,2};
