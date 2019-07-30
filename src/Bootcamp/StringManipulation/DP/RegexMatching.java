6675f98e9c649f26e5f3dda3af1eb2c91e913bd2
goutamadwant <adwant.goutam@gmail.com>
2019-07-30 00:12:45 -0700

    Update RegexMatching.java

--- src/Bootcamp/StringManipulation/DP/RegexMatching.java
+++ src/Bootcamp/StringManipulation/DP/RegexMatching.java
@@ -1,5 +1,33 @@
 package Bootcamp.StringManipulation.DP;
 
+
+/**
+ * Recursive formulation of DP
+ *
+ * DP How to process the '*'。
+ *
+ * dp[i][j] stands for whether s[0:i-1] matches p[0:j-1]
+ *
+ * Formula：Because only p will have regular expression，so we will analysis based on p.charAt(j - 1)
+ *
+ * p.charAt(j-1) != '.' && p.charAt(j-1) != '*'：dp[i][j] = dp[i-1][j-1] && (s.charAt(i-1) == p.charAt(j-1))
+ *
+ * p.charAt(j-1) == '.'：dp[i][j] = dp[i-1][j-1]
+ *
+ * The key point is when p.charAt(j-1) = '*'。Because '*' can match 0，1，or more p.charAt(j - 2)。
+ *
+ * 1. Match 0 element，remove p.charAt(j-2)，now p[0: j-1] = p[0: j-3]
+ *
+ * dp[i][j] = dp[i][j-2]
+ *
+ * 2. Match 1 element，here we have p[0: j-1] = p[0: j-2]
+ *
+ * dp[i][j] = dp[i][j-1]
+ *
+ * 3. Match 2+ elements，here p[0: j-1] = { p[0: j-2], p[j-2], ... , p[j-2] }
+ *
+ * dp[i][j] = dp[i-1][j] && (p[j-2]=='.' || s[i-2]==p[j-2])
+ */
 public class RegexMatching {
 
     public boolean matchRegexRecursive(char[] str, char[] pattern){
