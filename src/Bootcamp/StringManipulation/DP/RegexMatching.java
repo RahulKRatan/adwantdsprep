49f178fd97dceaba010aeb1ed6ca27bc7909cf93
goutamadwant <adwant.goutam@gmail.com>
2019-07-29 23:33:15 -0700

    Update RegexMatching.java

--- src/Bootcamp/StringManipulation/DP/RegexMatching.java
+++ src/Bootcamp/StringManipulation/DP/RegexMatching.java
@@ -4,6 +4,7 @@ public class RegexMatching {
 
 
     public static void main(String[] args) {
+        RegexMatching rm = new RegexMatching();
         System.out.println(rm.matchRegexRecursive("aaa".toCharArray(),"ab*a*c*a".toCharArray()));
     }
 }
