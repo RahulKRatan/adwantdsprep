c5339ac4245ad55fee20b5d3cfdb451706506622
goutamadwant <adwant.goutam@gmail.com>
2019-07-31 01:23:52 -0700

    Update DecodeString.java

--- src/Bootcamp/StringManipulation/DecodeString.java
+++ src/Bootcamp/StringManipulation/DecodeString.java
@@ -3,12 +3,12 @@ package Bootcamp.StringManipulation;
 public class DecodeString {
 
     public static String decodeString(String s) {
-
+        
 
         return "";
     }
 
     public static void main(String[] args) {
-        System.out.println(decodeString(""));
+        System.out.println(decodeString("3[a]2[bc]"));
     }
 }
