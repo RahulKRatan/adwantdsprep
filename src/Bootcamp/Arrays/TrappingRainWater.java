a0c08a82688a3c058cd327f74ae0661b5412b9b4
goutamadwant <adwant.goutam@gmail.com>
2019-07-30 21:18:20 -0700

    Update TrappingRainWater.java

--- src/Bootcamp/Arrays/TrappingRainWater.java
+++ src/Bootcamp/Arrays/TrappingRainWater.java
@@ -3,8 +3,14 @@ package Bootcamp.Arrays;
 public class TrappingRainWater {
 
     public int trap(int[] height) {
+        
 
+        return 0;
     }
 
-    
+    public static void main(String[] args) {
+        int[] height = new int[]{0,1,0,2,1,0,1,3,2,1,2,1};
+    }
+
+
 }
