846c1f50ff1331e3e77e2433ef079a0dcf0be704
goutamadwant <adwant.goutam@gmail.com>
2019-07-30 21:13:01 -0700

    Update ContainerWithMostWater.java

--- src/Bootcamp/Arrays/ContainerWithMostWater.java
+++ src/Bootcamp/Arrays/ContainerWithMostWater.java
@@ -4,6 +4,12 @@ package Bootcamp.Arrays;
  * Time complexity : O(n). Single pass.
  *
  * Space complexity : O(1). Constant space is used.
+ * The intuition behind this approach is that the area formed between the lines will always be limited by the height of the shorter line.
+ * Further, the farther the lines, the more will be the area obtained.
+ *
+ * We take two pointers, one at the beginning and one at the end of the array constituting the length of the lines.
+ * Further, we maintain a variable \text{maxarea}maxarea to store the maximum area obtained till now.
+ * At every step, we find out the area formed between them, update \text{maxarea}maxarea and move the pointer pointing to the shorter line towards the other end by one step.
  */
 public class ContainerWithMostWater {
 
