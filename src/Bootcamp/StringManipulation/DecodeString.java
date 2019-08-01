bb0a3035086b8d8d329c443a8ecc134995e7da06
goutamadwant <adwant.goutam@gmail.com>
2019-07-31 22:06:26 -0700

    Update DecodeString.java

--- src/Bootcamp/StringManipulation/DecodeString.java
+++ src/Bootcamp/StringManipulation/DecodeString.java
@@ -1,11 +1,44 @@
 package Bootcamp.StringManipulation;
 
+import java.util.Stack;
+
 public class DecodeString {
 
     public static String decodeString(String s) {
-
-
-        return "";
+        String res = "";
+        Stack<Integer> countStack = new Stack<>();
+        // resStack will always have recent calculated result
+        Stack<String> resStack = new Stack<>();
+        int idx = 0;
+        while (idx < s.length()) {
+            if (Character.isDigit(s.charAt(idx))) {
+                int count = 0;
+                while (Character.isDigit(s.charAt(idx))) {
+                    count = 10 * count + (s.charAt(idx) - '0');
+                    idx++;
+                }
+                countStack.push(count);
+            }
+            else if (s.charAt(idx) == '[') {
+                // pushing previous calculated result
+                resStack.push(res);
+                res = "";
+                idx++;
+            }
+            else if (s.charAt(idx) == ']') {
+                StringBuilder temp = new StringBuilder (resStack.pop());
+                int repeatTimes = countStack.pop();
+                for (int i = 0; i < repeatTimes; i++) {
+                    temp.append(res);
+                }
+                res = temp.toString();
+                idx++;
+            }
+            else {
+                res += s.charAt(idx++);
+            }
+        }
+        return res;
     }
 
     public static void main(String[] args) {
