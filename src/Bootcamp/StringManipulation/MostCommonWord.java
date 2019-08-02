1237634287aec69ec9ece4d8a5a279c28b6c6a1a
goutamadwant <adwant.goutam@gmail.com>
2019-08-01 23:14:03 -0700

    Update MostCommonWord.java

--- src/Bootcamp/StringManipulation/MostCommonWord.java
+++ src/Bootcamp/StringManipulation/MostCommonWord.java
@@ -1,4 +1,22 @@
 package Bootcamp.StringManipulation;
 
+import java.util.HashMap;
+
 public class MostCommonWord {
+
+    public static String mostCommonWord(String paragraph, String[] banned) {
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        paragraph = paragraph.replaceAll("\\p{Punct}","");
+        String[] input = paragraph.trim().split(" ");
+
+
+        return "";
+    }
+
+
+    public static void main(String[] args) {
+        String paragrah = "Bob hit a ball, the hit BALL flew far after it was hit.";
+        String[] banned = new String[]{"hit"};
+        System.out.println(mostCommonWord(paragrah,banned));
+    }
 }
