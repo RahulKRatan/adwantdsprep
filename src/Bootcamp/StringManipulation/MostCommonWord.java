61eaedee69b8a9fb47edc0f880c71e725d190e97
goutamadwant <adwant.goutam@gmail.com>
2019-08-02 00:06:59 -0700

    Update MostCommonWord.java

--- src/Bootcamp/StringManipulation/MostCommonWord.java
+++ src/Bootcamp/StringManipulation/MostCommonWord.java
@@ -1,18 +1,27 @@
 package Bootcamp.StringManipulation;
 
-import java.util.HashMap;
+import java.util.*;
 
 public class MostCommonWord {
 
     public static String mostCommonWord(String paragraph, String[] banned) {
-        HashMap<String, Integer> hashMap = new HashMap<>();
-        paragraph = paragraph.replaceAll("\\p{Punct}","");
-        String[] input = paragraph.trim().split(" ");
-
-
-        return "";
+        String[] words = paragraph.toLowerCase().split("[ !?',;.]+");
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String word : words) map.put(word, map.getOrDefault(word, 0) + 1);
+        for(String word : banned) if(map.containsKey(word)) map.remove(word);
+        String res = null;
+        for(String word : map.keySet())
+            if(res == null || map.get(word) > map.get(res))
+                res = word;
+        return res;
     }
-
+/*
+ Set<String> ban = new HashSet<>(Arrays.asList(banned));
+        Map<String, Integer> count = new HashMap<>();
+        String[] words = paragraph.replaceAll("\\W+" , " ").toLowerCase().split("\\s+");
+        for (String w : words) if (!ban.contains(w)) count.put(w, count.getOrDefault(w, 0) + 1);
+        return Collections.max(count.entrySet(), Map.Entry.comparingByValue()).getKey();
+ */
 
     public static void main(String[] args) {
         String paragrah = "Bob hit a ball, the hit BALL flew far after it was hit.";
