131524b1fffe9750196e2e0f6c0c13a59a1757a4
goutamadwant <adwant.goutam@gmail.com>
2019-08-04 20:13:27 -0700

    Update PartitionLabels.java

--- src/Bootcamp/StringManipulation/PartitionLabels.java
+++ src/Bootcamp/StringManipulation/PartitionLabels.java
@@ -19,13 +19,13 @@ public class PartitionLabels {
     public static List<Integer> partitionLabels(String S) {
         int[] last = new int[26];
         for (int i = 0; i < S.length(); ++i)
-            last[S.charAt(i) - 'a'] = i;
+            last[S.charAt(i) - 'a'] = i; // this will have last entry of that alphabet in the string
 
         int j = 0, anchor = 0;
         List<Integer> ans = new ArrayList();
         for (int i = 0; i < S.length(); ++i) {
-            j = Math.max(j, last[S.charAt(i) - 'a']);
-            if (i == j) {
+            j = Math.max(j, last[S.charAt(i) - 'a']); // we check here if we reached last element of labels and reset anchor
+            if (i == j) { // we check here if we reached last element of labels and add and reset anchor
                 ans.add(i - anchor + 1);
                 anchor = i + 1;
             }
