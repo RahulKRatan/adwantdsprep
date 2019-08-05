09b322f86a8ab1e47d16e1f8a4112d94f1d328ac
goutamadwant <adwant.goutam@gmail.com>
2019-08-05 00:53:36 -0700

    Update ShortestUnsortedContinuousSubarray.java

--- src/Bootcamp/Arrays/ShortestUnsortedContinuousSubarray.java
+++ src/Bootcamp/Arrays/ShortestUnsortedContinuousSubarray.java
@@ -1,4 +1,15 @@
 package Bootcamp.Arrays;
 
 public class ShortestUnsortedContinuousSubarray {
+
+    public static int findUnsortedSubarray(int[] nums) {
+
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+    int[] nums = new int[]{2, 6, 4, 8, 10, 9, 15};
+    System.out.println(findUnsortedSubarray(nums));
+    }
 }
