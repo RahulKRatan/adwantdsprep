dc5705144b66a06118b93a965b93691d7713e071
goutamadwant <adwant.goutam@gmail.com>
2019-08-04 19:29:34 -0700

    Update PartitionLabels.java

--- src/Bootcamp/StringManipulation/PartitionLabels.java
+++ src/Bootcamp/StringManipulation/PartitionLabels.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 public class PartitionLabels {
 
-    public static List<Integer> partitionLabels(String S) {
-        
+    public static List<Integer> partitionLabels(String s) {
+
         return null;
     }
 
     public static void main(String[] args) {
         String string = "ababcbacadefegdehijhklij";
+        List<Integer> result = partitionLabels(string);
+        System.out.println(result);
     }
 }
