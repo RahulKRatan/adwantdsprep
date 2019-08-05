218bf34e21aeac62bf10d1e75f3b52e511f52237
goutamadwant <adwant.goutam@gmail.com>
2019-08-05 00:53:44 -0700

    Create SingleNonDuplicateElementSortedArray.java

---
+++ src/Bootcamp/Arrays/SingleNonDuplicateElementSortedArray.java
@@ -0,0 +1,15 @@
+package Bootcamp.Arrays;
+
+public class SingleNonDuplicateElementSortedArray {
+
+    public static int singleNonDuplicate(int[] nums) {
+        
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,1,2,3,3,4,4,8,8};
+        System.out.println(singleNonDuplicate(nums));
+    }
+}
