b651dfec559759a3d5947f08dc73573ecf2d8455
goutamadwant <adwant.goutam@gmail.com>
2019-08-11 19:59:02 +0530

    Update SingleNonDuplicateElementSortedArray.java

--- src/Bootcamp/Arrays/SingleNonDuplicateElementSortedArray.java
+++ src/Bootcamp/Arrays/SingleNonDuplicateElementSortedArray.java
@@ -4,8 +4,30 @@ public class SingleNonDuplicateElementSortedArray {
 
     public static int singleNonDuplicate(int[] nums) {
         int start = 0, end = nums.length - 1;
+        while (start < end) {
+            // We want the first element of the middle pair,
+            // which should be at an even index if the left part is sorted.
+            // Example:
+            // Index: 0 1 2 3 4 5 6
+            // Array: 1 1 3 3 4 8 8
+            //            ^
+            int mid = (start + end) / 2;
+            if (mid % 2 == 1) mid--;
 
-        return 0;
+            // We didn't find a pair. The single element must be on the left.
+            // (pipes mean start & end)
+            // Example: |0 1 1 3 3 6 6|
+            //               ^ ^
+            // Next:    |0 1 1|3 3 6 6
+            if (nums[mid] != nums[mid + 1]) end = mid;
+
+                // We found a pair. The single element must be on the right.
+                // Example: |1 1 3 3 5 6 6|
+                //               ^ ^
+                // Next:     1 1 3 3|5 6 6|
+            else start = mid + 2;
+        }
+        return nums[start];
     }
 
     public static void main(String[] args) {
