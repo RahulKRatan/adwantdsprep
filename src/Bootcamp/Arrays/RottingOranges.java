7a5d15ccf2a745f5be602631b9ffdba517dfeaea
goutamadwant <adwant.goutam@gmail.com>
2019-08-13 10:55:45 +0530

    Create RottingOranges.java

---
+++ src/Bootcamp/Arrays/RottingOranges.java
@@ -0,0 +1,14 @@
+package Bootcamp.Arrays;
+
+public class RottingOranges {
+
+    public static int orangesRotting(int[][] grid) {
+        
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[][] oranges = new int[][]{{2,1,1},{1,1,0},{0,1,1}};
+        System.out.println(orangesRotting(oranges));
+    }
+}
