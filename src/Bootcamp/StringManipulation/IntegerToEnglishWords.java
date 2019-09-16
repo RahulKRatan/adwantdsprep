1bb06ac57d3601621eac280349a1587e87a57fc8
goutamadwant <adwant.goutam@gmail.com>
2019-09-15 23:02:47 -0700

    Update IntegerToEnglishWords.java

--- src/Bootcamp/StringManipulation/IntegerToEnglishWords.java
+++ src/Bootcamp/StringManipulation/IntegerToEnglishWords.java
@@ -2,7 +2,13 @@ package Bootcamp.StringManipulation;
 
 public class IntegerToEnglishWords {
 
+    public static String numberToWords(int num) {
+
+
+        return null;
+    }
+
     public static void main(String[] args) {
-        
+        System.out.println(numberToWords(124));
     }
 }
