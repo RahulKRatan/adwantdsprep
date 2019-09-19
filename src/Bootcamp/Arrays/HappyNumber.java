c41fba26f8728453a51c0a21a4b7ad5be396513e
goutamadwant <adwant.goutam@gmail.com>
2019-09-18 22:28:20 -0700

    Update HappyNumber.java

--- src/Bootcamp/Arrays/HappyNumber.java
+++ src/Bootcamp/Arrays/HappyNumber.java
@@ -1,13 +1,27 @@
 package Bootcamp.Arrays;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class HappyNumber {
 
     public static boolean isHappy(int n) {
-
+        Set<Integer> inLoop = new HashSet<Integer>();
+        while (inLoop.add(n)){
+            int temp = 0;
+            while(n!=0){
+                int val = n%10;
+                temp = temp + (val*val);
+                n = n/10;
+            }
+            if (temp == 1)
+                return true;
+            n = temp;
+        }
         return false;
     }
 
     public static void main(String[] args) {
-        System.out.println(isHappy(19));
+        System.out.println(isHappy(7));
     }
 }
