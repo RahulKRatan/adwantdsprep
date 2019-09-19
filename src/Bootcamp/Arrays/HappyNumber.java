60b17fcd2411754d70a40a983256701e3c08463d
goutamadwant <adwant.goutam@gmail.com>
2019-09-18 22:01:37 -0700

    Update HappyNumber.java

--- src/Bootcamp/Arrays/HappyNumber.java
+++ src/Bootcamp/Arrays/HappyNumber.java
@@ -3,11 +3,11 @@ package Bootcamp.Arrays;
 public class HappyNumber {
 
     public static boolean isHappy(int n) {
-        
+
         return false;
     }
 
     public static void main(String[] args) {
-        System.out.println(isHappy(124));
+        System.out.println(isHappy(19));
     }
 }
