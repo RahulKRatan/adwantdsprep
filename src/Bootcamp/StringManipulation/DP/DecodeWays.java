d45999d18e7ee38549fb91643d0b19361bac38e1
goutamadwant <adwant.goutam@gmail.com>
2019-09-22 22:23:32 -0700

    Update DecodeWays.java

--- src/Bootcamp/StringManipulation/DP/DecodeWays.java
+++ src/Bootcamp/StringManipulation/DP/DecodeWays.java
@@ -3,7 +3,7 @@ package Bootcamp.StringManipulation.DP;
 public class DecodeWays {
 
     public static int numDecodings(String s) {
-
+        
         for(int i=0;i<s.length();i++){
             System.out.println(s.substring(i,i+1));
         }
