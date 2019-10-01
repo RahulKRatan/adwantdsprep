6a0d6fbbaa226dba4e0afeb70dddf78af6d5228e
goutamadwant <adwant.goutam@gmail.com>
2019-09-30 21:13:41 -0700

    Create MaximalSquare.java

---
+++ src/Bootcamp/StringManipulation/DP/MaximalSquare.java
@@ -0,0 +1,15 @@
+package Bootcamp.StringManipulation.DP;
+
+public class MaximalSquare {
+
+    public static int maximalSquare(char[][] matrix) {
+        
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        char[][] chars = new char[][]{{'1','0','1','0','0'},{'1','0','1','1','1'},{'1','1','1','1','1'},{'1','0','0','1','0'}};
+        System.out.println(maximalSquare(chars));
+    }
+}
