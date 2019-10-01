bf7eb0f4efb0b5bd81fb822e30ae5c4982a003f0
goutamadwant <adwant.goutam@gmail.com>
2019-10-01 00:27:08 -0700

    Create MaximalRectangle.java

---
+++ src/Bootcamp/StringManipulation/DP/MaximalRectangle.java
@@ -0,0 +1,14 @@
+package Bootcamp.StringManipulation.DP;
+
+public class MaximalRectangle {
+
+    public static int maximalRectangle(char[][] matrix) {
+
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        char[][] chars = new char[][]{{'1','0','1','0','0'},{'1','0','1','1','1'},{'1','1','1','1','1'},{'1','0','0','1','0'}};
+    }
+}
