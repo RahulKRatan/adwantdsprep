7910ae7e31f95740c3e4cfd3c486699ef50d8159
goutamadwant <adwant.goutam@gmail.com>
2019-10-02 10:56:45 -0700

    Create FindTheDifference.java

---
+++ src/Bootcamp/Arrays/BitManipuation/FindTheDifference.java
@@ -0,0 +1,19 @@
+package Bootcamp.Arrays.BitManipuation;
+
+public class FindTheDifference {
+
+    public static char findTheDifference(String s, String t) {
+        int n = t.length();
+        char c = t.charAt(n - 1);
+        for (int i = 0; i < n - 1; ++i) {
+            c ^= s.charAt(i);
+            c ^= t.charAt(i);
+        }
+        return c;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findTheDifference("abcd","eabcd"));
+    }
+
+}
