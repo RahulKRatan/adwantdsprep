98758dc0d2629b50f4e9893698441650b4cda171
goutamadwant <adwant.goutam@gmail.com>
2019-10-06 21:51:44 -0700

    Create MeetingRooms.java

---
+++ src/Bootcamp/Arrays/MeetingRooms.java
@@ -0,0 +1,15 @@
+package Bootcamp.Arrays;
+
+public class MeetingRooms {
+
+    public boolean canAttendMeetings(int[][] intervals) {
+
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
