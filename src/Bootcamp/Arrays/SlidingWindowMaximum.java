17d4ea2e99f6f6fa421d6f4a9d14d7d3e3e9db91
goutamadwant <adwant.goutam@gmail.com>
2019-10-06 21:52:07 -0700

    Create SlidingWindowMaximum.java

---
+++ src/Bootcamp/Arrays/SlidingWindowMaximum.java
@@ -0,0 +1,15 @@
+package Bootcamp.Arrays;
+
+public class SlidingWindowMaximum {
+
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] ints = new int[]{1,3,-1,-3,5,3,6,7};
+        int[] result = maxSlidingWindow(ints,3);
+        System.out.println(result);
+    }
+}
