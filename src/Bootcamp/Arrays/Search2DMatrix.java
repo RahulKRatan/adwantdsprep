7168eb2041e8487c2e1722956fad37bc9c454fae
goutamadwant <adwant.goutam@gmail.com>
2019-10-08 01:29:21 -0700

    Create Search2DMatrix.java

---
+++ src/Bootcamp/Arrays/Search2DMatrix.java
@@ -0,0 +1,17 @@
+package Bootcamp.Arrays;
+
+public class Search2DMatrix {
+
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{{1,3,5,7},{10,11,16,20},{23,30,34,50}};
+        System.out.println(searchMatrix(matrix,3));
+    }
+
+
+}
