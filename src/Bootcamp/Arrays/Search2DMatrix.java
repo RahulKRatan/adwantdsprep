bfd819623bb190ea549a0e0e0076f1533d8a934a
goutamadwant <adwant.goutam@gmail.com>
2019-10-13 23:44:01 -0700

    Update Search2DMatrix.java

--- src/Bootcamp/Arrays/Search2DMatrix.java
+++ src/Bootcamp/Arrays/Search2DMatrix.java
@@ -1,9 +1,38 @@
 package Bootcamp.Arrays;
 
+/**
+Time complexity : O(log(mn)) since it's a standard binary search.
+Space complexity : O(1).
+
+ row = idx/n and col = idx % n.
+
+ */
 public class Search2DMatrix {
 
     public static boolean searchMatrix(int[][] matrix, int target) {
-        
+        if(matrix==null || matrix.length==0 || matrix[0].length==0)
+            return false;
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int start = 0;
+        int end = m*n-1;
+
+        while(start<=end){
+            int mid=(start+end)/2;
+            int midX=mid/n;  // row id
+            int midY=mid%n;  // column id
+
+            if(matrix[midX][midY]==target)
+                return true;
+
+            if(matrix[midX][midY]<target){
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
 
         return false;
     }
