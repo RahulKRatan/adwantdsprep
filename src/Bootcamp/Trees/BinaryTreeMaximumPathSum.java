5c29606daaef2f2f0f4cf39242dd1cdfdd4f08a2
goutamadwant <adwant.goutam@gmail.com>
2019-10-15 22:45:58 -0700

    Update BinaryTreeMaximumPathSum.java

--- src/Bootcamp/Trees/BinaryTreeMaximumPathSum.java
+++ src/Bootcamp/Trees/BinaryTreeMaximumPathSum.java
@@ -2,15 +2,17 @@ package Bootcamp.Trees;
 
 
 /**
- * For each node there can be four ways that the max path goes through the node:
- * 1. Node only
- * 2. Max path through Left Child + Node
- * 3. Max path through Right Child + Node
- * 4. Max path through Left Child + Node + Max path through Right Child
  *
- * The idea is to keep trace of four paths and pick up the max one in the end.
- * An important thing to note is, root of every subtree need to return maximum path sum such that at most one child of root is involved.
- * This is needed for parent function call. In below code, this sum is stored in ‘max_single’ and returned by the recursive function.
+ * Initiate max_sum as the smallest possible integer and call max_gain(node = root).
+ * Implement max_gain(node) with a check to continue the old path/to start a new path:
+ *      Base case : if node is null, the max gain is 0.
+ *      Call max_gain recursively for the node children to compute max gain from the left and right subtrees : left_gain = max(max_gain(node.left), 0) and
+ * right_gain = max(max_gain(node.right), 0).
+ *      Now check to continue the old path or to start a new path. To start a new path would cost price_newpath = node.val + left_gain + right_gain. Update max_sum if it's better to start a new path.
+ *      For the recursion return the max gain the node and one/zero of its subtrees could add to the current path : node.val + max(left_gain, right_gain).
+ *
+ * Time complexity : O(N) where N is number of nodes, since we visit each node not more than 2 times.
+ * Space complexity : O(log(N)). We have to keep a recursion stack of the size of the tree height, which is O(log(N)) for the binary tree.
  */
 public class BinaryTreeMaximumPathSum {
     TreeNode node;
@@ -28,18 +30,18 @@ public class BinaryTreeMaximumPathSum {
             return 0;
 
         // max sum on the left and right sub-trees of node
-        int left_gain = Math.max(findMaxUtil(node.left), 0);
-        int right_gain = Math.max(findMaxUtil(node.right), 0);
+        int left = Math.max(findMaxUtil(node.left), 0);
+        int right = Math.max(findMaxUtil(node.right), 0);
 
         // the price to start a new path where `node` is a highest node
-        int price_newpath = node.val + left_gain + right_gain;
+        int price_newpath = node.val + left + right;
 
         // update max_sum if it's better to start a new path
         res = Math.max(res, price_newpath);
 
         // for recursion :
         // return the max gain if continue the same path
-        return node.val + Math.max(left_gain, right_gain);
+        return node.val + Math.max(left, right);
     }
 
     public static void main(String[] args) {
