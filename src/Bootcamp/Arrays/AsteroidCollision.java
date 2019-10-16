f278c3a001d51d13eb7c74d04894a988e47476bf
goutamadwant <adwant.goutam@gmail.com>
2019-10-15 22:59:08 -0700

    Create AsteroidCollision.java

---
+++ src/Bootcamp/Arrays/AsteroidCollision.java
@@ -0,0 +1,16 @@
+package Bootcamp.Arrays;
+
+public class AsteroidCollision {
+
+    public static int[] asteroidCollision(int[] asteroids) {
+        
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] asteroids = new int[]{5, 10, -5};
+    }
+
+
+}
