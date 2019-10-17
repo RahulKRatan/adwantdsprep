efc8ae3059bff8107c4389a68667b3d7f1de162e
goutamadwant <adwant.goutam@gmail.com>
2019-10-17 00:12:46 -0700

    Update NQueens.java

--- src/Bootcamp/StringManipulation/Backtracking/NQueens.java
+++ src/Bootcamp/StringManipulation/Backtracking/NQueens.java
@@ -2,8 +2,9 @@ package Bootcamp.StringManipulation.Backtracking;
 
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+
 /**
  *
  * Given nxn board place n queen on this board so that they dont attack each other. One solution is to find
@@ -16,99 +17,55 @@ import java.util.List;
  */
 public class NQueens {
 
-    class Position {
-        int row, col;
-        Position(int row, int col) {
-            this.row = row;
-            this.col = col;
-        }
+    public static List<List<String>> solveNQueens(int n) {
+        char[][] board = new char[n][n];
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                board[i][j] = '.';
+        List<List<String>> res = new ArrayList<>();
+        dfs(board, 0, res);
+        return res;
     }
 
-    public Position[] solveNQueenOneSolution(int n) {
-        Position[] positions = new Position[n];
-        boolean hasSolution = solveNQueenOneSolutionUtil(n, 0, positions);
-        if (hasSolution) {
-            return positions;
-        } else {
-            return new Position[0];
-        }
-    }
-
-    private boolean solveNQueenOneSolutionUtil(int n, int row, Position[] positions) {
-        if (n == row) {
-            return true;
-        }
-        int col;
-        for (col = 0; col < n; col++) {
-            boolean foundSafe = true;
-
-            //check if this row and col is not under attack from any previous queen.
-            for (int queen = 0; queen < row; queen++) {
-                if (positions[queen].col == col || positions[queen].row - positions[queen].col == row - col ||
-                        positions[queen].row + positions[queen].col == row + col) {
-                    foundSafe = false;
-                    break;
-                }
-            }
-            if (foundSafe) {
-                positions[row] = new Position(row, col);
-                if (solveNQueenOneSolutionUtil(n, row + 1, positions)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    /*
-     *Solution for https://leetcode.com/problems/n-queens/
-     */
-    public List<List<String>> solveNQueens(int n) {
-        List<List<String>> result = new ArrayList<>();
-        Position[] positions = new Position[n];
-        solve(0, positions, result, n);
-        return result;
-    }
-
-    public void solve(int current, Position[] positions, List<List<String>> result, int n) {
-        if (n == current) {
-            StringBuffer buff = new StringBuffer();
-            List<String> oneResult = new ArrayList<>();
-            for (Position p : positions) {
-                for (int i = 0; i < n; i++) {
-                    if (p.col == i) {
-                        buff.append("Q");
-                    } else {
-                        buff.append(".");
-                    }
-                }
-                oneResult.add(buff.toString());
-                buff = new StringBuffer();
-
-            }
-            result.add(oneResult);
+    private static void dfs(char[][] board, int colIndex, List<List<String>> res) {
+        if(colIndex == board.length) {
+            res.add(construct(board));
             return;
         }
 
-        for (int i = 0; i < n; i++) {
-            boolean foundSafe = true;
-            for (int j = 0; j < current; j++) {
-                if (positions[j].col == i || positions[j].col - positions[j].row == i - current || positions[j].row + positions[j].col == i + current) {
-                    foundSafe = false;
-                    break;
-                }
-            }
-            if (foundSafe) {
-                positions[current] = new Position(current, i);
-                solve(current + 1, positions, result, n);
+        for(int i = 0; i < board.length; i++) {
+            if(validate(board, i, colIndex)) {
+                board[i][colIndex] = 'Q';
+                dfs(board, colIndex + 1, res);
+                board[i][colIndex] = '.';
             }
         }
     }
 
+    private static boolean validate(char[][] board, int x, int y) {
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < y; j++) {
+                if(board[i][j] == 'Q' && (x + j == y + i || x + y == i + j || x == i))
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static List<String> construct(char[][] board) {
+        List<String> res = new LinkedList<>();
+        for(int i = 0; i < board.length; i++) {
+            String s = new String(board[i]);
+            res.add(s);
+        }
+        return res;
+    }
+
+
     public static void main(String args[]) {
-        NQueens s = new NQueens();
-        Position[] positions = s.solveNQueenOneSolution(6);
-        Arrays.stream(positions).forEach(position -> System.out.println(position.row + " " + position.col));
+        List<List<String>> result = solveNQueens(4);
+        System.out.println(result);
     }
 
 
