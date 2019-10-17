98d926e07c4a433d93fba8789586ee5982625e33
goutamadwant <adwant.goutam@gmail.com>
2019-10-17 00:12:53 -0700

    Create ThreeSumClosest.java

---
+++ src/Bootcamp/StringManipulation/TwoSumVariations/ThreeSumClosest.java
@@ -0,0 +1,15 @@
+package Bootcamp.StringManipulation.TwoSumVariations;
+
+public class ThreeSumClosest {
+
+    public static int threeSumClosest(int[] nums, int target) {
+        int min = Integer.MAX_VALUE;
+        
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{-1,2,1,-4};
+        System.out.println(threeSumClosest(nums,1));
+    }
+}
