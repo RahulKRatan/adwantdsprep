6a0a0936d130826633a6f2de9ad7cb93c327f4bf
goutamadwant <adwant.goutam@gmail.com>
2019-10-18 00:52:20 -0700

    Update ShortestWordDistance.java

--- src/Bootcamp/StringManipulation/ShortestWordDistance.java
+++ src/Bootcamp/StringManipulation/ShortestWordDistance.java
@@ -1,4 +1,30 @@
 package Bootcamp.StringManipulation;
 
+
+/**
+ * O(N) and O(1)
+ */
 public class ShortestWordDistance {
+
+    public static int shortestDistance(String[] words, String word1, String word2) {
+        int i1 = -1, i2 = -1;
+        int minDistance = words.length;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(word1)) {
+                i1 = i;
+            } else if (words[i].equals(word2)) {
+                i2 = i;
+            }
+
+            if (i1 != -1 && i2 != -1) {
+                minDistance = Math.min(minDistance, Math.abs(i1 - i2));
+            }
+        }
+        return minDistance;
+    }
+
+    public static void main(String[] args) {
+        String[] strings = new String[]{"practice", "makes", "perfect", "coding", "makes"};
+        System.out.println(shortestDistance(strings,"makes","coding"));
+    }
 }
