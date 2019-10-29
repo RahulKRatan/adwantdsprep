fa947483e4be465398de8b383a697820a453d8ca
goutamadwant <adwant.goutam@gmail.com>
2019-10-28 22:57:45 -0700

    Update SwapkthNodesGivenLinkedList.java

--- src/Bootcamp/LinkedList/IK/SwapkthNodesGivenLinkedList.java
+++ src/Bootcamp/LinkedList/IK/SwapkthNodesGivenLinkedList.java
@@ -1,4 +1,22 @@
 package Bootcamp.LinkedList.IK;
 
+import Bootcamp.LinkedList.Node;
+
 public class SwapkthNodesGivenLinkedList {
+
+    public static Node swap_nodes(Node head, int k) {
+        
+
+        return null;
+
+    }
+
+    public static void main(String[] args) {
+        Node first = new Node(1);
+        first.next = new Node(2);
+        first.next.next = new Node(3);
+        first.next.next.next = new Node(4);
+        first.next.next.next.next = new Node(7);
+        first.next.next.next.next.next = new Node(0);
+    }
 }
