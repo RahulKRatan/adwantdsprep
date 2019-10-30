10638fc8e7d2f6a0113df4f18f92a566dbfde772
goutamadwant <adwant.goutam@gmail.com>
2019-10-29 22:31:13 -0700

    Update ZipGivenLinkedListFromEnds.java

--- src/Bootcamp/LinkedList/IK/ZipGivenLinkedListFromEnds.java
+++ src/Bootcamp/LinkedList/IK/ZipGivenLinkedListFromEnds.java
@@ -1,4 +1,20 @@
 package Bootcamp.LinkedList.IK;
 
+import Bootcamp.LinkedList.Node;
+
 public class ZipGivenLinkedListFromEnds {
+
+    public static Node zip_given_linked_list(Node head) {
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Node first = new Node(1);
+        first.next = new Node(2);
+        first.next.next = new Node(3);
+        first.next.next.next = new Node(4);
+        first.next.next.next.next = new Node(5);
+        first.next.next.next.next.next = new Node(6);
+    }
 }
