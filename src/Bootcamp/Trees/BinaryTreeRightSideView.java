d2e7f35452ff1015a9fba4f77c86112f0328874e
goutamadwant <adwant.goutam@gmail.com>
2019-11-05 00:14:08 -0800

    Update BinaryTreeRightSideView.java

--- src/Bootcamp/Trees/BinaryTreeRightSideView.java
+++ src/Bootcamp/Trees/BinaryTreeRightSideView.java
@@ -1,9 +1,16 @@
 package Bootcamp.Trees;
 
 public class BinaryTreeRightSideView {
-
+    TreeNode root;
 
     public static void main(String[] args) {
-        
+        BinaryTreeRightSideView tree = new BinaryTreeRightSideView();
+        tree.root = new TreeNode(1);
+        tree.root.left = new TreeNode(2);
+        tree.root.right = new TreeNode(3);
+        tree.root.left.left = new TreeNode(4);
+        tree.root.left.right = new TreeNode(5);
+        tree.root.right.right = new TreeNode(6);
+        tree.root.right.left = new TreeNode(7);
     }
 }
