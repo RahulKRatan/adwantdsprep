83eb1d5751b35bdf9dea5ea3afd8acfe8cee3fdd
goutamadwant <adwant.goutam@gmail.com>
2019-11-07 19:25:22 -0800

    Update ConstructBinaryTreefromPreorderAndInorderTraversal.java

--- src/Bootcamp/Trees/ConstructBinaryTreefromPreorderAndInorderTraversal.java
+++ src/Bootcamp/Trees/ConstructBinaryTreefromPreorderAndInorderTraversal.java
@@ -2,8 +2,15 @@ package Bootcamp.Trees;
 
 public class ConstructBinaryTreefromPreorderAndInorderTraversal {
 
+    public static TreeNode buildTree(int[] preorder, int[] inorder) {
+
+
+        return null;
+    }
 
     public static void main(String[] args) {
-        
+        int[] preorder = new int[]{3,9,20,15,7};
+        int[] inorder = new int[]{9,3,15,20,7};
+        TreeNode result = buildTree(preorder,inorder);
     }
 }
