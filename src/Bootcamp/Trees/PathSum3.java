e479ddc976cbea7827776b3d7037131e9b8c63ad
goutamadwant <adwant.goutam@gmail.com>
2019-11-09 15:56:20 -0800

    Update PathSum3.java

--- src/Bootcamp/Trees/PathSum3.java
+++ src/Bootcamp/Trees/PathSum3.java
@@ -9,7 +9,12 @@ If the node.val == sum that means the path from the original root to the current
 Since we aren't required to keep any information on the path itself, we simply return 1 representing a valid path.
 
 From a high level, pathSum() recursively visits each Node and pathSumFrom() recursively visits each child Node and
-totals their values and checks that against the sum. Every node will be checked including all possible paths downward. */
+totals their values and checks that against the sum. Every node will be checked including all possible paths downward.
+ Typical recursive DFS.
+
+Space: O(n) due to recursion.
+Time: O(n^2) in worst case (no branching); O(nlogn) in best case (balanced tree).
+*/
 
 public class PathSum3 {
     TreeNode node;
