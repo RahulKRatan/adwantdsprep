0ab67c2a07043690a92b84c6ea5df8eac22d3cec
goutamadwant <adwant.goutam@gmail.com>
2019-11-09 15:47:47 -0800

    Update PathSum2.java

--- src/Bootcamp/Trees/PathSum2.java
+++ src/Bootcamp/Trees/PathSum2.java
@@ -1,13 +1,13 @@
 package Bootcamp.Trees;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PathSum2 {
     TreeNode root;
     public static List<List<Integer>> pathSum(TreeNode root, int sum) {
-        List<List<Integer>> result  = new LinkedList<>();
-        List<Integer> num  = new LinkedList<>();
+        List<List<Integer>> result  = new ArrayList<>();
+        List<Integer> num  = new ArrayList<>();
         pathSum(root,sum,num,result);
         return result;
     }
@@ -17,15 +17,19 @@ public class PathSum2 {
 
         if (root == null)
             return;
-        num.add(new Integer(root.val));
+        num.add(root.val);
         if (root.left == null && root.right == null && sum == root.val) {
-            result.add(new LinkedList(num));
-            num.remove(num.size() - 1);//don't forget to remove the last integer
-            return;
+            // The thing is, you have to new a new list using values from currentResult then add it up to the nested list!!!!
+            // You would have nothing if you just add currentResult to the result because you just add a reference by doing this however we remove every last item along exiting the recursion,
+            // which means currentResult is always empty in the end.
+            result.add(new ArrayList(num));
         } else {
             pathSum(root.left, sum - root.val, num, result);
             pathSum(root.right, sum - root.val, num, result);
         }
+        //This is the backtracking point. If we execute two sub recursive code in else branch and still can't get a match pathSum,
+        // this means the current "root" which already added into the List currentResult can not lead us to the correct answer.
+        // So we need to remove it from List currentResult and try other possible branches, this is what backtracking means.
         num.remove(num.size() - 1);
     }
 
