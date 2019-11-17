716aa63f619da8770022af627fc4d3c421e70af4
goutamadwant <adwant.goutam@gmail.com>
2019-11-17 11:41:24 -0800

    Update IntegerToRoman.java

--- src/Bootcamp/Arrays/IntegerToRoman.java
+++ src/Bootcamp/Arrays/IntegerToRoman.java
@@ -3,14 +3,22 @@ package Bootcamp.Arrays;
 public class IntegerToRoman {
 
     public static String intToRoman(int number) {
+        if (number < 1 || number > 3999) return "";
+
+        // roman and value pair are important
         int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        String[] numerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        String[] roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < values.length; i++) {
-            while (number >= values[i]) {
+        int i = 0;
+        //iterate until the number becomes zero, NO NEED to go until the last element in roman array
+        while (number >0) {
+            while ( number >= values[i]) {
                 number -= values[i];
-                result.append(numerals[i]);
+                result.append(roman[i]);
             }
+            i++;
         }
         return result.toString();
     }
