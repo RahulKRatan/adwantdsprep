a5109b0282c23678d1ba20d7955c97b8e6ecb822
goutamadwant <adwant.goutam@gmail.com>
2019-11-17 11:13:21 -0800

    Update BalancedBinaryTree.java

--- src/Bootcamp/Trees/BalancedBinaryTree.java
+++ src/Bootcamp/Trees/BalancedBinaryTree.java
@@ -1,4 +1,18 @@
 package Bootcamp.Trees;
 
 public class BalancedBinaryTree {
+    TreeNode node;
+    public boolean isBalanced(TreeNode root) {
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        BalancedBinaryTree tree = new BalancedBinaryTree();
+        tree.node = new TreeNode(3);
+        tree.node.left = new TreeNode(9);
+        tree.node.right = new TreeNode(20);
+        tree.node.right.left = new TreeNode(15);
+        tree.node.right.right = new TreeNode(7);
+    }
 }
