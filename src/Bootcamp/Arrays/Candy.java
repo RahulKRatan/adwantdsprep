f309f2c37663d6f96a6c0928e08dbd92f50f0d30
goutamadwant <adwant.goutam@gmail.com>
2019-11-18 00:44:02 -0800

    Create Candy.java

---
+++ src/Bootcamp/Arrays/Candy.java
@@ -0,0 +1,14 @@
+package Bootcamp.Arrays;
+
+public class Candy {
+
+    public int candy(int[] ratings) {
+
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] ratings = new int[]{1,2,2};
+    }
+}
