acccb185ff350c979ca377c7e65c13e08b622679
goutamadwant <adwant.goutam@gmail.com>
2019-11-18 00:44:17 -0800

    Update MoveZeroes.java

--- src/Bootcamp/Arrays/MoveZeroes.java
+++ src/Bootcamp/Arrays/MoveZeroes.java
@@ -1,5 +1,8 @@
 package Bootcamp.Arrays;
 
+/**
+ * Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+ */
 public class MoveZeroes {
 
     public static void moveZeroes(int[] nums) {
