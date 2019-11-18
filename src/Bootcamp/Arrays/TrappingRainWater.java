8b2a0ae5c083ab74b1ce8a6f806ee2c3aa508a2b
goutamadwant <adwant.goutam@gmail.com>
2019-11-18 01:10:26 -0800

    Update TrappingRainWater.java

--- src/Bootcamp/Arrays/TrappingRainWater.java
+++ src/Bootcamp/Arrays/TrappingRainWater.java
@@ -6,10 +6,12 @@ package Bootcamp.Arrays;
  *
  * Algorithm:
  * 1: With given towerHeight array, create 2 arrays, maxSeenRight and maxSeenLeft.
- *    maxSeenLeft[i]  - max height on left side of Tower[i].
- *    maxSeenRight[i] - max height on right side of Tower[i].
+ *    left[i]  - max height on left side of Tower[i].
+ *    right[i] - max height on right side of Tower[i].
  * 2: Calculate for each tower:
- *    rainwater = rainwater + Max(Min(maxSeenLeft[i], maxSeenRight[i]) - towerHeight[i], 0);
+ *    rainwater = rainwater + Max(Min(left[i], right[i]) - height[i], 0);
+ *
+ *    Refer image in files
  */
 public class TrappingRainWater {
 
@@ -20,28 +22,44 @@ public class TrappingRainWater {
 
     public static int trap(int[] height) {
 
-        int maxSeenSoFar = 0;
-        int[] maxSeenRight = new int[height.length];
-        int maxSeenLeft = 0;
-        int rainwater = 0;
+        int result = 0;
 
-        for (int i = height.length - 1; i >= 0; i--) {
-            if (height[i] > maxSeenSoFar) {
-                maxSeenSoFar = height[i];
-                maxSeenRight[i] = maxSeenSoFar;
-            } else {
-                maxSeenRight[i] = maxSeenSoFar;
+        if(height==null || height.length<=2)
+            return result;
+
+        int left[] = new int[height.length];
+        int right[]= new int[height.length];
+
+        //scan from left to right
+        int max = height[0];
+        left[0] = height[0];
+        for(int i=1; i<height.length; i++){
+            if(height[i]<max){
+                left[i]=max;
+            }else{
+                left[i]=height[i];
+                max = height[i];
             }
         }
 
-        for (int i = 0; i < height.length; i++) {
-            rainwater = rainwater + Integer.max(Integer.min(maxSeenLeft, maxSeenRight[i]) - height[i], 0);
-            if (height[i] > maxSeenLeft) {
-                maxSeenLeft = height[i];
+        //scan from right to left
+        max = height[height.length-1];
+        right[height.length-1]=height[height.length-1];
+        for(int i=height.length-2; i>=0; i--){
+            if(height[i]<max){
+                right[i]=max;
+            }else{
+                right[i]=height[i];
+                max = height[i];
             }
         }
 
-        return rainwater;
+        //calculate totoal
+        for(int i=0; i<height.length; i++){
+            result+= Math.min(left[i],right[i])-height[i];
+        }
+
+        return result;
     }
 
 
