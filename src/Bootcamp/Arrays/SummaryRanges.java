82f95f45da674d82bd1739d6229b892b52c4ce61
goutamadwant <adwant.goutam@gmail.com>
2019-11-20 09:46:04 -0800

    Create SummaryRanges.java

---
+++ src/Bootcamp/Arrays/SummaryRanges.java
@@ -0,0 +1,30 @@
+package Bootcamp.Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SummaryRanges {
+
+    public static List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>();
+        int i = 0;
+        for(int j = 0; j< nums.length; j++){
+            if(j+1 < nums.length && nums[j]+1 == nums[j+1]){
+                continue;
+            }
+            if(i==j){
+                result.add(nums[i]+"");
+            }else {
+                result.add(nums[i]+"->"+nums[j]);
+            }
+            i = j+1;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{0,1,2,4,5,7};
+        System.out.println(summaryRanges(nums));
+    }
+}
