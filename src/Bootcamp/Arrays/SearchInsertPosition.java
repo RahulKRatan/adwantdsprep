1135fc4f70188ea92ac75fafc7d1061cce4c7757
goutamadwant <adwant.goutam@gmail.com>
2019-11-26 00:37:36 -0800

    Create SearchInsertPosition.java

---
+++ src/Bootcamp/Arrays/SearchInsertPosition.java
@@ -0,0 +1,20 @@
+package Bootcamp.Arrays;
+
+public class SearchInsertPosition {
+
+    public static int searchInsert(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] == target){
+                return i;
+            } else if(nums[i] > target){
+                return i;
+            }
+        }
+        return nums.length;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,3,5,6};
+        System.out.println(searchInsert(nums,2));
+    }
+}
