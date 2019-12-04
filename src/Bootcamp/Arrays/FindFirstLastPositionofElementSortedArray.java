8a02ef0f490eefe24f65df67b700298cba180a88
goutamadwant <adwant.goutam@gmail.com>
2019-12-03 22:33:52 -0800

    Update FindFirstLastPositionofElementSortedArray.java

--- src/Bootcamp/Arrays/FindFirstLastPositionofElementSortedArray.java
+++ src/Bootcamp/Arrays/FindFirstLastPositionofElementSortedArray.java
@@ -1,9 +1,9 @@
 package Bootcamp.Arrays;
 
 /**
- Time complexity : O(log{10}(n)
+ Time complexity : log(n)
 
- Space complexity : O(1)O(1)
+ Space complexity : O(1)
 
  All work is done in place, so the overall memory usage is constant.
  */
@@ -15,11 +15,11 @@ public class FindFirstLastPositionofElementSortedArray {
         int end = findPosition(nums, target, true);// to check if this is the last index we are looking for
         return new int[]{start, end};
     }
-    private static int findPosition(int[] A, int target, boolean isLast) {
+    private static int findPosition(int[] A, int target, boolean right) {
         int start = 0, end = A.length-1, index = -1;
         while (start <= end) {
             int mid = (start + end)/2;
-            if(isLast){
+            if(right){
                 if (A[mid] <= target) start = mid + 1; // = is important as it goes till last element
                 else end = mid-1;
             } else{
@@ -32,8 +32,8 @@ public class FindFirstLastPositionofElementSortedArray {
     }
 
     public static void main(String[] args) {
-        int[] nums = new int[]{1,1,1,1,1};
-        int[] result = searchRange(nums,1);
+        int[] nums = new int[]{5,7,7,8,8,10};
+        int[] result = searchRange(nums,8);
         for (int res: result) {
             System.out.println(res);
         }
