228760c7c41b169f36bae9efe85a9d97f6854ae8
goutamadwant <adwant.goutam@gmail.com>
2019-12-11 00:33:02 -0800

    Create TaskScheduler.java

---
+++ src/Bootcamp/Arrays/TaskScheduler.java
@@ -0,0 +1,14 @@
+package Bootcamp.Arrays;
+
+public class TaskScheduler {
+
+    public int leastInterval(char[] tasks, int n) {
+        
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
