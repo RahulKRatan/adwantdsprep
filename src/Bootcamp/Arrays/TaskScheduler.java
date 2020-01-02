66493b316950c547d85678ed7f36da3ebb294d91
goutamadwant <adwant.goutam@gmail.com>
2020-01-01 23:35:51 -0800

    Update TaskScheduler.java

--- src/Bootcamp/Arrays/TaskScheduler.java
+++ src/Bootcamp/Arrays/TaskScheduler.java
@@ -1,14 +1,45 @@
 package Bootcamp.Arrays;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+
 public class TaskScheduler {
 
-    public int leastInterval(char[] tasks, int n) {
-        
-
-        return 0;
+    public static int leastInterval(char[] tasks, int n) {
+        int[] map = new int[26];
+        for (char c: tasks)
+            map[c - 'A']++;
+        PriorityQueue< Integer > queue = new PriorityQueue < > (26, Collections.reverseOrder());
+        for (int f: map) {
+            if (f > 0)
+                queue.add(f);
+        }
+        int time = 0;
+        while (!queue.isEmpty()) {
+            int i = 0;
+            List< Integer > temp = new ArrayList< >();
+            while (i <= n) {
+                if (!queue.isEmpty()) {
+                    if (queue.peek() > 1)
+                        temp.add(queue.poll() - 1);
+                    else
+                        queue.poll();
+                }
+                time++;
+                if (queue.isEmpty() && temp.size() == 0)
+                    break;
+                i++;
+            }
+            for (int l: temp)
+                queue.add(l);
+        }
+        return time;
     }
 
     public static void main(String[] args) {
-
+        char[] tasks = new char[]{'A','A','A','B','B','B'};
+        System.out.println(leastInterval(tasks,2));
     }
 }
