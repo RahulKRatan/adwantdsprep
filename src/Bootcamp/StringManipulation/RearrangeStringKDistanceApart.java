930a424232cf92c1f63102c7762b1635871750cc
goutamadwant <adwant.goutam@gmail.com>
2020-01-20 22:33:20 -0800

    Update RearrangeStringKDistanceApart.java

--- src/Bootcamp/StringManipulation/RearrangeStringKDistanceApart.java
+++ src/Bootcamp/StringManipulation/RearrangeStringKDistanceApart.java
@@ -58,6 +58,6 @@ public class RearrangeStringKDistanceApart {
     }
 
     public static void main(String[] args) {
-        System.out.println(rearrangeString("aaadbbcc",2));
+        System.out.println(rearrangeString("aaabc",3));
     }
 }
