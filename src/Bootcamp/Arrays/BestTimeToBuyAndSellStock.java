d3caa85d2674b7e958717a2ddadf2ac6585cdc28
goutamadwant <adwant.goutam@gmail.com>
2020-01-26 18:59:16 -0800

    Update BestTimeToBuyAndSellStock.java

--- src/Bootcamp/Arrays/BestTimeToBuyAndSellStock.java
+++ src/Bootcamp/Arrays/BestTimeToBuyAndSellStock.java
@@ -2,12 +2,14 @@ package Bootcamp.Arrays;
 
 public class BestTimeToBuyAndSellStock {
     public int maxProfit(int[] prices) {
-
-
-        return 0;
-    }
-
-    public static void main(String[] args) {
-
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
+        }
+        return maxprofit;
     }
 }
