dfaeabc841adbc933fa7ff12e0e0697f6508a366
goutamadwant <adwant.goutam@gmail.com>
2020-01-28 00:06:45 -0800

    Update SumOfTwoIntegers.java

--- src/Bootcamp/Arrays/BitManipuation/SumOfTwoIntegers.java
+++ src/Bootcamp/Arrays/BitManipuation/SumOfTwoIntegers.java
@@ -11,7 +11,8 @@ package Bootcamp.Arrays.BitManipuation;
  *      ____
  *      1(1)01
  * Here we will forward the carry at the second bit to get the result.
- * So which bitwise operator can do this ? A simple observation says that XOR can do that,but it just falls short in dealing with the carry properly, but correctly adds when there is no need to deal with carry.
+ * So which bitwise operator can do this ? A simple observation says that XOR can do that,
+ * but it just falls short in dealing with the carry properly, but correctly adds when there is no need to deal with carry.
  * For Eg:
  *
  * 1   =>  001
@@ -23,7 +24,8 @@ package Bootcamp.Arrays.BitManipuation;
  * 2  => 010
  * 3^2=> 001
  * Here we can see XOR just fell short with the carry generated at the second bit.
- * So how can we find the carry ? The carry is generated when both the bits are set, i.e (1,1) will generate carry but (0,1 or 1,0 or 0,0) won't generate a carry, so which bitwise operator can do that ? AND gate ofcourse.
+ * So how can we find the carry ? The carry is generated when both the bits are set,
+ * i.e (1,1) will generate carry but (0,1 or 1,0 or 0,0) won't generate a carry, so which bitwise operator can do that ? AND gate ofcourse.
  *
  * To find the carry we can do
  *
