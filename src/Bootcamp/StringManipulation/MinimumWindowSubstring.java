9a2247896b4da03aba896885ca9fdce5e26b394b
goutamadwant <adwant.goutam@gmail.com>
2020-02-01 21:55:58 -0800

    Update MinimumWindowSubstring.java

--- src/Bootcamp/StringManipulation/MinimumWindowSubstring.java
+++ src/Bootcamp/StringManipulation/MinimumWindowSubstring.java
@@ -1,56 +1,35 @@
 package Bootcamp.StringManipulation;
 
-import java.util.HashMap;
-
 public class MinimumWindowSubstring {
 
     public static String minWindow(String s, String t) {
-        HashMap<Character, Integer> goal = new HashMap<>();
-        int goalSize = t.length();
-        int minLen = Integer.MAX_VALUE;
-        String result = "";
-
-        //target dictionary
-        for(int k=0; k<t.length(); k++){
-            goal.put(t.charAt(k), goal.getOrDefault(t.charAt(k), 0)+1);
+        if (t.length() > s.length()) return "";
+        int counter = 0;
+        // Array to save the character of the target substring  <Character, Frequency>
+        int[] map = new int[256];
+        for (char c : t.toCharArray()) {
+            if (map[c]++ == 0)
+                counter++;
         }
 
-        int i=0;
-        int total=0;
-        HashMap<Character, Integer> map = new HashMap<>();
-        for(int j=0; j<s.length(); j++){
-            char c = s.charAt(j);
-            if(!goal.containsKey(c)){
-                continue;
-            }
+        int begin = 0, end = 0, head = 0;
+        int len = Integer.MAX_VALUE;
 
-            //if c is a target character in the goal, and count is < goal, increase the total
-            int count = map.getOrDefault(c, 0);
-            if(count<goal.get(c)){
-                total++;
-            }
+        while(end < s.length()) {
+            char c = s.charAt(end++);
+            if (--map[c] == 0)  counter--;
 
-            map.put(c, count+1);
-
-            //when total reaches the goal, trim from left until no more chars can be trimmed.
-            if(total==goalSize){
-                while(!goal.containsKey(s.charAt(i)) || map.get(s.charAt(i))>goal.get(s.charAt(i))){
-                    char pc = s.charAt(i);
-                    if(goal.containsKey(pc) && map.get(pc)>goal.get(pc)){
-                        map.put(pc, map.get(pc)-1);
-                    }
-
-                    i++;
+            while (counter == 0) {
+                char tmp = s.charAt(begin);
+                if (++map[tmp] > 0)  counter++;
+                if (end - begin < len) {
+                    len = end - begin;
+                    head = begin;
                 }
-
-                if(minLen>j-i+1){
-                    minLen = j-i+1;
-                    result = s.substring(i, j+1);
-                }
+                begin++;
             }
         }
-
-        return result;
+        return (len == Integer.MAX_VALUE)? "" : s.substring(head, head + len);
     }
     public static void main(String[] args) {
         String s = "ADOBECODEBANC";
