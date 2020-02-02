ddaafdfc2c9d3a249af626fa4420e0267b05ed4c
goutamadwant <adwant.goutam@gmail.com>
2020-02-01 21:58:36 -0800

    Update MinimumWindowSubstring.java

--- src/Bootcamp/StringManipulation/MinimumWindowSubstring.java
+++ src/Bootcamp/StringManipulation/MinimumWindowSubstring.java
@@ -1,45 +1,12 @@
 package Bootcamp.StringManipulation;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MinimumWindowSubstring {
 
     public static String minWindow(String s, String t) {
-        if (t.length() > s.length()) return "";
-        int counter = 0;
-        // Array to save the character of the target substring  <Character, Frequency>
-        int[] map = new int[256];
-        for (char c : t.toCharArray()) {
-            if (map[c]++ == 0)
-                counter++;
-        }
-
-        int begin = 0, end = 0, head = 0;
-        int len = Integer.MAX_VALUE;
-
-        while(end < s.length()) {
-            char c = s.charAt(end++);
-            if (--map[c] == 0)  counter--;
-
-            while (counter == 0) {
-                char tmp = s.charAt(begin);
-                if (++map[tmp] > 0)  counter++;
-                if (end - begin < len) {
-                    len = end - begin;
-                    head = begin;
-                }
-                begin++;
-            }
-        }
-        return (len == Integer.MAX_VALUE)? "" : s.substring(head, head + len);
-    }
-    public static void main(String[] args) {
-        String s = "ADOBECODEBANC";
-        String t = "ABC";
-        System.out.println(minWindow(s,t));
-    }
-}
-
-/*
-  if(t.length()> s.length()) return "";
+        if(t.length()> s.length()) return "";
         Map<Character, Integer> map = new HashMap<>();
         for(char c : t.toCharArray()){
             map.put(c, map.getOrDefault(c,0) + 1);
@@ -76,4 +43,10 @@ public class MinimumWindowSubstring {
         }
         if(len == Integer.MAX_VALUE) return "";
         return s.substring(head, head+len);
- */
+    }
+    public static void main(String[] args) {
+        String s = "ADOBECODEBANC";
+        String t = "ABC";
+        System.out.println(minWindow(s,t));
+    }
+}
