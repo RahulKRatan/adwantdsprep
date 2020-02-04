7e9accff9014b9f1f46bf672bf28822b5f31a307
goutamadwant <adwant.goutam@gmail.com>
2020-02-04 00:18:15 -0800

    Update ReverseLinkedList.java

--- src/Bootcamp/LinkedList/ReverseLinkedList.java
+++ src/Bootcamp/LinkedList/ReverseLinkedList.java
@@ -13,21 +13,25 @@ public class ReverseLinkedList {
         return prev;
     }
 /*
-Recursive
-public Node reverseList(Node head) {
-    if(head==null || head.next == null)
-        return head;
+We always put a node's previous node as one's next,
+Take 1 -> 2 -> 3 -> N for example, we reverse the list by
+put 1's previous node null as 1's next,
+put 2's previous node 1 as 2's next,
+put 3's previous node 2 as 3's next,
+return 3 // put null's previous node 3 as null's next
 
-    //get second node
-    ListNode second = head.next;
-    //set first's next to be null
-    head.next = null;
+    public ListNode reverseList(ListNode head) {
+        return putPreAfterNode(head, null);
+    }
 
-    ListNode rest = reverseList(second);
-    second.next = head;
-
-    return rest;
-}
+    private ListNode putPreAfterNode(ListNode node, ListNode pre) {
+        if (node == null) {
+            return pre;
+        }
+        ListNode next = node.next;
+        node.next = pre;
+        return putPreAfterNode(next, node);
+    }
  */
 
 
