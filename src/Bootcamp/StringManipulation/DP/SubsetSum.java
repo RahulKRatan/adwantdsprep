8473314be1e634004b080e854d3471f9dbd559fd
goutamadwant <adwant.goutam@gmail.com>
2020-02-10 23:47:11 -0800

    Update SubsetSum.java

--- src/Bootcamp/StringManipulation/DP/SubsetSum.java
+++ src/Bootcamp/StringManipulation/DP/SubsetSum.java
@@ -26,10 +26,33 @@ public class SubsetSum {
 
     }
 
+    public boolean isSubsetSum(int set[],
+                               int n, int sum)
+    {
+        // Base Cases
+        if (sum == 0)
+            return true;
+        if (n == 0 && sum != 0)
+            return false;
+
+        // If last element is greater than
+        // sum, then ignore it
+        if (set[n-1] > sum)
+            return isSubsetSum(set, n-1, sum);
+
+        /* else, check if sum can be obtained
+        by any of the following
+            (a) including the last element
+            (b) excluding the last element */
+        return isSubsetSum(set, n-1, sum) ||
+                isSubsetSum(set, n-1, sum-set[n-1]);
+    }
+
     public static void main(String args[]) {
         SubsetSum ss = new SubsetSum();
         int arr1[] = {2, 3, 7, 8};
-        System.out.print(ss.subsetSum(arr1, 11));
+        System.out.println(ss.subsetSum(arr1, 11));
+        System.out.println(ss.isSubsetSum(arr1,arr1.length,11));
 
     }
 }
