d759adbfba20954e283e649f1966afd87f7a282a
goutamadwant <adwant.goutam@gmail.com>
2020-02-16 13:21:47 -0800

    Create KnapsackProblem.java

---
+++ src/Bootcamp/StringManipulation/DP/KnapsackProblem.java
@@ -0,0 +1,19 @@
+package Bootcamp.StringManipulation.DP;
+
+public class KnapsackProblem {
+
+    static int knapSack(int W, int wt[], int val[], int n)
+    {
+      return 0;
+    }
+
+    public static void main(String args[])
+    {
+        int val[] = new int[]{60, 100, 120};
+        int wt[] = new int[]{10, 20, 30};
+        int  W = 50;
+        int n = val.length;
+        System.out.println(knapSack(W, wt, val, n));
+    }
+
+}
