0c1a0de4698ee596ac64e8a4deccd9f8c73be335
goutamadwant <adwant.goutam@gmail.com>
2020-02-16 13:22:12 -0800

    Update WordBreak2.java

--- src/Bootcamp/StringManipulation/DP/WordBreak2.java
+++ src/Bootcamp/StringManipulation/DP/WordBreak2.java
@@ -1,33 +1,45 @@
 package Bootcamp.StringManipulation.DP;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class WordBreak2 {
 
     /*
-    recursive solution
+    recursive solution with memoization
      */
 
-    public List<String> wordBreak(String s, Set<String> wordDict) {
-        return word_Break(s, wordDict, 0);
+    public static List<String> wordBreak(String s, List<String> wordDict) {
+        return DFS(s, new HashSet<>(wordDict), new HashMap<>());
     }
-    public List<String> word_Break(String s, Set<String> wordDict, int start) {
-        LinkedList<String> res = new LinkedList<>();
-        if (start == s.length()) {
+    // DFS function returns an array including all substrings derived from s.
+    static List<String> DFS(String s, Set<String> wordDict, HashMap<String, LinkedList<String>> map) {
+        if (map.containsKey(s))
+            return map.get(s);
+
+        LinkedList<String>res = new LinkedList<>();
+        if (s.length() == 0) {
             res.add("");
+            return res;
         }
-        for (int end = start + 1; end <= s.length(); end++) {
-            if (wordDict.contains(s.substring(start, end))) {
-                List<String> list = word_Break(s, wordDict, end);
-                for (String l : list) {
-                    res.add(s.substring(start, end) + (l.equals("") ? "" : " ") + l);
-                }
+        for (String word : wordDict) {
+            if (s.startsWith(word)) {
+                List<String>sublist = DFS(s.substring(word.length()), wordDict, map);
+                for (String sub : sublist)
+                    res.add(word + (sub.isEmpty() ? "" : " ") + sub);
             }
         }
+        map.put(s, res);
         return res;
     }
 
+    public static void main(String[] args) {
+        List<String> wordDict = new ArrayList<>();
+        wordDict.add("cat");
+        wordDict.add("cats");
+        wordDict.add("and");
+        wordDict.add("sand");
+        wordDict.add("dog");
+        System.out.println(wordBreak("catsanddog", wordDict));
+    }
 
 }
