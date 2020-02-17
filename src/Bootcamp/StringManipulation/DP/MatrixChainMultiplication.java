91cf8b4fec79e86dde866f884b2a29c0e1f7f01e
goutamadwant <adwant.goutam@gmail.com>
2020-02-16 23:04:19 -0800

    Update MatrixChainMultiplication.java

--- src/Bootcamp/StringManipulation/DP/MatrixChainMultiplication.java
+++ src/Bootcamp/StringManipulation/DP/MatrixChainMultiplication.java
@@ -7,7 +7,9 @@ public class MatrixChainMultiplication {
     Recursive solution
      */
     public static int MatrixChainOrder(int p[], int i, int j)
-    { if (i == j)
+    {
+
+        if (i == j)
         return 0;
 
         int min = Integer.MAX_VALUE;
