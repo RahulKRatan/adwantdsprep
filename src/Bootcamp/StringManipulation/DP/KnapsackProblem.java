ccb885b2b59972f5e2f6d7e68f260e434285cd1a
goutamadwant <adwant.goutam@gmail.com>
2020-02-17 20:37:51 -0800

    Update KnapsackProblem.java

--- src/Bootcamp/StringManipulation/DP/KnapsackProblem.java
+++ src/Bootcamp/StringManipulation/DP/KnapsackProblem.java
@@ -38,6 +38,32 @@ Recursion tree for Knapsack capacity 2 units and 3 items of 1 unit weight.
         else return Integer.max( val[n-1] + knapSack(W-wt[n-1], wt, val, n-1), knapSack(W, wt, val, n-1));
     }
 
+    // Returns the maximum value that can be put in a knapsack of capacity W
+    /*
+    
+     */
+    static int knapSackDP(int W, int wt[], int val[], int n)
+    {
+        int i, w;
+        int K[][] = new int[n+1][W+1];
+
+        // Build table K[][] in bottom up manner
+        for (i = 0; i <= n; i++)
+        {
+            for (w = 0; w <= W; w++)
+            {
+                if (i==0 || w==0)
+                    K[i][w] = 0;
+                else if (wt[i-1] <= w)
+                    K[i][w] = Integer.max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w]);
+                else
+                    K[i][w] = K[i-1][w];
+            }
+        }
+
+        return K[n][W];
+    }
+
     public static void main(String args[])
     {
         int val[] = new int[]{60, 100, 120};
