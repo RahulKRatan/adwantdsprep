0f0b593e0da0ebdfcbfd9d1338368624ff8495aa
goutamadwant <adwant.goutam@gmail.com>
2020-03-01 09:36:05 -0800

    Create BasicCalculator.java

---
+++ src/Bootcamp/StringManipulation/BasicCalculator.java
@@ -0,0 +1,14 @@
+package Bootcamp.StringManipulation;
+
+public class BasicCalculator {
+
+    public int calculate(String s) {
+
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
