9d59b44654f7eb4218b07d9ee8d80debbc6c68a4
goutamadwant <adwant.goutam@gmail.com>
2020-03-01 09:35:56 -0800

    Create ConsecutiveNumbersSum.java

---
+++ src/Bootcamp/Arrays/ConsecutiveNumbersSum.java
@@ -0,0 +1,14 @@
+package Bootcamp.Arrays;
+
+public class ConsecutiveNumbersSum {
+
+    public int consecutiveNumbersSum(int N) {
+
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
