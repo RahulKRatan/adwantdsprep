f99bf78ef83a2b3b9fb1fe3afac1cbfe86d92406
goutamadwant <adwant.goutam@gmail.com>
2020-03-21 11:39:11 -0500

    Update ConsecutiveNumbersSum.java

--- src/Bootcamp/Arrays/ConsecutiveNumbersSum.java
+++ src/Bootcamp/Arrays/ConsecutiveNumbersSum.java
@@ -2,13 +2,13 @@ package Bootcamp.Arrays;
 
 public class ConsecutiveNumbersSum {
 
-    public int consecutiveNumbersSum(int N) {
+    public static int consecutiveNumbersSum(int N) {
 
 
         return 0;
     }
 
     public static void main(String[] args) {
-
+        System.out.println(consecutiveNumbersSum(15));
     }
 }
