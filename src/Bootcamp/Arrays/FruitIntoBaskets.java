0f8ae55bb48e36415ef7f1d140679f0c376e4478
goutamadwant <adwant.goutam@gmail.com>
2020-06-20 12:13:13 -0700

    Update FruitIntoBaskets.java

--- src/Bootcamp/Arrays/FruitIntoBaskets.java
+++ src/Bootcamp/Arrays/FruitIntoBaskets.java
@@ -2,7 +2,14 @@ package Bootcamp.Arrays;
 
 public class FruitIntoBaskets {
 
+    public static int totalFruit(int[] tree) {
+
+
+        return 0;
+    }
+
+
     public static void main(String[] args) {
-        
+
     }
 }
