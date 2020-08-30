e81d0f2a7b24496fa68db690fa55f5b4a3e808fa
goutamadwant <adwant.goutam@gmail.com>
2020-08-30 13:01:33 -0700

    Update KClosestPointsOrigin.java

--- src/InterviewPractice/Arrays/KClosestPointsOrigin.java
+++ src/InterviewPractice/Arrays/KClosestPointsOrigin.java
@@ -7,22 +7,30 @@ import java.util.*;
 /*
 The advantage of this solution is it can deal with real-time(online) stream data. It does not have to know the size of the data previously.
 The disadvatage of this solution is it is not the most efficient solution.
+
+I had the exact same trouble in getting this understood. However, what helped me is the fact that it's the distance of each point from the origin that matters and not the relative distance between the points themselves!
+e.g.: Let's say the two points are (2, 2) and (3, 3). We can deduce that (3, 3) is the farthest from the origin as below:
+
+d1^2 = (2 - 0)^2 + (2 - 0)^2 = 2^2 + 2^2
+d2^2 = (3 - 0)^2 + (3 - 0)^2 = 3^2 + 3^2
+
+Now, deducing which among d1 and d2 is farthest from the origin is a trivial matter of computing d1^2 - d2^2 which is exactly what the comparator does. Hope this explanation helps!
+
+
+
  */
 public class KClosestPointsOrigin {
     public static int[][] kClosest(int[][] points, int K) {
-//        Arrays.sort(points,(p1,p2)->p1[0] * p1[0] + p1[1] * p1[1] - p2[0] * p2[0] - p2[1] * p2[1]);
-//        return Arrays.copyOfRange(points, 0, K);
-        // max-heap
-        PriorityQueue<int[]> pq = new PriorityQueue<>((p1, p2) -> p2[0] * p2[0] + p2[1] * p2[1] - p1[0] * p1[0] - p1[1] * p1[1]);
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((p1, p2) -> p2[0] * p2[0] + p2[1] * p2[1] - p1[0] * p1[0] - p1[1] * p1[1]);
         for (int[] p : points) {
-            pq.offer(p);
-            if (pq.size() > K) {
-                pq.poll();
+            maxHeap.offer(p);
+            if (maxHeap.size() > K) {
+                maxHeap.poll();
             }
         }
         int[][] res = new int[K][2];
         while (K > 0) {
-            res[--K] = pq.poll();
+            res[--K] = maxHeap.poll();
         }
         return res;
     }
