4772ea963d83ec3ea03fd06fd1d55872ed03fe01
goutamadwant <adwant.goutam@gmail.com>
2020-08-30 13:32:41 -0700

    Update MeetingRooms.java

--- src/InterviewPractice/Arrays/Interval/MeetingRooms.java
+++ src/InterviewPractice/Arrays/Interval/MeetingRooms.java
@@ -5,7 +5,25 @@ import java.util.Comparator;
 
 public class MeetingRooms {
 
+    /**
+     * The idea here is to sort the meetings by starting time. Then, go through the meetings one by one and make sure that each meeting ends before the next one starts.
+     * @param intervals
+     * @return
+     *
+     * Time complexity : O(nlogn). The time complexity is dominated by sorting. Once the array has been sorted, only O(n) time is taken to go through the array and determine if there is any overlap.
+     *
+     * Space complexity : O(1). Since no additional space is allocated.
+     *
+     */
     public static boolean canAttendMeetings(int[][] intervals) {
+        Comparator<int[]> c=(int[] a, int[] b) -> (a[0]-b[0]);
+        Arrays.sort(intervals, c);
+        for(int i=0; i<intervals.length-1; i++)
+            if(intervals[i][1]>intervals[i+1][0]) return false;
+        return true;
+}
+
+    public static boolean canAttendMeetings2(int[][] intervals) {
         int[] left = new int[intervals.length];
         int[] right = new int[intervals.length];
 
@@ -25,25 +43,6 @@ public class MeetingRooms {
 
         return true;
     }
-
-    /**
-     * The idea here is to sort the meetings by starting time. Then, go through the meetings one by one and make sure that each meeting ends before the next one starts.
-     * @param intervals
-     * @return
-     *
-     * Time complexity : O(nlogn). The time complexity is dominated by sorting. Once the array has been sorted, only O(n) time is taken to go through the array and determine if there is any overlap.
-     *
-     * Space complexity : O(1). Since no additional space is allocated.
-     *
-     */
-    public static boolean canAttendMeetings2(int[][] intervals) {
-        Comparator<int[]> c=(int[] a, int[] b) -> (a[0]-b[0]);
-        Arrays.sort(intervals, c);
-        for(int i=0; i<intervals.length-1; i++)
-            if(intervals[i][1]>intervals[i+1][0]) return false;
-        return true;
-}
-
 
     public static void main(String[] args) {
         int[][] ints = new int[][]{{0,30},{5,10},{15,20}};
