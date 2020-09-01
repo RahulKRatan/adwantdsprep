5cd80fce0b58dc3c5101712a54e63103cf287798
goutamadwant <adwant.goutam@gmail.com>
2020-08-31 19:20:24 -0700

    Update MergeTwoSortedArrays.java

--- src/InterviewPractice/Arrays/MergeTwoSortedArrays.java
+++ src/InterviewPractice/Arrays/MergeTwoSortedArrays.java
@@ -8,20 +8,20 @@ public class MergeTwoSortedArrays {
 
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
 
-        // two get pointers for nums1 and nums2
-        int p1 = m - 1;
-        int p2 = n - 1;
-        // set pointer for nums1
-        int p = m + n - 1;
-
-        // while there are still elements to compare
-        while ((p1 >= 0) && (p2 >= 0))
-            // compare two elements from nums1 and nums2
-            // and add the largest one in nums1
-            nums1[p--] = (nums1[p1] < nums2[p2]) ? nums2[p2--] : nums1[p1--];
-
-        // add missing elements from nums2
-        System.arraycopy(nums2, 0, nums1, 0, p2 + 1);
+        int i=m-1;
+        int j=n-1;
+        int k=nums1.length-1;
+        while(i >= 0 && j>= 0){
+            if(nums1[i] > nums2[j]){
+                nums1[k--] = nums1[i--];
+            }
+            else{
+                nums1[k--] = nums2[j--];
+            }
+        }
+        while(j >= 0){
+            nums1[k--]=nums2[j--];
+        }
 
     }
 
