17a91e2c8c1f1951d8b1304d84b7dce8b6324996
goutamadwant <adwant.goutam@gmail.com>
2020-08-31 19:20:33 -0700

    Update RangeAddition.java

--- src/InterviewPractice/Arrays/RangeAddition.java
+++ src/InterviewPractice/Arrays/RangeAddition.java
@@ -1,30 +1,52 @@
 package InterviewPractice.Arrays;
 
 /**
+ *
+ * Smart question & solution
+ *
  * We can track each range's start and end when iterating over the ranges.
  * And in the final result array, adjust the values on the change points.
+ *
+ * The algorithm makes use of the above intuition to simply store changes at the borders of the update ranges (instead of processing the entire range). Finally a single post processing operation is carried out over the entire output array.
+ *
+ * The two steps that are required are as follows:
+ *
+ * For each update query (start, end, inc) on the array arr, we need to do only two operations:
+ *
+ * Update start boundary of the range:
+ * arr_{start} = arr_{start} + inc
+ *
+ * Update just beyond the end boundary of the range:
+ * arr_{end+1} = arr_{end+1} - inc
+ *
+ * Final Transformation. The cumulative sum of the entire array is taken (0 - based indexing)
+ *
+ * arr_i = arr_i + arr_{i-1} forall
+ *
+ *
  */
 public class RangeAddition {
 
     public static int[] getModifiedArray(int length, int[][] updates) {
-        int[] result = new int[length];
-        if(updates==null||updates.length==0)
-            return result;
-
-        for(int i=0; i<updates.length; i++){
-            result[updates[i][0]] += updates[i][2];
-            if(updates[i][1]<length-1){
-                result[updates[i][1]+1] -=updates[i][2];
+        int[] nums = new int[length];
+        int k = updates.length;
+        for (int i = 0; i < k; i++) {
+            int start = updates[i][0];
+            int end = updates[i][1];
+            int inc = updates[i][2];
+            nums[start] += inc;
+            if (end < length - 1) {
+                nums[end + 1] -= inc;
             }
         }
 
-        int v=0;
-        for(int i=0; i<length; i++){
-            v += result[i];
-            result[i]=v;
+        int sum = 0;
+        for (int i = 0; i < length; i++) {
+            sum += nums[i];
+            nums[i] = sum;
         }
+        return nums;
 
-        return result;
     }
 
     public static void main(String[] args) {
