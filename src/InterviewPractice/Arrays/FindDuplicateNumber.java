7167538b44f8754b7d0d3305733c2f10aa976280
goutamadwant <adwant.goutam@gmail.com>
2020-09-02 21:23:05 -0700

    Update FindDuplicateNumber.java

--- src/InterviewPractice/Arrays/FindDuplicateNumber.java
+++ src/InterviewPractice/Arrays/FindDuplicateNumber.java
@@ -3,28 +3,40 @@ package InterviewPractice.Arrays;
 //https://leetcode.com/problems/find-the-duplicate-number/discuss/72846/My-easy-understood-solution-with-O(n)-time-and-O(1)-space-without-modifying-the-array.-With-clear-explanation.
 ///https://medium.com/solvingalgo/solving-algorithmic-problems-find-a-duplicate-in-an-array-3d9edad5ad41
 // important
+
+/**
+ * Floyd's algorithm consists of two phases and uses two pointers, usually called tortoise and hare.
+ *
+ * In phase 1, hare = nums[nums[hare]] is twice as fast as tortoise = nums[tortoise].
+ * Since the hare goes fast, it would be the first one who enters the cycle and starts to run around the cycle.
+ * At some point, the tortoise enters the cycle as well, and since it's moving slower the hare
+ * catches the tortoise up at some intersection point. Now phase 1 is over, and the tortoise has lost.
+ *
+ * In phase 2, we give the tortoise a second chance by slowing down the hare,
+ * so that it now moves with the speed of tortoise: tortoise = nums[tortoise], hare = nums[hare].
+ * The tortoise is back at the starting position, and the hare starts from the intersection point.
+ */
+
+
 public class FindDuplicateNumber {
 
     public static int findDuplicate(int[] nums){
-        if (nums.length > 1)
-        {
-            int slow = nums[0];
-            int fast = nums[nums[0]];
-            while (slow != fast)
-            {
-                slow = nums[slow];
-                fast = nums[nums[fast]];
-            }
+        // Find the intersection point of the two runners.
+        int tortoise = nums[0];
+        int hare = nums[0];
+        do {
+            tortoise = nums[tortoise];
+            hare = nums[nums[hare]];
+        } while (tortoise != hare);
 
-            fast = 0;
-            while (fast != slow)
-            {
-                fast = nums[fast];
-                slow = nums[slow];
-            }
-            return slow;
+        // Find the "entrance" to the cycle.
+        tortoise = nums[0];
+        while (tortoise != hare) {
+            tortoise = nums[tortoise];
+            hare = nums[hare];
         }
-        return -1;
+
+        return hare;
     }
 
     public static void main(String[] args) {
