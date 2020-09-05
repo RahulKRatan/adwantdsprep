1f3f9f69de91dc81ce0d3ca83ef8555a80452cd2
goutamadwant <adwant.goutam@gmail.com>
2020-09-04 20:49:03 -0700

    Update WordBreak.java

--- src/InterviewPractice/DP/WordBreak.java
+++ src/InterviewPractice/DP/WordBreak.java
@@ -74,3 +74,32 @@ public class WordBreak {
     }
 
 }
+
+/*
+Time complexity : O(n^2)
+Space complexity : O(n)
+
+public class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordDictSet=new HashSet(wordDict);
+        Queue<Integer> queue = new LinkedList<>();
+        int[] visited = new int[s.length()];
+        queue.add(0);
+        while (!queue.isEmpty()) {
+            int start = queue.remove();
+            if (visited[start] == 0) {
+                for (int end = start + 1; end <= s.length(); end++) {
+                    if (wordDictSet.contains(s.substring(start, end))) {
+                        queue.add(end);
+                        if (end == s.length()) {
+                            return true;
+                        }
+                    }
+                }
+                visited[start] = 1;
+            }
+        }
+        return false;
+    }
+}
+ */
