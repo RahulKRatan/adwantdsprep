123260d536260d093a45f5599c48eb52393c495a
goutamadwant <adwant.goutam@gmail.com>
2020-09-07 00:06:06 -0700

    Update UniquePaths.java

--- src/InterviewPractice/DP/UniquePaths.java
+++ src/InterviewPractice/DP/UniquePaths.java
@@ -4,28 +4,29 @@ package InterviewPractice.DP;
  * 91. Decode Ways
  * 70. Climbing Stairs
  * 509. Fibonacci Number
+ *
+ * https://leetcode.com/problems/unique-paths/discuss/405983/Easy-understand-Java-Solutions-with-Explanations-(DP-Top-down-Bottom-up-Linear-Space)
+ *Time: O(MN)
+ * Space: O(N) or O(M)
  */
 public class UniquePaths {
 
-    public static int uniquePaths(int m, int n) {
-       return paths(m,n,1,1);
-    }
+    /*
+    public int uniquePaths(int m, int n) {
+  if (m == 0 || n == 0) {
+    throw new IllegalArgumentException("m or n can't be 0");
+  }
+  return numPaths(m - 1, n - 1);
+}
 
-    public static int paths(int m, int n, int x, int y){
-        if(x==m && y == n){
-            return 1;
-        }
-        if(x<m && y<n){
-           return paths(m,n,x+1,y) + paths(m,n,x,y+1);
-        }
-        if(x<m){
-            return paths(m,n,x+1,y);
-        }
-        if(y<n){
-            return paths(m,n,x,y+1);
-        }
-        return 0;
-    }
+private int numPaths(int i, int j) {
+  if (i == 0 || j == 0) { // includes the row 0 and col 0
+    return 1;
+  }
+  return numPaths(i - 1, j) + numPaths(i, j - 1);
+}
+
+     */
 
     //DP solution
 
@@ -56,7 +57,34 @@ public class UniquePaths {
 
 
     public static void main(String[] args) {
-        int result = uniquePaths(3,2);
+        int result = uniquePathsDP(3,2);
         System.out.println(result);
     }
 }
+
+/*
+DP (Top-down with Memoization)
+
+public int uniquePaths(int m, int n) {
+  if (m == 0 || n == 0) {
+    throw new IllegalArgumentException("m or n can't be 0");
+  }
+  int[][] mem = new int[m][n];
+  for (int i = 0; i < m; ++i) { // init
+    for (int j = 0; j < n; ++j) {
+      mem[i][j] = -1;
+    }
+  }
+  return numPaths(m - 1, n - 1, mem);
+}
+
+private int numPaths(int i, int j, int[][] mem) {
+  if (i == 0 || j == 0) {
+    return 1;
+  }
+  if (mem[i - 1][j] == -1) mem[i - 1][j] = numPaths(i - 1, j, mem);
+  if (mem[i][j - 1] == -1) mem[i][j - 1] = numPaths(i, j - 1, mem);
+  return mem[i - 1][j] + mem[i][j - 1];
+}
+
+ */
