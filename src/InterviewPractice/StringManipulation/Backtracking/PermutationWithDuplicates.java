96005d865abd41a7defade0d49a790c4e7541ecf
goutamadwant <adwant.goutam@gmail.com>
2020-09-13 11:09:07 -0700

    Update PermutationWithDuplicates.java

--- src/InterviewPractice/StringManipulation/Backtracking/PermutationWithDuplicates.java
+++ src/InterviewPractice/StringManipulation/Backtracking/PermutationWithDuplicates.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * https://www.youtube.com/watch?v=A3ge2mdQi4g&ab_channel=AmellPeralta
+ */
 public class PermutationWithDuplicates {
 
     public static List<List<Integer>> permute(int[] nums) {
@@ -18,16 +21,26 @@ public class PermutationWithDuplicates {
             list.add(new ArrayList<>(tempList));
         } else{
             for(int i = 0; i < nums.length; i++){
-                if(used[i] || i > 0 && nums[i] == nums[i-1] && !used[i - 1]) continue; // element already exists, skip
+                if(used[i]) continue;
+                //if(i > 0 && nums[i] == nums[i-1] && !used[i - 1]) continue; // element already exists, skip
                 used[i] = true;
                 tempList.add(nums[i]);
                 backtrack(list, tempList, nums,used);
                 used[i] = false;
                 tempList.remove(tempList.size() - 1);
+                while (i+1 < nums.length && nums[i] == nums[i+1]){
+                    ++i;
+                }
             }
         }
     }
-
+/*
+The difficulty is to handle the duplicates.
+With inputs as [1a, 1b, 2a],
+If we don't handle the duplicates, the results would be: [1a, 1b, 2a], [1b, 1a, 2a]...,
+so we must make sure 1a goes before 1b to avoid duplicates
+By using nums[i-1]==nums[i] && !used[i-1], we can make sure that 1b cannot be choosed before 1a
+ */
 
     public static void main(String[] args) {
         int[] nums = new int[]{3,3,0,3};
