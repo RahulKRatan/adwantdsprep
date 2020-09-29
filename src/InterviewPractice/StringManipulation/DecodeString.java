7a7e1da57775332490dec7709434955630bd8b64
goutamadwant <adwant.goutam@gmail.com>
2020-09-29 16:10:54 -0700

    Update DecodeString.java

--- src/InterviewPractice/StringManipulation/DecodeString.java
+++ src/InterviewPractice/StringManipulation/DecodeString.java
@@ -8,32 +8,32 @@ public class DecodeString {
         String res = ""; // final result
         Stack<Integer> countStack = new Stack<>(); // num of times to iterate
         Stack<String> resStack = new Stack<>(); // resStack will always have recent calculated result
-        int idx = 0;
-        while (idx < s.length()) {
-            if (Character.isDigit(s.charAt(idx))) {
+        int index = 0;
+        while (index < s.length()) {
+            if (Character.isDigit(s.charAt(index))) {
                 int count = 0;
-                while (Character.isDigit(s.charAt(idx))) {
-                    count = 10 * count + (s.charAt(idx) - '0');
-                    idx++;
+                while (Character.isDigit(s.charAt(index))) {
+                    count = 10 * count + (s.charAt(index) - '0');
+                    index++;
                 }
                 countStack.push(count);
             }
-            else if (s.charAt(idx) == '[') {
+            else if (s.charAt(index) == '[') {
                 resStack.push(res); // pushing previous calculated result
                 res = "";
-                idx++;
+                index++;
             }
-            else if (s.charAt(idx) == ']') {
+            else if (s.charAt(index) == ']') {
                 StringBuilder temp = new StringBuilder (resStack.pop());
                 int repeatTimes = countStack.pop();
                 for (int i = 0; i < repeatTimes; i++) {
                     temp.append(res);
                 }
                 res = temp.toString();
-                idx++;
+                index++;
             }
             else {
-                res += s.charAt(idx++);
+                res += s.charAt(index++);
             }
         }
         return res;
