c9c23a6b99cc43de67d3719095d54aac312fba71
goutamadwant <adwant.goutam@gmail.com>
2020-10-03 23:22:52 -0700

    Update MinimumRemoveMakeValidParentheses.java

--- src/InterviewPractice/StringManipulation/MinimumRemoveMakeValidParentheses.java
+++ src/InterviewPractice/StringManipulation/MinimumRemoveMakeValidParentheses.java
@@ -1,9 +1,33 @@
 package InterviewPractice.StringManipulation;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
 public class MinimumRemoveMakeValidParentheses {
     public static String minRemoveToMakeValid(String s) {
-        
-        return s;
+        Set<Integer> indexesToRemove = new HashSet<>();
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } if (s.charAt(i) == ')') {
+                if (stack.isEmpty()) {
+                    indexesToRemove.add(i);
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        // Put any indexes remaining on stack into the set.
+        while (!stack.isEmpty()) indexesToRemove.add(stack.pop());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (!indexesToRemove.contains(i)) {
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
     }
 
     public static void main(String[] args) {
