69f1a25a1359f663813259b99bd5af61d336de58
goutamadwant <adwant.goutam@gmail.com>
2020-10-04 00:30:00 -0700

    Create DailyTemperatures.java

---
+++ src/InterviewPractice/StackQueues/DailyTemperatures.java
@@ -0,0 +1,13 @@
+package InterviewPractice.StackQueues;
+
+public class DailyTemperatures {
+    public static int[] dailyTemperatures(int[] T) {
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] T = new int[]{73, 74, 75, 71, 69, 72, 76, 73};
+        System.out.println(dailyTemperatures(T));
+    }
+}
