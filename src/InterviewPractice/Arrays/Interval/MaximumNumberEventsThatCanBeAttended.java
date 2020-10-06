d2c7e7501c11a619c567b8e91d17a766b0da495d
goutamadwant <adwant.goutam@gmail.com>
2020-10-05 21:20:33 -0700

    Update MaximumNumberEventsThatCanBeAttended.java

--- src/InterviewPractice/Arrays/Interval/MaximumNumberEventsThatCanBeAttended.java
+++ src/InterviewPractice/Arrays/Interval/MaximumNumberEventsThatCanBeAttended.java
@@ -1,9 +1,22 @@
 package InterviewPractice.Arrays.Interval;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class MaximumNumberEventsThatCanBeAttended {
 
+    public static int maxEvents(int[][] events) {
+        Comparator<int[]> c= Comparator.comparingInt((int[] a) -> a[0]);
+        Arrays.sort(events, c);
+        int count = 1;
+        for(int i=0; i<events.length-1; i++)
+            if(events[i][1]<=events[i+1][0]) count+=1;
+
+        return count;
+    }
 
     public static void main(String[] args) {
-        
+        int[][] ints = new int[][]{{1,2},{2,3},{3,4},{1,2}};
+        System.out.println(maxEvents(ints));
     }
 }
