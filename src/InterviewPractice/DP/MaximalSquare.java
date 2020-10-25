819c64677e9e6c36aa46f2beeeecbaca1009a4e9
goutamadwant <adwant.goutam@gmail.com>
2020-10-25 13:12:47 -0700

    Update MaximalSquare.java

--- src/InterviewPractice/DP/MaximalSquare.java
+++ src/InterviewPractice/DP/MaximalSquare.java
@@ -5,8 +5,6 @@ package InterviewPractice.DP;
  * Auxiliary Space: O(m*n) where m is number of rows and n is number of columns in the given matrix.
  *
  * 1) Construct a sum matrix S[R][C] for the given M[R][C].
- *      a)    Copy first row and first columns as it is from M[][] to S[][]
- *      b)    For other entries, use following expressions to construct S[][]
  *          If M[i][j] is 1 then
  *             S[i][j] = min(S[i][j-1], S[i-1][j], S[i-1][j-1]) + 1
  *          Else If M[i][j] is 0
@@ -14,58 +12,29 @@ package InterviewPractice.DP;
  * 2)Find the maximum entry in S[R][C]
  * 3)Using the value and coordinates of maximum entry in S[i],print
          sub-matrix of M[][]
- *
+ * https://leetcode.com/problems/maximal-square/solution/
  * https://www.youtube.com/watch?v=_Lf1looyJMU&t=2s&ab_channel=TusharRoy-CodingMadeSimples
  *
  */
 public class MaximalSquare {
 
-    private static int min(int a,int b, int c){
-        int l = Math.min(a, b);
-        return Math.min(l, c);
-    }
-
     public static int maximalSquare(char[][] matrix) {
-        if(matrix.length == 0){
-            return 0;
-        }
-        int result[][] = new int[matrix.length][matrix[0].length];
-        int max = 0;
-        for(int i=0; i < matrix.length; i++){
-            result[i][0] = Character.getNumericValue(matrix[i][0]);
-            if (result[i][0] == 1)
-            {
-                max = 1;
-            }
-        }
-
-        for(int i=0; i < matrix[0].length; i++){
-            result[0][i] = Character.getNumericValue(matrix[0][i]);
-            if (result[0][i] == 1)
-            {
-                max = 1;
-            }
-
-        }
-
-        for(int i=1; i < matrix.length; i++){
-            for(int j=1; j < matrix[i].length; j++){
-                if(matrix[i][j] == '0'){
-                    continue;
-                }
-                int t = min(result[i-1][j],result[i-1][j-1],result[i][j-1]);
-                result[i][j] = (t+1);
-                if(result[i][j] > max){
-                    max = result[i][j];
+        int rows = matrix.length, cols = rows > 0 ? matrix[0].length : 0;
+        int[][] dp = new int[rows + 1][cols + 1];
+        int maxsize = 0;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                if (matrix[i-1][j-1] == '1'){
+                    dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+                    maxsize = Math.max(maxsize, dp[i][j]);
                 }
             }
         }
-        return max*max;
+        return maxsize * maxsize;
     }
 
     public static void main(String[] args) {
-        //char[][] chars = new char[][]{{'1','0','1','0','0'},{'1','0','1','1','1'},{'1','1','1','1','1'},{'1','0','0','1','0'}};
-        char[][] chars = new char[][]{};
+        char[][] chars = new char[][]{{'1','0','1','0','0'},{'1','0','1','1','1'},{'1','1','1','1','1'},{'1','0','0','1','0'}};
         System.out.println(maximalSquare(chars));
     }
 }
