588d615e5474ae29f42cce5559a268c64bfec6fb
goutamadwant <adwant.goutam@gmail.com>
2020-11-04 22:37:48 -0800

    Update MinimumHeightTrees.java

--- src/InterviewPractice/Trees/MinimumHeightTrees.java
+++ src/InterviewPractice/Trees/MinimumHeightTrees.java
@@ -1,4 +1,18 @@
 package InterviewPractice.Trees;
 
+import java.util.List;
+
 public class MinimumHeightTrees {
+
+    public static List<Integer> findMinHeightTrees(int n, int[][] edges) {
+
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[][] edges = new int[][]{{3,0},{3,1},{3,2},{3,4},{5,4}};
+        List<Integer> result = findMinHeightTrees(6,edges);
+    }
+
 }
