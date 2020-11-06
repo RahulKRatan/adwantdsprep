095cb35868806a369b8cca400633bfec8a705684
goutamadwant <adwant.goutam@gmail.com>
2020-11-05 21:47:18 -0800

    Update MinimumCostMoveChipsTheSamePosition.java

--- src/InterviewPractice/Arrays/MinimumCostMoveChipsTheSamePosition.java
+++ src/InterviewPractice/Arrays/MinimumCostMoveChipsTheSamePosition.java
@@ -1,4 +1,9 @@
 package InterviewPractice.Arrays;
 
 public class MinimumCostMoveChipsTheSamePosition {
+
+
+    public static void main(String[] args) {
+        int[] positions = new int[]{};
+    }
 }
