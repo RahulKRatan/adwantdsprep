c5e2c09797a85b57475b6638f2e3fc1e888a54bb
goutamadwant <adwant.goutam@gmail.com>
2020-11-08 15:56:08 -0800

    Update FindSmallestDivisorGivenThreshold.java

--- src/InterviewPractice/Arrays/FindSmallestDivisorGivenThreshold.java
+++ src/InterviewPractice/Arrays/FindSmallestDivisorGivenThreshold.java
@@ -1,5 +1,9 @@
 package InterviewPractice.Arrays;
 
+/**
+ * 
+ */
+
 public class FindSmallestDivisorGivenThreshold {
     public static int smallestDivisor(int[] nums, int threshold) {
         int left = 1, right = (int)1e6; //1e6 is 10 power 6 which is 1 million as mentioned in the problem.
