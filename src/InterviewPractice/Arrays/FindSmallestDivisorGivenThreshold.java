13782a6510989d3762bef672ca7c9301f480956f
goutamadwant <adwant.goutam@gmail.com>
2020-11-07 21:09:05 -0800

    Update FindSmallestDivisorGivenThreshold.java

--- src/InterviewPractice/Arrays/FindSmallestDivisorGivenThreshold.java
+++ src/InterviewPractice/Arrays/FindSmallestDivisorGivenThreshold.java
@@ -1,8 +1,11 @@
 package InterviewPractice.Arrays;
 
 public class FindSmallestDivisorGivenThreshold {
+    public static int smallestDivisor(int[] nums, int threshold) {
 
+        return 0;
+    }
     public static void main(String[] args) {
-        
+
     }
 }
