e87a0c2f1b373155245c852e4605ab9add6de318
goutamadwant <adwant.goutam@gmail.com>
2020-11-11 20:55:46 -0800

    Update ValidSquare.java

--- src/InterviewPractice/Arrays/ValidSquare.java
+++ src/InterviewPractice/Arrays/ValidSquare.java
@@ -1,4 +1,17 @@
 package InterviewPractice.Arrays;
 
 public class ValidSquare {
+    public static boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
+        
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] p1 = new int[]{0,0};
+        int[] p2 = new int[]{1,1};
+        int[] p3 = new int[]{1,0};
+        int[] p4 = new int[]{0,1};
+        System.out.println(validSquare(p1,p2,p3,p4));
+    }
 }
