62539e59f3b2b63a411fb2c8874dc921a0940613
goutamadwant <adwant.goutam@gmail.com>
2020-11-15 09:33:48 -0800

    Update RandomPickwithWeight.java

--- src/InterviewPractice/Design/RandomPickwithWeight.java
+++ src/InterviewPractice/Design/RandomPickwithWeight.java
@@ -1,4 +1,16 @@
 package InterviewPractice.Design;
 
 public class RandomPickwithWeight {
+    public RandomPickwithWeight(int[] w) {
+
+    }
+
+    public static int pickIndex() {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        
+    }
 }
