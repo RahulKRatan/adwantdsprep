4a7c5511d0c383db049bdaa5b421959771cd3217
goutamadwant <adwant.goutam@gmail.com>
2020-11-15 18:37:08 -0800

    Update CountOfSmallerNumbersAfterSelf.java

--- src/InterviewPractice/Arrays/CountOfSmallerNumbersAfterSelf.java
+++ src/InterviewPractice/Arrays/CountOfSmallerNumbersAfterSelf.java
@@ -1,13 +1,74 @@
 package InterviewPractice.Arrays;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CountOfSmallerNumbersAfterSelf {
-    public List<Integer> countSmaller(int[] nums) {
+    static int[] count;
 
-        return null;
+    public static List<Integer> countSmaller(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        int size = nums.length;
+        count = new int[size];
+        int[] indexes = new int[size];
+        for(int i = 0; i < size; i++){
+            indexes[i] = i;
+        }
+        mergesort(nums, indexes, 0, nums.length - 1);
+        for(int i = 0; i < count.length; i++){
+            result.add(count[i]);
+        }
+        return result;
     }
+
+    private static void mergesort(int[] nums, int[] indexes, int start, int end){
+        if(end <= start){
+            return;
+        }
+        int mid = (start + end) / 2;
+        mergesort(nums, indexes, start, mid);
+        mergesort(nums, indexes, mid + 1, end);
+
+        merge(nums, indexes, start, end);
+    }
+
+    private static void merge(int[] nums, int[] indexes, int start, int end){
+        int mid = (start + end) / 2;
+        int left_index = start;
+        int right_index = mid+1;
+        int rightcount = 0;
+        int[] new_indexes = new int[end - start + 1];
+
+        int sort_index = 0;
+        while(left_index <= mid && right_index <= end){
+            if(nums[indexes[right_index]] < nums[indexes[left_index]]){
+                new_indexes[sort_index] = indexes[right_index];
+                rightcount++;
+                right_index++;
+            }else{
+                new_indexes[sort_index] = indexes[left_index];
+                count[indexes[left_index]] += rightcount;
+                left_index++;
+            }
+            sort_index++;
+        }
+        while(left_index <= mid){
+            new_indexes[sort_index] = indexes[left_index];
+            count[indexes[left_index]] += rightcount;
+            left_index++;
+            sort_index++;
+        }
+        while(right_index <= end){
+            new_indexes[sort_index++] = indexes[right_index++];
+        }
+        for(int i = start; i <= end; i++){
+            indexes[i] = new_indexes[i - start];
+        }
+    }
+
     public static void main(String[] args) {
         int[] nums = new int[]{5,2,6,1};
+        List<Integer> result = countSmaller(nums);
+        System.out.println(result);
     }
 }
