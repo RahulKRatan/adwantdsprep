66919c7acf435c8b85fe34044ce1d63c6f3b471f
goutamadwant <adwant.goutam@gmail.com>
2020-11-15 17:35:42 -0800

    Create CountOfSmallerNumbersAfterSelf.java

---
+++ src/InterviewPractice/Arrays/CountOfSmallerNumbersAfterSelf.java
@@ -0,0 +1,13 @@
+package InterviewPractice.Arrays;
+
+import java.util.List;
+
+public class CountOfSmallerNumbersAfterSelf {
+    public List<Integer> countSmaller(int[] nums) {
+
+        return null;
+    }
+    public static void main(String[] args) {
+        int[] nums = new int[]{5,2,6,1};
+    }
+}
