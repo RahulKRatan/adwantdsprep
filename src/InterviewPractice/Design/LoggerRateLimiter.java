4cae166278affa2e843f99de3386324b70b869b6
goutamadwant <adwant.goutam@gmail.com>
2020-11-16 22:26:18 -0800

    Update LoggerRateLimiter.java

--- src/InterviewPractice/Design/LoggerRateLimiter.java
+++ src/InterviewPractice/Design/LoggerRateLimiter.java
@@ -1,4 +1,27 @@
 package InterviewPractice.Design;
 
+import java.util.HashMap;
+
 public class LoggerRateLimiter {
+    private HashMap<String, Integer> mapLogRateLimiter;
+
+    public LoggerRateLimiter() {
+        mapLogRateLimiter = new HashMap<>();
+    }
+
+    public boolean shouldPrintMessage(int timestamp, String message) {
+
+        if (!mapLogRateLimiter.containsKey(message)) {
+            mapLogRateLimiter.put(message, timestamp);
+            return true;
+        }
+
+        Integer oldTimestamp = mapLogRateLimiter.get(message);
+        if (timestamp - oldTimestamp >= 10) {
+            mapLogRateLimiter.put(message, timestamp);
+            return true;
+        } else
+            return false;
+    }
+
 }
