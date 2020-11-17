4b70c64d97ec13090e638fc22f9675176e24b5a6
goutamadwant <adwant.goutam@gmail.com>
2020-11-17 12:12:35 -0800

    Update MaximumPointsYouCanObtainFromCards.java

--- src/InterviewPractice/DP/MaximumPointsYouCanObtainFromCards.java
+++ src/InterviewPractice/DP/MaximumPointsYouCanObtainFromCards.java
@@ -1,4 +1,13 @@
 package InterviewPractice.DP;
 
 public class MaximumPointsYouCanObtainFromCards {
+    public static int maxScore(int[] cardPoints, int k) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] cardPoints = new int[]{1,2,3,4,5,6,1};
+        System.out.println(maxScore(cardPoints,3));
+    }
 }
