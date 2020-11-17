949f5610f19ccbc5a2a9571f7acceeddee7dd57d
goutamadwant <adwant.goutam@gmail.com>
2020-11-17 13:29:39 -0800

    Create ValidateStackSequences.java

---
+++ src/InterviewPractice/StackQueues/ValidateStackSequences.java
@@ -0,0 +1,12 @@
+package InterviewPractice.StackQueues;
+
+public class ValidateStackSequences {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] pushed = new int[]{1,2,3,4,5};
+        int[] popped = new int[]{4,5,3,2,1};
+    }
+}
