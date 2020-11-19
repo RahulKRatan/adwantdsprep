336b0cb50bad44fc1b65d22f227ab69ee924ec0d
goutamadwant <adwant.goutam@gmail.com>
2020-11-18 21:18:47 -0800

    Update ValidateStackSequences.java

--- src/InterviewPractice/StackQueues/ValidateStackSequences.java
+++ src/InterviewPractice/StackQueues/ValidateStackSequences.java
@@ -1,12 +1,26 @@
 package InterviewPractice.StackQueues;
 
-public class ValidateStackSequences {
-    public boolean validateStackSequences(int[] pushed, int[] popped) {
+import java.util.Stack;
 
-        return false;
+public class ValidateStackSequences {
+    public static boolean validateStackSequences(int[] pushed, int[] popped) {
+        int length = pushed.length;
+        Stack<Integer> stack = new Stack();
+
+        int j = 0;
+        for (int x: pushed) {
+            stack.push(x);
+            while (!stack.isEmpty() && j < length && stack.peek() == popped[j]) {
+                stack.pop();
+                j++;
+            }
+        }
+
+        return j == length;
     }
     public static void main(String[] args) {
         int[] pushed = new int[]{1,2,3,4,5};
         int[] popped = new int[]{4,5,3,2,1};
+        System.out.println(validateStackSequences(pushed,popped));
     }
 }
