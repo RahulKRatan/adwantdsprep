79c6348d03d6d6e4063dc8538180e2aba61af2eb
goutamadwant <adwant.goutam@gmail.com>
2020-11-19 17:11:46 -0800

    Create FindTwoNonOverlappingSubArraysEachWithTargetSum.java

---
+++ src/InterviewPractice/Arrays/FindTwoNonOverlappingSubArraysEachWithTargetSum.java
@@ -0,0 +1,13 @@
+package InterviewPractice.Arrays;
+
+public class FindTwoNonOverlappingSubArraysEachWithTargetSum {
+    public static int minSumOfLengths(int[] arr, int target) {
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{3,1,1,1,5,1,2,1};
+        System.out.println(minSumOfLengths(arr,3));
+    }
+}
