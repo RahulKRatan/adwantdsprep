70e2e0da9791520ee7b10ed0180e6003e3fd3a50
goutamadwant <adwant.goutam@gmail.com>
2020-11-20 16:36:50 -0800

    Create StringTransformsIntoAnotherString.java

---
+++ src/InterviewPractice/StringManipulation/StringTransformsIntoAnotherString.java
@@ -0,0 +1,11 @@
+package InterviewPractice.StringManipulation;
+
+public class StringTransformsIntoAnotherString {
+    public static boolean canConvert(String str1, String str2) {
+
+        return false;
+    }
+    public static void main(String[] args) {
+        System.out.println(canConvert("aabcc","ccdee"));
+    }
+}
