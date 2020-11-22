56f660daeb477130bbdcd1ecfb106cdf20c393d7
goutamadwant <adwant.goutam@gmail.com>
2020-11-21 23:27:47 -0800

    Create EncodeStringWithShortestLength.java

---
+++ src/InterviewPractice/StringManipulation/EncodeStringWithShortestLength.java
@@ -0,0 +1,12 @@
+package InterviewPractice.StringManipulation;
+
+public class EncodeStringWithShortestLength {
+    public static String encode(String s) {
+
+        return "";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(encode("aaaa"));
+    }
+}
