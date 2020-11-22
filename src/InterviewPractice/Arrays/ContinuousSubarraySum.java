0bfad45497159faf09d9e8c57c75a4ca9cec7a58
goutamadwant <adwant.goutam@gmail.com>
2020-11-22 00:12:42 -0800

    Update ContinuousSubarraySum.java

--- src/InterviewPractice/Arrays/ContinuousSubarraySum.java
+++ src/InterviewPractice/Arrays/ContinuousSubarraySum.java
@@ -1,4 +1,13 @@
 package InterviewPractice.Arrays;
 
 public class ContinuousSubarraySum {
+
+    public static boolean checkSubarraySum(int[] nums, int k) {
+            
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkSubarraySum(new int[]{23, 2, 4, 6, 7},6));
+    }
 }
