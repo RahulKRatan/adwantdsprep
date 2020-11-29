36609fef6f425d844a7d5d5eea14d36ab5bb6cbf
goutamadwant <adwant.goutam@gmail.com>
2020-11-28 19:11:43 -0800

    Create LongestStringChain.java

---
+++ src/InterviewPractice/StringManipulation/LongestStringChain.java
@@ -0,0 +1,12 @@
+package InterviewPractice.StringManipulation;
+
+public class LongestStringChain {
+    public static int longestStrChain(String[] words) {
+
+        return 0;
+    }
+    public static void main(String[] args) {
+        String[] words = new String[]{"a","b","ba","bca","bda","bdca"};
+        System.out.println(longestStrChain(words));
+    }
+}
