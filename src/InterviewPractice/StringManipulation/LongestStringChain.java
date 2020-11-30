ca12c070745c157c42c344147bb19a838f325fd6
goutamadwant <adwant.goutam@gmail.com>
2020-11-29 16:32:43 -0800

    Update LongestStringChain.java

--- src/InterviewPractice/StringManipulation/LongestStringChain.java
+++ src/InterviewPractice/StringManipulation/LongestStringChain.java
@@ -2,7 +2,7 @@ package InterviewPractice.StringManipulation;
 
 public class LongestStringChain {
     public static int longestStrChain(String[] words) {
-
+        
         return 0;
     }
     public static void main(String[] args) {
