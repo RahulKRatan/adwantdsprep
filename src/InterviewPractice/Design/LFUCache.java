d538b39937ccde5aaf80b11c13649cecbf0a03d0
goutamadwant <adwant.goutam@gmail.com>
2020-11-29 17:00:27 -0800

    Update LFUCache.java

--- src/InterviewPractice/Design/LFUCache.java
+++ src/InterviewPractice/Design/LFUCache.java
@@ -1,4 +1,18 @@
 package InterviewPractice.Design;
 
 public class LFUCache {
+
+    public LFUCache(int capacity) {
+
+    }
+
+    public int get(int key) {
+
+        return 0;
+    }
+
+    public void put(int key, int value) {
+
+    }
+
 }
