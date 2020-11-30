9cc97e7dc633b3a833bcb5f56c314adb3df20778
goutamadwant <adwant.goutam@gmail.com>
2020-11-29 16:38:16 -0800

    Update LongestStringChain.java

--- src/InterviewPractice/StringManipulation/LongestStringChain.java
+++ src/InterviewPractice/StringManipulation/LongestStringChain.java
@@ -1,12 +1,27 @@
 package InterviewPractice.StringManipulation;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestStringChain {
     public static int longestStrChain(String[] words) {
-        
-        return 0;
+        Map<String, Integer> dp = new HashMap<>();
+        Arrays.sort(words, (a, b)->a.length() - b.length());
+        int res = 0;
+        for (String word : words) {
+            int best = 0;
+            for (int i = 0; i < word.length(); ++i) {
+                String prev = word.substring(0, i) + word.substring(i + 1);
+                best = Math.max(best, dp.getOrDefault(prev, 0) + 1);
+            }
+            dp.put(word, best);
+            res = Math.max(res, best);
+        }
+        return res;
     }
     public static void main(String[] args) {
-        String[] words = new String[]{"a","b","ba","bca","bda","bdca"};
+        String[] words = new String[]{"xbc","pcxbcf","xb","cxbc","pcxbc"};
         System.out.println(longestStrChain(words));
     }
 }
