fa75467dbd06d75ffba5100c05cc731e19af1703
goutamadwant <adwant.goutam@gmail.com>
2020-12-05 14:03:36 -0800

    Update RichestCustomerWealth.java

--- src/InterviewPractice/Arrays/RichestCustomerWealth.java
+++ src/InterviewPractice/Arrays/RichestCustomerWealth.java
@@ -1,4 +1,21 @@
 package InterviewPractice.Arrays;
 
 public class RichestCustomerWealth {
+    public int maximumWealth(int[][] accounts) {
+        int max = 0;
+        for(int i=0;i<accounts.length;i++){
+            int sum = 0;
+            for(int j=0;j<accounts[0].length;j++){
+                sum += accounts[i][j];
+            }
+            if(sum>max){
+                max = sum;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[][] accounts = new int[][]{};
+    }
 }
