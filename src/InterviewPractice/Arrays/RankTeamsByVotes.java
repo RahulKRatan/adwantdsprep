059e047980c39dbd6bd678bed2b8908d5cbc51b2
goutamadwant <adwant.goutam@gmail.com>
2020-12-08 15:38:23 -0800

    Update RankTeamsByVotes.java

--- src/InterviewPractice/Arrays/RankTeamsByVotes.java
+++ src/InterviewPractice/Arrays/RankTeamsByVotes.java
@@ -1,12 +1,13 @@
 package InterviewPractice.Arrays;
 
 public class RankTeamsByVotes {
-    public String rankTeams(String[] votes) {
+    public static String rankTeams(String[] votes) {
 
         return "";
     }
     public static void main(String[] args) {
-    String[] votes = new String[]{};
+    String[] votes = new String[]{"ABC","ACB","ABC","ACB","ACB"};
+    System.out.println(rankTeams(votes));
     }
 }
 
