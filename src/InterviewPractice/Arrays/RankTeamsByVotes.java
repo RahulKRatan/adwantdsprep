bff8e32e0eb75a293df97065580d714871e82afb
goutamadwant <adwant.goutam@gmail.com>
2020-12-08 16:01:11 -0800

    Update RankTeamsByVotes.java

--- src/InterviewPractice/Arrays/RankTeamsByVotes.java
+++ src/InterviewPractice/Arrays/RankTeamsByVotes.java
@@ -1,9 +1,34 @@
 package InterviewPractice.Arrays;
 
+import java.util.*;
+
 public class RankTeamsByVotes {
     public static String rankTeams(String[] votes) {
+        Map<Character, int[]> map = new HashMap<>();
+        int l = votes[0].length();
+        for(String vote : votes){
+            for(int i = 0; i < l; i++){
+                char c = vote.charAt(i);
+                map.putIfAbsent(c, new int[l]);
+                map.get(c)[i]++;
+            }
+        }
 
-        return "";
+        List<Character> list = new ArrayList<>(map.keySet());
+        list.sort((a, b) -> {
+            for (int i = 0; i < l; i++) {
+                if (map.get(a)[i] != map.get(b)[i]) {
+                    return map.get(b)[i] - map.get(a)[i];
+                }
+            }
+            return a - b;
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for(char c : list){
+            sb.append(c);
+        }
+        return sb.toString();
     }
     public static void main(String[] args) {
     String[] votes = new String[]{"ABC","ACB","ABC","ACB","ACB"};
