ab84308e444b023940b744c1403779b7e4d0fbb5
goutamadwant <adwant.goutam@gmail.com>
2020-12-09 20:51:45 -0800

    Create MinimumNumberTapsToOpenToWaterGarden.java

---
+++ src/InterviewPractice/DP/MinimumNumberTapsToOpenToWaterGarden.java
@@ -0,0 +1,14 @@
+package InterviewPractice.DP;
+
+public class MinimumNumberTapsToOpenToWaterGarden {
+
+    public static int minTaps(int n, int[] ranges) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] ranges = new int[]{1,2,1,0,2,1,0,1};
+        System.out.println(minTaps(7,ranges));
+    }
+}
