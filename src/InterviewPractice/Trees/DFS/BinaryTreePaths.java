c6112a0af0355d5d9ba875b7c03e03a1dc20c118
goutamadwant <adwant.goutam@gmail.com>
2020-12-27 10:26:20 -0800

    Update BinaryTreePaths.java

--- src/InterviewPractice/Trees/DFS/BinaryTreePaths.java
+++ src/InterviewPractice/Trees/DFS/BinaryTreePaths.java
@@ -2,13 +2,19 @@ package InterviewPractice.Trees.DFS;
 
 import InterviewPractice.Trees.TreeNode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BinaryTreePaths {
     TreeNode node;
     public static List<String> binaryTreePaths(TreeNode root) {
+        List<String> result = new ArrayList<>();
 
-        return null;
+        return result;
+    }
+
+    public void helper(){
+
     }
 
     public static void main(String[] args) {
@@ -17,5 +23,7 @@ public class BinaryTreePaths {
         tree.node.left = new TreeNode(2);
         tree.node.right = new TreeNode(3);
         tree.node.left.right = new TreeNode(5);
+        List<String> result = binaryTreePaths(tree.node);
+        System.out.println(result);
     }
 }
