39b09b0edc01be7f0e9a001a57694cbf1e246434
goutamadwant <adwant.goutam@gmail.com>
2021-02-16 00:06:39 -0800

    Create BasicCalculatorII.java

---
+++ src/InterviewPractice/StackQueues/BasicCalculatorII.java
@@ -0,0 +1,11 @@
+package InterviewPractice.StackQueues;
+
+public class BasicCalculatorII {
+    public static int calculate(String s) {
+        return 0;
+    }
+    public static void main(String[] args) {
+        String s = "(1+(4+5+2)-3)+(6+8)";
+        System.out.println(calculate(s));
+    }
+}
