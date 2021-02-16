35fffa5757373b15964a7e5175ef2c07e2ce7b3e
goutamadwant <adwant.goutam@gmail.com>
2021-02-15 23:58:20 -0800

    Update CoinChange2.java

--- src/InterviewPractice/DP/CoinChange2.java
+++ src/InterviewPractice/DP/CoinChange2.java
@@ -1,9 +1,13 @@
 package InterviewPractice.DP;
 
+/**
+ * You are given coins of different denominations and a total amount of money.
+ * Write a function to compute the number of combinations that make up that amount. You may assume that you have infinite number of each kind of coin.
+ */
 public class CoinChange2 {
 
     public static int change(int amount, int[] coins) {
-        
+
         return 0;
     }
     public static void main(String[] args) {
