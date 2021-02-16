572bb56db4f494d151ef940cb193d6ec1b34bf13
goutamadwant <adwant.goutam@gmail.com>
2021-02-15 23:36:57 -0800

    Create CoinChange2.java

---
+++ src/InterviewPractice/DP/CoinChange2.java
@@ -0,0 +1,10 @@
+package InterviewPractice.DP;
+
+public class CoinChange2 {
+
+    public static void main(String[] args) {
+        int[] coins = new int[]{1,2,5};
+        System.out.println(coinChange(coins,11));
+    }
+
+}
