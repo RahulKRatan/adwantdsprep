120e11e11c80daec27645fb544a9147510d7a1d3
goutamadwant <adwant.goutam@gmail.com>
2021-02-16 00:09:54 -0800

    Create MaximumLengthConcatenatedStringUniqueCharacters.java

---
+++ src/InterviewPractice/Backtracking/MaximumLengthConcatenatedStringUniqueCharacters.java
@@ -0,0 +1,21 @@
+package InterviewPractice.Backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MaximumLengthConcatenatedStringUniqueCharacters {
+
+    public static int maxLength(List<String> arr) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        List<String> arr = new ArrayList<>();
+        arr.add("un");
+        arr.add("iq");
+        arr.add("ue");
+        System.out.println(maxLength(arr));
+    }
+
+}
