1641fac9abd08a4304f6f4cd1109bf5ff8b916bc
goutamadwant <adwant.goutam@gmail.com>
2021-02-15 23:42:16 -0800

    Update CoinChange2.java

--- src/InterviewPractice/DP/CoinChange2.java
+++ src/InterviewPractice/DP/CoinChange2.java
@@ -2,9 +2,13 @@ package InterviewPractice.DP;
 
 public class CoinChange2 {
 
+    public static int change(int amount, int[] coins) {
+        
+        return 0;
+    }
     public static void main(String[] args) {
         int[] coins = new int[]{1,2,5};
-        System.out.println(coinChange(coins,11));
+        System.out.println(change(11,coins));
     }
 
 }
