fec0b0da10080875c9e1864d03081aadfcf909fe
goutamadwant <adwant.goutam@gmail.com>
2021-04-13 23:34:39 -0700

    Update DeepestLeavesSum.java

--- src/InterviewPractice/Trees/DFS/DeepestLeavesSum.java
+++ src/InterviewPractice/Trees/DFS/DeepestLeavesSum.java
@@ -2,27 +2,35 @@ package InterviewPractice.Trees.DFS;
 
 import InterviewPractice.Trees.TreeNode;
 
+/**
+ *  Time : O(N) & Space : O(1)
+ */
 public class DeepestLeavesSum {
     TreeNode node;
     static int sum = 0;
+    static int maxDepth = 0;
     public static int deepestLeavesSum(TreeNode root) {
-        int maxDepth = maxDepth(root);
-        findSum(root,1,maxDepth);
+        //int maxDepth = maxDepth(root);
+        findSum(root,1);
         return sum;
     }
 
-    public static int maxDepth(TreeNode root){
+   /* public static int maxDepth(TreeNode root){
         if(root == null) return 0;
         return 1 + Math.max(maxDepth(root.left),maxDepth(root.right));
-    }
+    }*/
 
-    public static void findSum(TreeNode root, int currDepth, int maxDepth){
+    public static void findSum(TreeNode root, int currDepth){
         if(root != null){
+            if(currDepth > maxDepth){
+                maxDepth = currDepth;
+                sum = 0;
+            }
             if(currDepth == maxDepth){
                 sum += root.val;
             }
-            findSum(root.left,currDepth+1,maxDepth);
-            findSum(root.right,currDepth+1,maxDepth);
+            findSum(root.left,currDepth+1);
+            findSum(root.right,currDepth+1);
         }
     }
 
