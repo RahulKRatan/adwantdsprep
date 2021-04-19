284f27c0906a7836939f095536b430277152ccc7
goutamadwant <adwant.goutam@gmail.com>
2021-04-18 18:54:01 -0700

    Create SetMatrixZeroes.java

---
+++ src/InterviewPractice/Matrix/SetMatrixZeroes.java
@@ -0,0 +1,14 @@
+package InterviewPractice.Matrix;
+
+public class SetMatrixZeroes {
+
+    public static void setZeroes(int[][] matrix) {
+        
+        return;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+        setZeroes(matrix);
+    }
+}
