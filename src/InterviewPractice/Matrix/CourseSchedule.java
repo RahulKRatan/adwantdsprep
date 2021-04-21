911c9481374158f9cf0cdc34d9b00fea7dac8b05
goutamadwant <adwant.goutam@gmail.com>
2021-04-20 20:40:43 -0700

    Create CourseSchedule.java

---
+++ src/InterviewPractice/Matrix/CourseSchedule.java
@@ -0,0 +1,50 @@
+package InterviewPractice.Matrix;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+/*
+Time Complexity:
+If we use adjacency matrix : O(V2)
+If we use adjacency list: O(E+V)
+ */
+public class CourseSchedule {
+
+    public static boolean canFinish(int numCourses, int[][] prerequisites) {
+
+
+        if (numCourses == 0 || prerequisites.length == 0) return true;
+
+        // Convert graph presentation from edges to indegree of adjacent list.
+        int indegree[] = new int[numCourses];
+        for (int i = 0; i < prerequisites.length; i++) // Indegree - how many prerequisites are needed.
+            indegree[prerequisites[i][0]]++;
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++)
+            if (indegree[i] == 0)
+                queue.add(i);
+
+        // How many courses don't need prerequisites.
+        int canFinishCount = queue.size();
+        while (!queue.isEmpty()) {
+            int prerequisite = queue.remove(); // Already finished this prerequisite course.
+            for (int i = 0; i < prerequisites.length; i++)  {
+                if (prerequisites[i][1] == prerequisite) {
+                    indegree[prerequisites[i][0]]--;
+                    if (indegree[prerequisites[i][0]] == 0) {
+                        canFinishCount++;
+                        queue.add(prerequisites[i][0]);
+                    }
+                }
+            }
+        }
+
+        return (canFinishCount == numCourses);
+    }
+
+    public static void main(String[] args) {
+        int[][] prerequisites = new int[][]{{1,0},{2,0},{3,1},{3,2}};
+        System.out.println(canFinish(4,prerequisites));
+    }
+}
