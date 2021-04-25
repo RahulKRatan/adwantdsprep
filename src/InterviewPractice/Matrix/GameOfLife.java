f333766b981cec46ef9e558812a9d219b44f6271
goutamadwant <adwant.goutam@gmail.com>
2021-04-24 20:03:21 -0700

    Update GameOfLife.java

--- src/InterviewPractice/Matrix/GameOfLife.java
+++ src/InterviewPractice/Matrix/GameOfLife.java
@@ -2,10 +2,129 @@ package InterviewPractice.Matrix;
 
 public class GameOfLife {
 
+    /*
+    Time Complexity: O(M×N), where M is the number of rows and N is the number of columns of the Board.
+    Space Complexity: O(M×N), where M is the number of rows and N is the number of columns of the Board. This is the space occupied by the copy board we created initially.
+     */
     public void gameOfLife(int[][] board) {
+        // Neighbors array to find 8 neighboring cells for a given cell
+        int[] neighbors = {0, 1, -1};
 
+        int rows = board.length;
+        int cols = board[0].length;
+
+        // Create a copy of the original board
+        int[][] copyBoard = new int[rows][cols];
+
+        // Create a copy of the original board
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                copyBoard[row][col] = board[row][col];
+            }
+        }
+
+        // Iterate through board cell by cell.
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+
+                // For each cell count the number of live neighbors.
+                int liveNeighbors = 0;
+
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+
+                        if (!(neighbors[i] == 0 && neighbors[j] == 0)) {// this condition is to exclude its own element. we need to only check neighbours.
+                            int r = (row + neighbors[i]);
+                            int c = (col + neighbors[j]);
+
+                            // Check the validity of the neighboring cell.
+                            // and whether it was originally a live cell.
+                            // The evaluation is done against the copy, since that is never updated.
+                            if ((r < rows && r >= 0) && (c < cols && c >= 0) && (copyBoard[r][c] == 1)) {
+                                liveNeighbors += 1;
+                            }
+                        }
+                    }
+                }
+
+                // Rule 1 or Rule 3
+                if ((copyBoard[row][col] == 1) && (liveNeighbors < 2 || liveNeighbors > 3)) {
+                    board[row][col] = 0;
+                }
+                // Rule 4
+                if (copyBoard[row][col] == 0 && liveNeighbors == 3) {
+                    board[row][col] = 1;
+                }
+            }
+        }
     }
 
+    /*
+    The problem could also be solved in-place. O(M×N) space complexity could be too expensive when the board is very large. We only have two states live(1) or dead(0) for a cell.
+    We can use some dummy cell value to signify previous state of the cell along with the new changed value.
+
+    For e.g. If the value of the cell was 1 originally but it has now become 0 after applying the rule, then we can change the value to -1.
+    The negative sign signifies the cell is now dead(0) but the magnitude signifies the cell was a live(1) cell originally.
+
+    Also, if the value of the cell was 0 originally but it has now become 1 after applying the rule, then we can change the value to 2.
+    The positive sign signifies the cell is now live(1) but the magnitude of 2 signifies the cell was a dead(0) cell originally.
+
+    the only trick is to denote live --> die as -1 and die --> live as 2
+     */
+    public void gameOfLifeImproved(int[][] board) {
+// Neighbors array to find 8 neighboring cells for a given cell
+        int[] neighbors = {0, 1, -1};
+
+        int rows = board.length;
+        int cols = board[0].length;
+
+        // Iterate through board cell by cell.
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+
+                // For each cell count the number of live neighbors.
+                int liveNeighbors = 0;
+
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+
+                        if (!(neighbors[i] == 0 && neighbors[j] == 0)) {
+                            int r = (row + neighbors[i]);
+                            int c = (col + neighbors[j]);
+
+                            // Check the validity of the neighboring cell.
+                            // and whether it was originally a live cell.
+                            if ((r < rows && r >= 0) && (c < cols && c >= 0) && (Math.abs(board[r][c]) == 1)) {
+                                liveNeighbors += 1;
+                            }
+                        }
+                    }
+                }
+
+                // Rule 1 or Rule 3
+                if ((board[row][col] == 1) && (liveNeighbors < 2 || liveNeighbors > 3)) {
+                    // -1 signifies the cell is now dead but originally was live.
+                    board[row][col] = -1;
+                }
+                // Rule 4
+                if (board[row][col] == 0 && liveNeighbors == 3) {
+                    // 2 signifies the cell is now live but was originally dead.
+                    board[row][col] = 2;
+                }
+            }
+        }
+
+        // Get the final representation for the newly updated board.
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                if (board[row][col] > 0) {
+                    board[row][col] = 1;
+                } else {
+                    board[row][col] = 0;
+                }
+            }
+        }
+    }
     public static void main(String[] args) {
         int[][] matrix = new int[][]{{0,1,0},{0,0,1},{1,1,1},{0,0,0}};
     }
