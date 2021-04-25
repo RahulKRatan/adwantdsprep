5afd7461d78a953d5d5ea8273ff72378c7066cbb
goutamadwant <adwant.goutam@gmail.com>
2021-04-24 19:16:59 -0700

    Create GameOfLife.java

---
+++ src/InterviewPractice/Matrix/GameOfLife.java
@@ -0,0 +1,12 @@
+package InterviewPractice.Matrix;
+
+public class GameOfLife {
+
+    public void gameOfLife(int[][] board) {
+
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{{0,1,0},{0,0,1},{1,1,1},{0,0,0}};
+    }
+}
