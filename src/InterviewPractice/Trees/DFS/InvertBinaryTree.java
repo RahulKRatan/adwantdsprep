ddc241e36588b5a3e4adcf5caa6203d749c13625
goutamadwant <adwant.goutam@gmail.com>
2021-06-14 00:36:44 -0700

    Update InvertBinaryTree.java

--- src/InterviewPractice/Trees/DFS/InvertBinaryTree.java
+++ src/InterviewPractice/Trees/DFS/InvertBinaryTree.java
@@ -5,6 +5,16 @@ import InterviewPractice.Trees.TreeNode;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**DFS
+ * Since each node in the tree is visited only once, the time complexity is O(n), where n is the number of nodes in the tree. We cannot do better than that, since at the very least we have to visit each node to invert it.
+ *
+ * Because of recursion, O(h) function calls will be placed on the stack in the worst case, where h is the height of the tree. Because h in O(n) , the space complexity is O(n).
+ *
+ *BFS
+ * Since each node in the tree is visited / added to the queue only once, the time complexity is O(n)O(n), where nn is the number of nodes in the tree.
+ *
+ * Space complexity is O(n)O(n), since in the worst case, the queue will contain all nodes in one level of the binary tree. For a full binary tree, the leaf level has O(n) leaves.
+ */
 public class InvertBinaryTree {
         TreeNode node;
         // BFS solution
@@ -12,13 +22,12 @@ public class InvertBinaryTree {
         if (root == null) {
             return null;
         }
-
-        final Queue<TreeNode> queue = new LinkedList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
 
         while(!queue.isEmpty()) {
-            final TreeNode node = queue.poll();
-            final TreeNode left = node.left;
+            TreeNode node = queue.poll();
+            TreeNode left = node.left;
             node.left = node.right;
             node.right = left;
 
