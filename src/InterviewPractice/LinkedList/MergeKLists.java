4785be93c684c669cac0738f0e89ca5b3849210c
goutamadwant <adwant.goutam@gmail.com>
2021-06-14 00:23:14 -0700

    Update MergeKLists.java

--- src/InterviewPractice/LinkedList/MergeKLists.java
+++ src/InterviewPractice/LinkedList/MergeKLists.java
@@ -2,12 +2,28 @@ package InterviewPractice.LinkedList;
 
 import java.util.*;
 
+/**
+ * The complexity of this problem is O(knlogk), no algorithm can run it O(nlogk). We have T(n) = 2T(n/2) + kn, and therefore, O(kn*logk).
+ *
+ * Or you can think about the complexity on each node and expand. For each node, the complexity is O(logk), which is the complexity for adding to the PQ, and there are k*n that many of nodes, which gives us O(knlogk).
+ *
+ * I would say this, where k = number of lists and n = average number of nodes in each list
+ * Time : O(k log k + nk log k), where k log k for inserting k nodes in the min heap.
+ * Space : O(nk)
+ */
 public class MergeKLists {
 
     public static Node mergeKLists(List<Node> lists) {
-        Comparator<Node> comparator = Comparator.comparingInt(o -> o.val);
-        Queue<Node> minHeap = new PriorityQueue<>(comparator);
 
+        Queue<Node> minHeap = new PriorityQueue<>(Comparator.comparingInt(o -> o.val));// increasing list 1..,2..,3..
+        /*
+        public static class NodeComparator implements Comparator<ListNode> {
+        @Override
+        public int compare(ListNode o1, ListNode o2) {
+            return o1.val - o2.val;
+            }
+         }
+         */
         for(Node node: lists){
             if(node != null) {
                 minHeap.add(node);
