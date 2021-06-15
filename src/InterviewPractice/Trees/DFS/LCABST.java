162db4c252d56f92cf05b1b3082e45ce7c25ea92
goutamadwant <adwant.goutam@gmail.com>
2021-06-15 01:02:50 -0700

    Update LCABST.java

--- src/InterviewPractice/Trees/DFS/LCABST.java
+++ src/InterviewPractice/Trees/DFS/LCABST.java
@@ -3,7 +3,7 @@ package InterviewPractice.Trees.DFS;
 import InterviewPractice.Trees.TreeNode;
 
 /**
- * Time Complexity: O(N), where NN is the number of nodes in the BST. In the worst case we might be visiting all the nodes of the BST.
+ * Time Complexity: O(N), where N is the number of nodes in the BST. In the worst case we might be visiting all the nodes of the BST.
  *
  * Space Complexity: O(N). This is because the maximum amount of space utilized by the recursion stack would be NN since the height of a skewed BST could be NN.
  */
@@ -23,14 +23,13 @@ public class LCABST {
         }
     }
 
-    /*
+    /* Iterative
+    
     Time Complexity : O(N)
     Space Complexity : O(1)
 
+class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-
-        // Value of current node or parent node.
-        int parentVal = root.val;
 
         // Value of p
         int pVal = p.val;
@@ -38,16 +37,28 @@ public class LCABST {
         // Value of q;
         int qVal = q.val;
 
-        if (pVal > parentVal && qVal > parentVal) {
-            // If both p and q are greater than parent
-            return lowestCommonAncestor(root.right, p, q);
-        } else if (pVal < parentVal && qVal < parentVal) {
-            // If both p and q are lesser than parent
-            return lowestCommonAncestor(root.left, p, q);
-        } else {
-            // We have found the split point, i.e. the LCA node.
-            return root;
+        // Start from the root node of the tree
+        TreeNode node = root;
+
+        // Traverse the tree
+        while (node != null) {
+
+            // Value of ancestor/parent node.
+            int parentVal = node.val;
+
+            if (pVal > parentVal && qVal > parentVal) {
+                // If both p and q are greater than parent
+                node = node.right;
+            } else if (pVal < parentVal && qVal < parentVal) {
+                // If both p and q are lesser than parent
+                node = node.left;
+            } else {
+                // We have found the split point, i.e. the LCA node.
+                return node;
+            }
         }
+        return null;
+    }
 }
      */
     public static void main(String[] args) {
