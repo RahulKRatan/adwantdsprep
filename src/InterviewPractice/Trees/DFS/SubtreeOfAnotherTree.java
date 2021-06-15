4e392573f1afb4ff6069e4241e03ad7fe159f31c
goutamadwant <adwant.goutam@gmail.com>
2021-06-14 23:09:41 -0700

    Update SubtreeOfAnotherTree.java

--- src/InterviewPractice/Trees/DFS/SubtreeOfAnotherTree.java
+++ src/InterviewPractice/Trees/DFS/SubtreeOfAnotherTree.java
@@ -1,6 +1,27 @@
-package InterviewPractice.Trees;
+package InterviewPractice.Trees.DFS;
 
 
+import InterviewPractice.Trees.TreeNode;
+
+/**
+ * The question is exactly similar to the Leetcode 100 Same Tree
+ * Solution for Leetcode 100: https://leetcode.com/problems/same-tree/discuss/148340/CPP-Easy-to-Understand
+ *
+ * Also Check Leetcode 101 [Symmetric Tree]https://leetcode.com/problems/symmetric-tree/description/)
+ * Leetcode 101 eh? :P
+ *
+ * Okay so now you will be absolutely comfortable with this question. It just requires you to
+ *
+ * Start with a node of tree s (lets call this s-node)
+ * Compare the trees forming with root s-node and root t
+ * If the trees match(leetcode 100 logic) then return true
+ * Else go to step one and check for s->left || s->right
+ *
+ * If assume m is the number of nodes in the 1st tree and n is the number of nodes in the 2nd tree, then
+ * Time complexity: O(m*n), worst case: for each node in the 1st tree, we need to check if isSame(Node s, Node t). Total m nodes, isSame(...) takes O(n) worst case
+ * Space complexity: O(height of 1str tree)(Or you can say: O(m) for worst case, O(logm) for average case)
+ *
+ */
 //For each node during pre-order traversal of s, use a recursive function isSame to validate if sub-tree started with this node is the same with t.
 public class SubtreeOfAnotherTree {
     TreeNode node1;
