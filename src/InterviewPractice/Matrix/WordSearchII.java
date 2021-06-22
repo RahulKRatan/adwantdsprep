5ffaa6834fde13e5c02e17be934997ba66e3d90f
goutamadwant <adwant.goutam@gmail.com>
2021-06-22 00:46:32 -0700

    Create WordSearchII.java

---
+++ src/InterviewPractice/Matrix/WordSearchII.java
@@ -0,0 +1,15 @@
+package InterviewPractice.Matrix;
+
+import java.util.List;
+
+public class WordSearchII {
+    public static List<String> findWords(char[][] board, String[] words) {
+
+        return null;
+    }
+    public static void main(String[] args) {
+        char[][] chars = new char[][]{{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+        String[] word = new String[]{"ABCCED"};
+        System.out.println(findWords(chars,word));
+    }
+}
